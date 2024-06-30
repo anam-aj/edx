cb6a9542df1036bcca0dbde384ac01435ec4051d
anam-aj <anamaj.786@gmail.com>
2024-06-30 15:07:09 +0000

    Sun, Jun 30, 2024, 3:07 PM +00:00

--- hello.c
+++ hello.c
@@ -1,8 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
+
 int main(void)
 {
-    string name = get_string("Please enter you name: ")
-    int age = get_int("Please enter your age: ")
-    int number = get_int()
+    string name = get_string("Please enter you name: ");
+    int age = get_int("Please enter your age: ");
+    int number = get_int("Please enter your contact number: ");
+
+    printf()
 }
