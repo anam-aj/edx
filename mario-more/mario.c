c3ae3c11baf30c7985f1eda806d71e9cef591674
anam-aj <anamaj.786@gmail.com>
2024-07-02 13:06:47 +0000

    Tue, Jul 2, 2024, 6:36 PM +05:30

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -6,13 +6,13 @@ void print_row(int spaces, int bricks);
 
 int main(void)
 {
-    // Promt the user for pyramid height
-    int height; 
+    // Promt the user for pyramid height from 1 to 8(inclusive)
+    int height;
     do
     {
         height = get_int("Please enter the height: ");
     }
-    while (height < 1);
+    while (height < 1 || height > 8);
 
     // Prints the pyramid with given height
     for (int r = 0; r < height; r++)
