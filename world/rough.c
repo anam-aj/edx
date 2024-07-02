22eaf11c8bafe555132234ea402e74f016257c79
anam-aj <anamaj.786@gmail.com>
2024-07-02 12:23:32 +0000

    Tue, Jul 2, 2024, 5:53 PM +05:30

---
+++ world/rough.c
@@ -0,0 +1,16 @@
+// Prints pyramid
+#include <cs50.h>
+#include <stdio.h>
+
+void print_row(int spaces, int bricks);
+
+int main(void)
+{
+    // Promt the user for pyramid height
+    int height;
+    do
+    {
+        height = get_int("Please enter the height: ");
+    }
+    while (height < 1);
+}
