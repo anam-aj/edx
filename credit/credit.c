634b09ac415f6a3f8443e5a8fbd4bd3775fd27f4
anam-aj <anamaj.786@gmail.com>
2024-07-03 18:16:50 +0000

    Wed, Jul 3, 2024, 11:46 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -36,10 +36,10 @@ int main(void)
         {
             printf("VISA\n");
         }
-        
+
         else
         {
-        printf("INVALID\n");
+            printf("INVALID\n");
         }
     }
 
