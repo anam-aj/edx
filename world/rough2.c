ef99f6238f1a7cfb9d256e749685955048dae83d
anam-aj <anamaj.786@gmail.com>
2024-07-03 17:25:35 +0000

    Wed, Jul 3, 2024, 10:55 PM +05:30

--- world/rough2.c
+++ world/rough2.c
@@ -8,13 +8,37 @@ int main(void)
 {
     long number;
     // Promts the user to enter card no
+    do
+    {
+        number = get_long("Enter Card Number: ");
+    }
+    while (number <= 0);
 
 
-    number = get_long("Enter Card Number: ");
+    int sum_of_even_place_digits = 0;
+    int sum_of_odd_place_digits = 0;
+    int i = 1;
 
+    while (number > 0)
+    {
+        printf("--%i\n", i);
+        int digit_at_odd_place = number % 10;
+        printf("odd %i\n", digit_at_odd_place);
+        sum_of_odd_place_digits += digit_at_odd_place;
+        number = (number / 10);
+        printf("%li\n", number);
 
-    int new = number/10;
-    printf("%i\n", new);
-
-
+        int digit_at_even_place = number % 10;
+        printf("even %i\n", digit_at_even_place);
+        sum_of_even_place_digits += digit_at_even_place;
+        number = (number / 10);
+        printf("%li\n", number);
+        i++;
+    }
+    printf("even %i\n", sum_of_even_place_digits);
+    printf("odd %i\n", net_sum)
+    int net_sum = (sum_of_odd_place_digits) + (2 * sum_of_even_place_digits);
+    printf("sum %i\n", net_sum);
+    int unit_digit = net_sum % 10;
+    printf("unit %i\n", unit_digit);
 }
