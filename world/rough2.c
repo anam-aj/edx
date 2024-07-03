c1ab3641a1034f8051d2b3d2ee49cd5ecf50f32b
anam-aj <anamaj.786@gmail.com>
2024-07-03 16:24:00 +0000

    Wed, Jul 3, 2024, 9:54 PM +05:30

--- world/rough2.c
+++ world/rough2.c
@@ -2,21 +2,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int check_sum(long number);
 
 
-int main(void)
+
+
+int check_sum(long number)
 {
-    int change;
-    do
-    {
-        change = get_int("Change owed: ");
-    }
-    while (change < 0);
+    int sum_of_even_place_digits = 0;
+    int sum_of_odd_place_digits = 0;
 
-    if (change == 11 ||
-    change == 10)
+    while (number > 0)
     {
-        printf("b\n");
+        int digit_at_odd_place = number % 10;
+        sum_of_odd_place_digits += digit_at_odd_place;
+        number = (number / 10);
+
+        int digit_at_even_place = number % 10;
+        sum_of_even_place_digits += digit_at_even_place;
+        number = (number / 10);
     }
 
+    int net_sum = (sum_of_odd_place_digits) + (2 * sum_of_even_place_digits);
+    int unit_digit = net_sum % 10;
+    return unit_digit;
 }
