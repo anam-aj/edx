03e4c3208fcccd7f098879100528221efd9f3d55
anam-aj <anamaj.786@gmail.com>
2024-07-03 15:30:59 +0000

    Wed, Jul 3, 2024, 9:00 PM +05:30

--- world/rough2.c
+++ world/rough2.c
@@ -2,7 +2,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int no_of_coins(int amount, int coin_value);
+
 
 int main(void)
 {
@@ -13,33 +13,9 @@ int main(void)
     }
     while (change < 0);
 
-    // No of Quarters
-    int quarters = no_of_coins(change, 25);
+    if (change == (10 || 11))
+    {
+        printf("b");
+    }
 
-    // Remaining change
-    change %= 25;
-
-    // No of dimes
-    int dimes = no_of_coins(change, 10);
-
-    change %= 10;
-
-    // No of nickles
-    int nickles = no_of_coins(change, 5);
-
-    change %= 5;
-
-    // No of pennies
-    int pennies = no_of_coins(change, 1);
-
-    int total_coins = (quarters + dimes + nickles + pennies);
-
-    printf("%i\n", total_coins);
-}
-
-// Function to calculate no of coins of given value
-int no_of_coins(int amount, int coin_value)
-{
-    int coins = (amount / coin_value);
-    return coins;
 }
