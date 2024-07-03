a090dff3097dfd3d3e6790d7e9dbe13e348dd346
anam-aj <anamaj.786@gmail.com>
2024-07-03 14:40:25 +0000

    Wed, Jul 3, 2024, 8:10 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -17,10 +17,10 @@ int main(void)
     }
     while (card_number <= 0);
 
-    printf("%i/n", check_sum(card_number));
-    printf("%i/n", no_of_digits(card_number));
-    printf("%li/n", first_digit(card_number));
-    printf("%li/n", first_two_digits(card_number));
+    printf("%i\n", check_sum(card_number));
+    printf("%i\n", no_of_digits(card_number));
+    printf("%li\n", first_digit(card_number));
+    printf("%li\n", first_two_digits(card_number));
 }
 
 // function to give first digit of the given number
