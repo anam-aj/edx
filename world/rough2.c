0e382987ab4afa78970e6ab452ee99d5da6fc53c
anam-aj <anamaj.786@gmail.com>
2024-07-04 14:49:04 +0000

    Thu, Jul 4, 2024, 8:19 PM +05:30

--- world/rough2.c
+++ world/rough2.c
@@ -1,22 +1,11 @@
 // Programme to check validity and type of card
+
 #include <cs50.h>
 #include <stdio.h>
 
-int check_sum(long number);
-int no_of_digits(long number);
-long first_digit(long number);
-long first_two_digits(long number);
+
 
 int main(void)
 {
-    long card_number;
-    // Promts the user to enter card no
-    do
-    {
-        card_number = get_long("Enter Card Number: ");
-    }
-    while (card_number <= 0);
-
-    int num = card_number % 10;
-    printf("%i\n", num);
+    char c1 = 
 }
