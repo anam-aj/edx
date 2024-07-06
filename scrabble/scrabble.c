39e04b4a42d81c87a657ebb5d332e399f9b0a418
anam-aj <anamaj.786@gmail.com>
2024-07-06 17:24:52 +0000

    Sat, Jul 6, 2024, 10:54 PM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -44,7 +44,7 @@ int score(string word)
                          'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
     // Array of points
-    int points[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
+    int points[26] = {1, 3, 3, 2,  1, 4, 2, 4, 1, 8, 5, 1, 3,
                       1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
     int sum = 0;
