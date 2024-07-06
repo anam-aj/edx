b147930314344840760711e23a76479dfa828181
anam-aj <anamaj.786@gmail.com>
2024-07-06 11:44:42 +0000

    Sat, Jul 6, 2024, 5:14 PM +05:30

--- world/rough2.c
+++ world/rough2.c
@@ -8,15 +8,15 @@
 
 int main(int argc, string argv[])
 {
-    int array1[3] = {2,3,4};
+    //int array1[3] = {2,3,4};
     char array2[3] = {'a','b','c'};
     string array3[3] = {"abc","qwe","iop"};
 
-    int var1 = 2345;
-    char var2 = 'a';
-    string var3 = "ghj";
+    //int var1 = 2345;
+    //char var2 = 'a';
+    string var3 = "abc";
 
-    string array4[3] = {array2, array3[0], (array2)};
+    string array4[3] = {"abc", var3, array2};
 
     //printf("good %i  \n", var1);
     printf("good %s %s %s \n", array4[0], array4[1], array4[2]);
