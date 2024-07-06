62979acf108ba31515c8503bebb42b6826be5f51
anam-aj <anamaj.786@gmail.com>
2024-07-06 18:44:37 +0000

    Sun, Jul 7, 2024, 12:14 AM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -14,10 +14,22 @@ int main(void)
 // Funtion to count no of spaces between words
 int space_count(string text)
 {
-    
+    int no_of_spaces = 0;
+    for (i = 0, len = strlen(text); i < len; i++)
+    {
+        if (text[i] == ' ')
+        {
+            no_of_spaces++;
+        }
+    }
+
+    return no_of_spaces;
 }
+
 // Function to count word in given text
 int word_count(string text)
 {
+    int no_of_words = 0;
+    
 
 }
