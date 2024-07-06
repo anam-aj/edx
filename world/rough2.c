6a2ebb1c13b3c3df274020b5d2b84d3db5964615
anam-aj <anamaj.786@gmail.com>
2024-07-06 05:31:20 +0000

    Sat, Jul 6, 2024, 11:01 AM +05:30

--- world/rough2.c
+++ world/rough2.c
@@ -8,7 +8,8 @@
 
 int main(int argc, string argv[])
 {
-   string list[] = {"a"bcv"c","b"};
+   string list[] = {"ac","b"};
+   string list2[] = {list[]};
 
-   printf("good %s %s \n", list[0], list[1]);
+   printf("good %s %s \n", list2[0], list[1]);
 }
