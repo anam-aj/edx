0e4bf4a109255253f9de91ef0d60cd1a9e1d4c6f
anam-aj <anamaj.786@gmail.com>
2024-07-06 16:08:26 +0000

    Sat, Jul 6, 2024, 9:38 PM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -6,11 +6,15 @@
 
 int main(void)
 {
-     char alphabet[26] = {A, B, C, D, E, F, G, H, I, J, K, L, M,
-                          N, O, P, Q, R, S, T, U, V, W, X, Y, Z}
+     char letter[26] = {A, B, C, D, E, F, G, H, I, J, K, L, M,
+                        N, O, P, Q, R, S, T, U, V, W, X, Y, Z}
 
-     char alphabet[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3
-                          1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4,10}
+     int points[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3
+                       1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4,10}
+
+     
+
+
 }
 
 
