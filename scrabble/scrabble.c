ab68ce3af678eca36b128242f79c78182aa30d20
anam-aj <anamaj.786@gmail.com>
2024-07-06 18:16:52 +0000

    Sat, Jul 6, 2024, 11:46 PM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -6,32 +6,34 @@
 #include <string.h>
 
 // Funtions declaration
-int score(string word)
+int score(string word);
 
 int main(void)
 {
     // Promt user to enter word
-    string p1_word = get_string("Player 1 enter your word");
-    string p2_word = get_string("Player 2 enter your word");
+    string p1_word = get_string("Player 1 enter your word: ");
+    string p2_word = get_string("Player 2 enter your word: ");
 
     // Calculate player score
     int p1_score = score(p1_word);
     int p2_score = score(p2_word);
 
+    // Print the winner
     if (p1_score > p2_score)
     {
-        printf("Player 1 wins!");
+        printf("Player 1 wins!\n");
     }
 
     else if (p1_score < p2_score)
     {
-        printf("Player 2 wins!");
+        printf("Player 2 wins!\n");
     }
 
     else
     {
-        printf("Tie!")
+        printf("Tie!\n");
     }
+}
 
 // Function to calculate score acoording to scrabble rules
 int score(string word)
@@ -39,18 +41,22 @@ int score(string word)
 
     // Array of letters
     char alphabet[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+                         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+                        };
 
-    // Array of characters
-    int points[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3
-                      1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+    // Array of points
+    int points[26] = {1, 3, 3, 2,  1, 4, 2, 4, 1, 8, 5, 1, 3,
+                      1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10
+                     };
 
+    // Variable for score
     int sum = 0;
 
-    // Extract letters from one by one from the given word
+    // Traverse given word
     for (int i = 0, n = strlen(word); i < n; i++)
     {
-        // Converts letter to uppercase(if required)
+        // extract letter from given word one by one
+        // and converts to uppercase(if required)
         char letter = toupper(word[i]);
 
         // Traverse the alphabet array to match the letter
