56ee7d5a8c8806a8bcbe1cdd0349f4621380104a
anam-aj <anamaj.786@gmail.com>
2024-07-06 16:41:29 +0000

    Sat, Jul 6, 2024, 10:11 PM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -16,19 +16,18 @@ int main(void)
 // Function to calculate score acoording to scrabble rules
 int score(string word)
 {
+    int sum = 0;
     // Array of letters
-    char letter[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                       'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+    char letter[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
 
     // Array of characters
-    int points[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3
-                      1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4,10}
+    int points[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10}
 
     int sum = 0;
 
     for (int i = 0, n = strlen(word); i < n; i++)
     {
-        // Converts string to uppercase
+        // Converts character to uppercase
         sum += toupper(word[i])
     }
 
