6eaab119bfd2c6989da3c91b51767792107aaabf
anam-aj <anamaj.786@gmail.com>
2024-07-06 17:36:22 +0000

    Sat, Jul 6, 2024, 11:06 PM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -47,6 +47,7 @@ int score(string word)
     int points[26] = {1, 3, 3, 2,  1, 4, 2, 4, 1, 8, 5, 1, 3,
                       1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
+    // Variable for score
     int sum = 0;
 
     // Traverse given word
