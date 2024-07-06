ad0ad13d10899d9889a992b9e43236988bbe4f16
anam-aj <anamaj.786@gmail.com>
2024-07-06 20:47:58 +0000

    Sun, Jul 7, 2024, 2:17 AM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -28,12 +28,12 @@ int main(void)
     // Printing grade level
     if (index < 0)
     {
-        printf("Before Grade 1");
+        printf("Before Grade 1\n");
     }
 
     else if (index >= 16)
     {
-        printf("Grade 16+");
+        printf("Grade 16+\n");
     }
 
     else
