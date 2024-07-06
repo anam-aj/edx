8e9af9df303d0b2f71062f60ba503eb767ceb203
anam-aj <anamaj.786@gmail.com>
2024-07-06 16:22:45 +0000

    Sat, Jul 6, 2024, 9:52 PM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -10,18 +10,21 @@ int main(void)
     string p1_word = get_string("Player 1 enter your word");
     string p2_word = get_string("Player 2 enter your word");
 
-    
+}
+
+// Function to calculate score acoording to scrabble rules
+int score(string word)
+{
     // Array of letters
-    char letter[26] = {A, B, C, D, E, F, G, H, I, J, K, L, M,
-                       N, O, P, Q, R, S, T, U, V, W, X, Y, Z}
+    char letter[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                       'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
 
     // Array of characters
     int points[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3
                       1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4,10}
 
-
+    int sum1 = 0;
+    for 
 
 
 }
-
-
