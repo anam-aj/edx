2baf60f0f6fce37f584e7b76a3efb27878ceec23
anam-aj <anamaj.786@gmail.com>
2024-07-06 20:14:06 +0000

    Sun, Jul 7, 2024, 1:44 AM +05:30

--- rough/rough.c
+++ rough/rough.c
@@ -1,12 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <math.h>
 
 int main(void)
 {
     //int i = 2;
     float j = 3.3;
 
-    printf("%i\n", round(j));
+    printf("%d\n", round(j));
 }
