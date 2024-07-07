ea84cfc0ba3c961bfbc59655e62816bd302d2245
anam-aj <anamaj.786@gmail.com>
2024-07-07 17:22:22 +0000

    Sun, Jul 7, 2024, 10:52 PM +05:30

--- rough/rough3.c
+++ rough/rough3.c
@@ -1,36 +1,13 @@
-// Program to encrypt msg using caesar cipher
 
-#include <cs50.h>
-#include <ctype.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// Function decalration
-string ciphertext(string plaintext);
-int isstrdigit(string str);
-
+#include <cs50.h>
 int main(void)
 {
     string s = "a1n";
-
-    //int in = atoi(s[1]);
-
-    //printf("%i\n", in);
-    //printf("%s\n", s[1]);
-
     string str = s;
 
     str[2] = 'p';
 
     printf("%s\n", s);
-
-    //printf("%s\n", str);
-    //printf("%s\n", s);
-
-    //str = 'o';
-
-    //printf("%c\n", str);
-    //printf("%c\n", s[1]);
 
 }
