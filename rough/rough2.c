3b0e7e639e0ad064def8cb71d526ef5107454f3b
anam-aj <anamaj.786@gmail.com>
2024-07-07 18:25:24 +0000

    Sun, Jul 7, 2024, 11:55 PM +05:30

--- rough/rough2.c
+++ rough/rough2.c
@@ -6,48 +6,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-void cipher_text(void);
+string fun(string str);
 
-int main(int argc, string argv[])
+int main(void)
 {
-    // only one command line argument
-    // if 0 or more, error msg of choice and return 1
-    if (argc != 2)
-    {
-        printf("Please give exactly 1 argument\n");
-        return 1;
-    }
-
-    // command line arg should only be decimal digits, a non negaive integr
-    // if not print "Usage: ./caesar key", return 1
-    else
-    {
-        for (int i = 0, len = strlen(argv[1]); i < len; i++)
-        {
-            if (isdigit(argv[1][i]) == 0)
-            {
-                printf("Usage: ./caesar key\n");
-                return 1;
-            }
-        }
-
-        int key = atoi(argv[1]);
+    string s = "abc";
 
 
 
-        // user input with get string with two spaces "plaintext:  "
-        //string plaintext = get_string("plaintext:  ");
+    printf("%s\n", fun(s));
 
-        // output  with one space "ciphertext: "
-        //string ciphertext = cipher_text(plaintext);
-
-        // preserve lowercase and upppercase
-
-        // exit main with 0
-    }
 }
 
-string cipher_text(string text, int key)
+string fun(string str)
 {
-    int k = key;
+    str[1] = 'p';
+    return str;
 }
