bd4ee99a25abf2f80d965862129e07840e90a219
anam-aj <anamaj.786@gmail.com>
2024-07-07 15:46:08 +0000

    Sun, Jul 7, 2024, 9:16 PM +05:30

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -12,7 +12,7 @@ int isstrdigit(string str);
 
 int main(int argc, string argv[])
 {
-    if ((argc == 2) && (isstrdigit(argv1) == 0))
+    if ((argc == 2) && (isstrdigit(argv[1]) == 0))
     {
         str
 
