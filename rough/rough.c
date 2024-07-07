f2b29cba8b17f769b6a7b2bcac5b80ef9b958be9
anam-aj <anamaj.786@gmail.com>
2024-07-07 15:14:05 +0000

    Sun, Jul 7, 2024, 8:44 PM +05:30

--- rough/rough.c
+++ rough/rough.c
@@ -7,10 +7,12 @@
 #include <string.h>
 
 int main(int argc, string argv[])
-// only one command line argument,non negative integer
-// if 0 or more, error msg of choice and return 1
+{
+    // only one command line argument,non negative integer
+    // if 0 or more, error msg of choice and return 1
+    if (argc)
 
-// command line arg should only be digit
+// command line arg should only be digit, non negaive integr
 // if not print "Usage: ./caesar key", return 1
 
 // user input with get string with two spaces "plaintext:  "
@@ -21,7 +23,7 @@ int main(int argc, string argv[])
 
 // exit main with 0
 
-
+}
 
 
 int main(int argc, string argv[])
