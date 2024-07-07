3013b8dca7eff0ffb9d31448ce30ac090e1491c4
anam-aj <anamaj.786@gmail.com>
2024-07-07 17:40:40 +0000

    Sun, Jul 7, 2024, 11:10 PM +05:30

--- rough/rough1.c
+++ rough/rough1.c
@@ -6,40 +6,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, string argv[])
+int main(void)
 {
-    // only one command line argument
-    // if 0 or more, error msg of choice and return 1
-    if (argc != 2)
-    {
-        printf("Please give exactly 1 argument\n");
-        return 1;
-    }
+    string s = "abc";
 
-    // command line arg should only be decimal digits, a non negaive integr
-    // if not print "Usage: ./caesar key", return 1
-    else
-    {
-        for (int i = 0, len = strlen(argv[1]); i < len; i++)
-        {
-            if (isdigit(argv[1][i]) == 0)
-            {
-                printf("Usage: ./caesar key\n");
-                return 1;
-            }
-        }
+    s[1] = s[1] + 2;
 
-        int key = atoi(argv[1]);
-        printf("%i\n", key);
+    printf("%s\n", s);
 
-        // user input with get string with two spaces "plaintext:  "
-        //string plaintext = get_string("plaintext:  ");
-
-        // output  with one space "ciphertext: "
-        //string ciphertext = cipher_text(plaintext);
-
-        // preserve lowercase and upppercase
-
-        // exit main with 0
-    }
 }
