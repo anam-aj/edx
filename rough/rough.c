4008366b954c557738dbdbf63f92338b6f8e95a4
anam-aj <anamaj.786@gmail.com>
2024-07-07 14:59:15 +0000

    Sun, Jul 7, 2024, 8:29 PM +05:30

--- rough/rough.c
+++ rough/rough.c
@@ -6,25 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Function decalration
-string ciphertext(string plaintext);
-int isstrdigit(string str);
-
 int main(int argc, string argv[])
 {
-    string s[3] = {"abc", "123", "a1"};
-
-    //int in = atoi(s[1]);
-
-    //printf("%i\n", in);
-    //printf("%s\n", s[1]);
-
-    string str[3] = s[];
-    //str = "poi";
-
-    //printf("%s\n", str);
-    //printf("%s\n", s[1]);
-
-
-
+    
 }
