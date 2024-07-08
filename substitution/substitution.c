6ec9e84c3cbe53db7add92f159381406d5d18c57
anam-aj <anamaj.786@gmail.com>
2024-07-08 17:01:51 +0000

    Mon, Jul 8, 2024, 10:31 PM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,11 @@
 // exactly 1 command line argument i.e. key,
 // if less than1 or more, print error msg of chioce,
-// return 1 immediatly
+// return 1 from main immediately
+
+// key has exactly 26 character, only alphabetical
+// each letter only once, if not then error msg of choice
+// return 1 from main immediately
 
 // key is case insensitive
 
-// 
+//
