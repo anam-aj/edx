2e6eba187cd91c87807232479a0848e4176ad111
anam-aj <anamaj.786@gmail.com>
2024-07-08 15:14:59 +0000

    Mon, Jul 8, 2024, 8:44 PM +05:30

---
+++ rough/rough6.c
@@ -0,0 +1,26 @@
+// Program to encrypt msg using caesar cipher
+
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    string a = get_string('s: ');
+
+
+    //a[2] = {"abc","xyz","pqr"};
+    //printf("%i\n", a);
+
+    char b = a[1][1];
+    printf("%c\n", b);
+
+    a[1][1] = 'a';
+    //printf("%s\n", a[1]);
+
+
+
+
+}
