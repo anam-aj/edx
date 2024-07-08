b76d823e0087ae665ed142042bf5160aa6b32767
anam-aj <anamaj.786@gmail.com>
2024-07-08 19:03:45 +0000

    Tue, Jul 9, 2024, 12:33 AM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -21,7 +21,7 @@ int main(int argc, string argv[])
         return 1;
     }
 
-    // Checks if argument alphabetical and for no repetition of letter
+    // Checks if argument alphabetical and for repetition
     // prints error msg accordingly
     if ((is_str_alpha(argv[1]) == 'n') || (is_rep(argv[1]) == 'y'))
     {
