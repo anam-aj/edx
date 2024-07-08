dc09018be754200b432ec7ba61797c47520b3ea8
anam-aj <anamaj.786@gmail.com>
2024-07-08 16:55:56 +0000

    Mon, Jul 8, 2024, 10:25 PM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,6 +1,7 @@
 // exactly 1 command line argument i.e. key,
-// if less than1 or more, print error msg of chioce, return 1
+// if less than1 or more, print error msg of chioce,
+// return 1 immediatly
 
 // key is case insensitive
 
-//
+// 
