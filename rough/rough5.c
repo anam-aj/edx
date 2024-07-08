8c9c52904985874abbf92de79469e1ed0d344bc1
anam-aj <anamaj.786@gmail.com>
2024-07-08 14:44:57 +0000

    Mon, Jul 8, 2024, 8:14 PM +05:30

--- rough/rough5.c
+++ rough/rough5.c
@@ -11,7 +11,7 @@ int main(void)
     string a[3] = {"abc","xyz","pqr"};
     //printf("%i\n", a);
 
-    string a[3] = {"abc","xyz","pqr"};
+    a[2] = {"abc","xyz","pqr"};
     //printf("%i\n", a);
 
 
