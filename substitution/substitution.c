ad27b9022918fd589014d12166363b86120b217c
anam-aj <anamaj.786@gmail.com>
2024-07-08 17:44:50 +0000

    Mon, Jul 8, 2024, 11:14 PM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -8,11 +8,12 @@
 
 // exactly 1 command line argument i.e. key,
 // if less than1 or more, print error msg of chioce,
+// key has exactly 26 character
 // return 1 from main immediately
 
 int main(int argc, string argv[])
 {
-    if (argc != 2)
+    if ((argc != 2) || (strlen(argv[1]) != 26))
     {
         printf("Exactly 1 argument, only alphbetical,
                 26 character, no repetition"
@@ -21,9 +22,7 @@ int main(int argc, string argv[])
         return 1;
     }
 
-    // key has exactly 26 character
-    // return 1 from main immediately
-    else if (strlen(argv[1]) != 26)
+    else if
     {
         printf("Exactly 1 argument, only alphbetical,
                 26 character, no repetition"
@@ -34,19 +33,9 @@ int main(int argc, string argv[])
 
     // only alphabetical
     // return 1 from main immediately
-    else
-    {
-        for (int i = 0; i < 26; i++)
-        {
-            if (argv[1][i] < 'A'
-            || (argv[1][i] > 'Z' && argv[1][i] < 'a' )
-            ||  argv[1][i] > 'Z'
-               )
-        }
-    }
 
-        // each letter only once, if not then error msg of choice
-        // return 1 from main immediately
+    // each letter only once, if not then error msg of choice
+    // return 1 from main immediately
 
 }
 
