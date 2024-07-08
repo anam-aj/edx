7a56f56765386a33cf84dc4024ceb06e2bb9ba19
anam-aj <anamaj.786@gmail.com>
2024-07-08 14:52:18 +0000

    Mon, Jul 8, 2024, 8:22 PM +05:30

--- rough/rough4.c
+++ rough/rough4.c
@@ -11,8 +11,9 @@ int main(void)
     char a[3] = {'a','b','c'};
     //printf("%i\n", a);
 
-    a[4] = {'x','y','z','w'};
+    //a[4] = {'x','y','z','w'};
     //printf("%i\n", a);
+    char q[3] = a;
 
 
 
