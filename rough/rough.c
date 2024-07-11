45edf09d6c5f2a510c6f8439d8a7f6c0d27b4d17
anam-aj <anamaj.786@gmail.com>
2024-07-11 17:13:57 +0000

    Thu, Jul 11, 2024, 10:43 PM +05:30

--- rough/rough.c
+++ rough/rough.c
@@ -1,31 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int collatz(int number, int counter);
 
 int main(void)
 {
-    int number = get_int("number: ");
+    int array1[][];
+    
 
-    int steps = collatz(number, 0);
-
-    printf("%i\n", steps);
-
-}
-
-int collatz(int number)
-{
-
-    if (number == 1)
-    {
-        return 0;
-    }
-    else if (number % 2 == 0)
-    {
-        return 1 + collatz(number / 2);
-    }
-    else
-    {
-        return 1 + collatz((3 * number) + 1);
-    }
 }
