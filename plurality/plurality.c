a334ea1bfd88e704b2e7e7e4522326404d973b69
anam-aj <anamaj.786@gmail.com>
2024-07-11 13:09:01 +0000

    Thu, Jul 11, 2024, 6:39 PM +05:30

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,7 +66,10 @@ int main(int argc, string argv[])
 bool vote(string name)
 {
     // TODO
-    for (int i = 0, )
+    for (int i = 0, i < candidate_count; i++)
+    {
+        
+    }
     return false;
 }
 
