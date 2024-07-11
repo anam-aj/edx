8ecab75df31dc4d008f79f323c04cf4fe15230ad
anam-aj <anamaj.786@gmail.com>
2024-07-11 13:23:53 +0000

    Thu, Jul 11, 2024, 6:53 PM +05:30

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,7 +66,7 @@ int main(int argc, string argv[])
 bool vote(string name)
 {
     // TODO
-    for (int i = 0, i < candidate_count; i++)
+    for (int i = 0 ; i < candidate_count; i++)
     {
         if (strcmp(name, candidate[i]) == 0);
         {
@@ -81,6 +81,14 @@ bool vote(string name)
 void print_winner(void)
 {
     // TODO
+    int maxvotes = 0;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidate[i].votes > maxvotes);
+        {
+            maxvotes = candidate[i].votes
+        }
+    }
     
     return;
 }
