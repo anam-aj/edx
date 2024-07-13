253efe2b37bf91f843c1b2dd8c4156e774ac7353
anam-aj <anamaj.786@gmail.com>
2024-07-13 12:48:33 +0000

    Sat, Jul 13, 2024, 6:18 PM +05:30

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -124,6 +124,7 @@ void record_preferences(int ranks[])
             if ( ranks[i] != j)
             {
                 preferences[ranks[i]][j]++;
+                preferences[j][j]++;
             }
         }
     }
