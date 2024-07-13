e800cc40068368b06d48b064bec0a3f70f69637e
anam-aj <anamaj.786@gmail.com>
2024-07-13 17:52:21 +0000

    Sat, Jul 13, 2024, 11:22 PM +05:30

--- rough/rough2.c
+++ rough/rough2.c
@@ -8,33 +8,8 @@
 
 int main(void)
 {
-    string a = "abc";
-    printf("%s\n", a);
-
-    a = "pqr";
-    printf("%s\n", a);
-
-    string p = "xyz";
-    string q = p;
-    printf("%s\n", q);
-
-    q = "abc";
-
-    printf("%s\n", p);
-    printf("%s\n\n", q);
-
-    char t = q[1];
-    printf("%c\n", t);
-
-    q[1] ='k';
-    //printf("%s\n", q);
-
-    //string l = q;
-    //printf("%s\n", l);
-
-
-
-
-
+  int n = 1;
 }
+
+int num () 
 
