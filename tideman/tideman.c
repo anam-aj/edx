931ebf68829ea54e211cdcb1d55629d4620c0298
anam-aj <anamaj.786@gmail.com>
2024-07-14 14:53:46 +0000

    Sun, Jul 14, 2024, 8:23 PM +05:30

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -159,8 +159,6 @@ void record_preferences(int ranks[])
 void add_pairs(void)
 {
     // TODO
-    //int maxpairs = (candidate_count * (candidate_count - 1)) / 2;
-    //int p = 0;
     for (int i = 0; i < candidate_count; i++)
     {
         for (int j = i + 1 ; j < candidate_count; j++)
