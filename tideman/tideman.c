ec8a7f5dd20f143fffde6af17e39b763fc25838c
anam-aj <anamaj.786@gmail.com>
2024-07-14 12:45:38 +0000

    Sun, Jul 14, 2024, 6:15 PM +05:30

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -125,7 +125,7 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    // TODO
+    // TODO - holy duck it was hard as duck
     for (int i = 0; i < candidate_count; i++)
     {
         for (int j = 0; j < candidate_count; j++)
