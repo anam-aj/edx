e6cfb8938e0750362b1976ddf2b466faf83cd0c5
anam-aj <anamaj.786@gmail.com>
2024-07-14 13:22:23 +0000

    Sun, Jul 14, 2024, 6:52 PM +05:30

--- rough/rough4.c
+++ rough/rough4.c
@@ -186,6 +186,7 @@ void add_pairs(void)
 
                 else if (preferences[j][k] < preferences[k][j])
                 {
+                    printf("j: %i  k:%i \n", j, k);
                     printf("second:%i  %i/\n\n", preferences[j][k], preferences[k][j]);
                     pairs[i].winner = k;
                     pairs[i].loser = j;
