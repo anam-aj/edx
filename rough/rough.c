829ff101ba0f2005be634df42057d79146827545
anam-aj <anamaj.786@gmail.com>
2024-07-16 14:50:57 +0000

    Tue, Jul 16, 2024, 8:20 PM +05:30

--- rough/rough.c
+++ rough/rough.c
@@ -6,14 +6,14 @@ int main(void)
     string s1 = "abc";
     char s2[3] = {'a', 'b', 'c'};
 
-    printf("%c /n", s1[1]);
-    printf("%c /n", s2[1]);
+    printf("%c \n", s1[1]);
+    printf("%c \n", s2[1]);
 
-    //s1[1] = 'x';
+    s1[1] = 'x';
     s2[1] = 'x';
 
-    printf("%c /n", s1[1]);
-    printf("%c /n", s2[1]);
+    printf("%c \n", s1[1]);
+    printf("%c \n", s2[1]);
 
 
 
