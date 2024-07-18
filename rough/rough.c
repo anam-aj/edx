302861d8941676c3bca6b29394b196b494ea5aed
anam-aj <anamaj.786@gmail.com>
2024-07-18 15:44:58 +0000

    Thu, Jul 18, 2024, 9:14 PM +05:30

--- rough/rough.c
+++ rough/rough.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
 #include <cs50.h>
 #include <ctype.h>
+#include <stdio.h>
 
 void swap(int *a, int *b);
 
@@ -9,8 +9,8 @@ int main(void)
     int x = 10;
     int y = 112;
 
-    int *a = &x;
-    int *b = &y;
+    int* a = &x;
+    int* b = &y;
     swap(a, b);
     printf("x is %i  y is %i\n", x, y);
 }
