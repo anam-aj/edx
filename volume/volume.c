468632aef607246acc76e7dd683653de19d8d04d
anam-aj <anamaj.786@gmail.com>
2024-07-21 09:02:54 +0000

    Sun, Jul 21, 2024, 2:32 PM +05:30

--- volume/volume.c
+++ volume/volume.c
@@ -44,6 +44,7 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
 
+    int16_t chr2;
     while ((fread(&chr2, sizeof(int16_t), 1, input)) != 0)
     {
         fwrite(&chr2, sizeof(int16_t), 1, output);
