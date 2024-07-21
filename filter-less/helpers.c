6e852192b558e903cf5c8592ea7f1ebc3a4add09
anam-aj <anamaj.786@gmail.com>
2024-07-21 17:49:44 +0000

    Sun, Jul 21, 2024, 11:19 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -182,9 +182,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 }
             }
 
-            int avg_Bl = (int) round(sum_Bl / 3.0);
-            int avg_Gr = (int) round(sum_Gr / 3.0);
-            int avg_Re = (int) round(sum_Re / 3.0);
+            int avg_Bl = (int) round(sum_Bl / 9.0);
+            int avg_Gr = (int) round(sum_Gr / 9.0);
+            int avg_Re = (int) round(sum_Re / 9.0);
 
             cpy_image[i][j].rgbtBlue = avg_Bl;
             cpy_image[i][j].rgbtGreen = avg_Gr;
