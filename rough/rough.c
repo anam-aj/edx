cacce84548547f5adb2e4d03587c374bb804744c
anam-aj <anamaj.786@gmail.com>
2024-07-21 14:47:45 +0000

    Sun, Jul 21, 2024, 8:17 PM +05:30

--- rough/rough.c
+++ rough/rough.c
@@ -9,29 +9,5 @@
 
 int main(void)
 {
-
-    // TODO: Copy header from input file to output file
-    FILE *pt = fopen("input.wav", "r");
-
-    char chr;
-    for (int i = 0; i < 44; i++)
-    {
-
-       chr = fread(&chr, sizeof(char), 1, pt);
-       printf("%c,", chr);
-
-    }
-    printf("\n");
-
-    // TODO: Read samples from input file and write updated data to output file
-    //int16_t chr2;
-    //int16_t chr3;
-    //while ((fread(&chr2, sizeof(int16_t), 1, input)) != EOF)
-    //{
-        //chr3 = factor * chr2;
-        //fwrite(&chr3, sizeof(int16_t), 1, output);
-    //}
-
-    // Close files
-    fclose(pt);
+    int i = 55.5;
 }
