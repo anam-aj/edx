de805f14505cd3e44e2dab206e3cc6f8b4569fb4
anam-aj <anamaj.786@gmail.com>
2024-07-21 15:12:26 +0000

    Sun, Jul 21, 2024, 8:42 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -26,10 +26,13 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             float sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+            int round_sepiaBlue = (int) round(sepiaBlue);
+
             float sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+            int round_sepiaGreen = (int) round(sepiaBlue);
+
             float sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
-
-
+            int round_sepiaBlue = (int) round(sepiaBlue);
         }
     }
 
