f1a1a6db5c8feb50809df74ea9544b90897c70a7
anam-aj <anamaj.786@gmail.com>
2024-07-21 14:53:32 +0000

    Sun, Jul 21, 2024, 8:23 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -7,7 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            float avg = image[i][j];
+            float avg = image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed;
         }
     }
     return;
