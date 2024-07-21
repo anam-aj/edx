3e2922e207a0f2e380c1591cf8fbf2c34601503c
anam-aj <anamaj.786@gmail.com>
2024-07-21 06:10:56 +0000

    Sun, Jul 21, 2024, 11:40 AM +05:30

--- volume/rough.c
+++ volume/rough.c
@@ -7,28 +7,9 @@
 // Number of bytes in .wav header
 const int HEADER_SIZE = 44;
 
-int main(int argc, char *argv[])
+int main(void)
 {
-    // Check command-line arguments
-    if (argc != 4)
-    {
-        printf("Usage: ./volume input.wav output.wav factor\n");
-        return 1;
-    }
 
-    // Open files and determine scaling factor
-    FILE *input = fopen(argv[1], "r");
-    if (input == NULL)
-    {
-        printf("Could not open file.\n");
-        return 1;
-    }
-
-    FILE *output = fopen(argv[2], "w");
-    if (output == NULL)
-    {
-        printf("Could not open file.\n");
-        return 1;
     }
 
     float factor = atof(argv[3]);
