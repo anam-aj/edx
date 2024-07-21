e983afbea56bc332466f461b72990fa42105f31c
anam-aj <anamaj.786@gmail.com>
2024-07-21 16:40:53 +0000

    Sun, Jul 21, 2024, 10:10 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -131,9 +131,22 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 {
                     sum += image[i - 1][j + 1].rgbtBlue;
                 }
-
-
             }
+
+            if (i + 1 >= 0)
+            {
+                if (j - 1 >= 0)
+                {
+                    sum += image[i - 1][j - 1].rgbtBlue;
+                }
+
+                sum += image[i - 1][j].rgbtBlue;
+
+                if (j + 1 >= 0)
+                {
+                    sum += image[i - 1][j + 1].rgbtBlue;
+                }
+
         }
     }
     return;
