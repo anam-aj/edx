31ed56550a898f06269f59916f1d38b9e2c4bc99
anam-aj <anamaj.786@gmail.com>
2024-07-22 07:17:52 +0000

    Mon, Jul 22, 2024, 12:47 PM +05:30

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -331,6 +331,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                 }
             }
 
+            // 
             float SqSum_Gx_Gy_Bl = (float) ((sum_Bl_Gx * sum_Bl_Gx) + (sum_Bl_Gy * sum_Bl_Gy));
             float SqSum_Gx_Gy_Gr = (float) ((sum_Gr_Gx * sum_Gr_Gx) + (sum_Gr_Gy * sum_Gr_Gy));
             float SqSum_Gx_Gy_Re = (float) ((sum_Re_Gx * sum_Re_Gx) + (sum_Re_Gy * sum_Re_Gy));
