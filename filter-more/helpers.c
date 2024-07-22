394497d137e1be66e453378505d70c5bd3003e63
anam-aj <anamaj.786@gmail.com>
2024-07-22 05:25:14 +0000

    Mon, Jul 22, 2024, 10:55 AM +05:30

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -7,11 +7,11 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            // Calculate avg colour value for ([i][j)th pixel
+            // Calculate avg colour value for ([i][j)th pixel(target pixel)
             float avg = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0;
             int round_avg = (int) round(avg);
 
-            // Change the colour value for ([i][j])th pixel
+            // Change the colour value for ([i][j])th pixel(target pixel)
             image[i][j].rgbtBlue = round_avg;
             image[i][j].rgbtGreen = round_avg;
             image[i][j].rgbtRed = round_avg;
