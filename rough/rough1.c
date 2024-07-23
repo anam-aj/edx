f84cef116b43d13dd94c81db17b7d8d3be29619d
anam-aj <anamaj.786@gmail.com>
2024-07-23 17:02:07 +0000

    Tue, Jul 23, 2024, 10:32 PM +05:30

--- rough/rough1.c
+++ rough/rough1.c
@@ -2,22 +2,14 @@
 
 int main(void)
 {
-    int a;
-    a = 2;
-    printf("%p\n", &a);
-    a = 3;
-    printf("%p\n", &a);
+
 
 
     char *namept;
     namept = "xyz";
-    printf("%p\n", namept);
-    namept = "abcdef";
-    printf("%p\n", namept);
-    //printf("%s\n", namept);
+    *namept = 'p';
+    printf("%s\n", namept);
+    printf("%c\n", *namept);
 
-    char buffer[14];
-    int i = 5;
-    sprintf(buffer, "This is CS%03i", i);
-    printf("%s\n", buffer);
+
 }
