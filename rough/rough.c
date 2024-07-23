c7cb4d57d9209e4d76ff9020788cd152f35bdc66
anam-aj <anamaj.786@gmail.com>
2024-07-23 04:27:13 +0000

    Tue, Jul 23, 2024, 9:57 AM +05:30

--- rough/rough.c
+++ rough/rough.c
@@ -3,9 +3,7 @@
 int main(void)
 {
     FILE *file = fopen("cs5033.txt", "w");
-    //if (file != NULL)
-    //{
-        //fprintf(file, "This is CS50\n");
-        fclose(file);
-    //}
+
+    fclose(file);
+
 }
