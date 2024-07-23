64ef26ad422ed5c0e159d8954a348d8222fc7715
anam-aj <anamaj.786@gmail.com>
2024-07-23 16:07:19 +0000

    Tue, Jul 23, 2024, 9:37 PM +05:30

--- rough/rough1.c
+++ rough/rough1.c
@@ -4,13 +4,11 @@ int main(void)
 {
     char *namept;
     namept = "xyz";
+    namept = "abcdef";
     printf("%s\n", namept);
 
-    char name[] = {'a','b','c','\0'};
-    printf("%s\n", name);
-
-    char buffer[13];
-
+    char *buffer;
+    buffer = "qwe";
     int i = 50;
     sprintf(buffer, "This is CS%i", i);
     printf("%s\n", buffer);
