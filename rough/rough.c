9dcfca10c54ac367537113a021ae87fd8fa9febd
anam-aj <anamaj.786@gmail.com>
2024-07-23 04:40:44 +0000

    Tue, Jul 23, 2024, 10:10 AM +05:30

--- rough/rough.c
+++ rough/rough.c
@@ -3,9 +3,10 @@
 int main(void)
 {
     char *f = "abcd";
+    char *f2 = "abcd";
 
     FILE *file1 = fopen(f, "w");
-    FILE *file2 = fopen("abcd", "w");
+    FILE *file2 = fopen(f2, "w");
 
     fclose(file1);
     fclose(file2);
