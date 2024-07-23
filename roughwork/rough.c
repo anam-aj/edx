30ae7f0d7fbbf17f96891692aca2e4e17ea169f5
anam-aj <anamaj.786@gmail.com>
2024-07-23 14:55:45 +0000

    Tue, Jul 23, 2024, 8:25 PM +05:30

--- roughwork/rough.c
+++ roughwork/rough.c
@@ -3,10 +3,10 @@
 int main(void)
 {
     char *name[4];
-    //name[0] = ;
-    //name[1] = ;
-    //name[2] = ;
-    //name[3] = ;
+    name[0] = ;
+    name[1] = ;
+    name[2] = ;
+    name[3] = ;
 
     //printf("%p\n", name);
 
