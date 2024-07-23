6d10355a0797b9dccaed08108f99ac7cc0bba570
anam-aj <anamaj.786@gmail.com>
2024-07-23 18:54:46 +0000

    Wed, Jul 24, 2024, 12:24 AM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -3,12 +3,16 @@
 
 int main(int argc, char *argv[])
 {
+    // Checks no of argument
     if (argc != 2)
     {
         printf("Usage: ./recover file_name\n");
         return 1;
     }
 
-    
+    // Reads file to recover from
+    FIlE *memory_card = fopen("argv[1]", "r");
+
+
 
 }
