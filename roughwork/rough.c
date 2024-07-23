10add431c6ad8fb8bc88ea826e15d2dedb9502e2
anam-aj <anamaj.786@gmail.com>
2024-07-23 07:00:04 +0000

    Tue, Jul 23, 2024, 12:30 PM +05:30

--- roughwork/rough.c
+++ roughwork/rough.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
 int main(void)
-{   "xyz";
-    char *name = "abcd";
+{
+    char *name = "xyz";
+
+    
+
+
+
     printf("%p\n", name);
-    printf("%p\n", &name);
-    printf("%p\n", "abcd");
-
-    //name = "xyz";
-    //printf("%p\n", name);
-    //FILE *file1 = fopen(name, "w");
-
-    //printf("hi\n");
+    FILE *file1 = fopen(name, "w");
 
 
 }
