b6da24be6c60707768ff9bb5e8054baca3bf8fa4
anam-aj <anamaj.786@gmail.com>
2024-07-23 07:17:32 +0000

    Tue, Jul 23, 2024, 12:47 PM +05:30

--- roughwork/rough.c
+++ roughwork/rough.c
@@ -2,26 +2,6 @@
 
 int main(void)
 {
-    char *s;
-    //printf("%p\n", s);
-
-    s = "asdf";
-    printf("%p\n", s);
-    printf("%p\n", "asdf");
-    printf("%p\n", &s);
-
-
-    printf("%s\n", s);
-
-
-    s = "xyz";
-    printf("%p\n", s);
-    printf("%p\n", "xyz");
-    printf("%p\n", &s);
-
-
-    printf("%s\n", s);
-
     //char *name[4];
     //name[0] = ;
     //name[1] = ;
