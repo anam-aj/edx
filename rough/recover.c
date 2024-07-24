dcf91eeeeb404be98e81b256fa07f97f0cdf129c
anam-aj <anamaj.786@gmail.com>
2024-07-24 15:08:25 +0000

    Wed, Jul 24, 2024, 8:38 PM +05:30

--- rough/recover.c
+++ rough/recover.c
@@ -45,6 +45,7 @@ int main(int argc, char *argv[])
         }
     }
 
+    FILE *image = fopen(name, "a");
     while ((fread(buffer, sizeof(uint8_t), 512, memory_card)) == 512)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&
