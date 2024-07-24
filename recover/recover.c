f511bdbf6cd938961954968494a5ab4eb73ef77e
anam-aj <anamaj.786@gmail.com>
2024-07-24 13:51:03 +0000

    Wed, Jul 24, 2024, 7:21 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -41,6 +41,8 @@ int main(int argc, char *argv[])
         }
     }
 
+    FILE *image = fopen(name, "a");
+    
     while ((fread(buffer, sizeof(uint8_t), 512, memory_card)) != 0)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
