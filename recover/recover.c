0614c63e5971482896fdc21fdf4e2e662fc9fa6c
anam-aj <anamaj.786@gmail.com>
2024-07-24 14:08:29 +0000

    Wed, Jul 24, 2024, 7:38 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
     }
 
     // Reads memory card
-    while ((fread(buffer, sizeof(uint8_t), 512, memory_card)) != 0)
+    while ((fread(buffer, sizeof(uint8_t), 512, memory_card)) == 512)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&
             (buffer[3] & 0xf0) == 0xe0)
