b2a08a14dfc418702db49c2463e18f543a8bb253
anam-aj <anamaj.786@gmail.com>
2024-07-24 15:05:16 +0000

    Wed, Jul 24, 2024, 8:35 PM +05:30

--- rough/rough1.c
+++ rough/rough1.c
@@ -1,10 +1,66 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    char buffer[14];
+    // Checks no of argument
+    if (argc != 2)
+    {
+        printf("Usage: ./recover file_name\n");
+        return 1;
+    }
 
-    int i = 1;
-    sprintf(buffer, "This is CS%03i", i);
-    printf("%s\n", buffer);
+    // Open file to recover from, for reading
+    FILE *memory_card = fopen(argv[1], "r");
+    if (memory_card == NULL)
+    {
+        return 1;
+    }
+
+    // Create name of jpg files
+    char name[8];
+    int srl_num = 0;
+    sprintf(name, "%03i.jpg", srl_num);
+
+    // Buffer to read from memory card
+    uint8_t buffer[512];
+
+    // Open first image for writing
+    FILE *image = fopen(name, "w");
+    if (image == NULL)
+    {
+        return 1;
+    }
+
+    // Reads memory card
+    while ((fread(buffer, sizeof(uint8_t), 512, memory_card)) == 512)
+    {
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&
+            (buffer[3] & 0xf0) == 0xe0)
+        {
+            (fwrite(buffer, sizeof(uint8_t), 512, image));
+            break;
+        }
+    }
+
+    while ((fread(buffer, sizeof(uint8_t), 512, memory_card)) == 512)
+    {
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&
+            (buffer[3] & 0xf0) == 0xe0)
+        {
+            fclose(image);
+            srl_num++;
+            sprintf(name, "%03i.jpg", srl_num);
+            image = fopen(name, "w");
+            (fwrite(buffer, sizeof(uint8_t), 512, image));
+        }
+        else
+        {
+            (fwrite(buffer, sizeof(uint8_t), 512, image));
+        }
+    }
+    fclose(image);
+
+    fclose(memory_card);
 }
