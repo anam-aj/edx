826552d2bbf3f91b756dbb9a53b75d71c38d80fe
anam-aj <anamaj.786@gmail.com>
2024-07-28 12:51:54 +0000

    Sun, Jul 28, 2024, 6:21 PM +05:30

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -49,9 +49,8 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        member->parents[0] = NULL;
-        member->parents[0] = NULL;
         
+
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
     }
 
@@ -59,6 +58,8 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
+        member->parents[0] = NULL;
+        member->parents[0] = NULL;
 
         // TODO: Randomly assign alleles
     }
