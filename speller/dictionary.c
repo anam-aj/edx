2aaf7f8dbe9567bed47d2ba8561d1a95444aad25
anam-aj <anamaj.786@gmail.com>
2024-07-29 04:45:43 +0000

    Mon, Jul 29, 2024, 10:15 AM +05:30

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -36,7 +36,7 @@ unsigned int hash(const char *word)
 bool load(const char *dictionary)
 {
     // Open the file to read
-    FILE *file = fopen(dictionary, 'r');
+    FILE *file = fopen(dictionary, "r");
     if (file == NULL)
     {
         return false;
