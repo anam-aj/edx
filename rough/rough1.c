19b5c286a76df65d8a5c8df6a0bc56b159af44e8
anam-aj <anamaj.786@gmail.com>
2024-07-30 07:10:04 +0000

    Tue, Jul 30, 2024, 12:40 PM +05:30

--- rough/rough1.c
+++ rough/rough1.c
@@ -2,7 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <strings.h>
 
+unsigned int hash(const char *word);
 
 int main(void)
 {
