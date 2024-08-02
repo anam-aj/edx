e8c094fdf61aa8ca4d89b0869b3c35097197953a
anam-aj <anamaj.786@gmail.com>
2024-08-02 07:56:13 +0000

    Fri, Aug 2, 2024, 1:26 PM +05:30

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -10,37 +10,36 @@ def main()
         change = get_float("Change owed: ")
         if change < 0:
             break
-        
-    dollars = change / 1
+
+    dollars = change // 1
     cents = change % 1
 
+    change = int(dollars + cents)
+
     # No of Quarters
-    int quarters = no_of_coins(change, 25)
+    quarters = no_of_coins(change, 25)
 
     # Remaining change
     change %= 25
 
     # No of dimes
-    int dimes = no_of_coins(change, 10)
-
+    dimes = no_of_coins(change, 10)
     change %= 10
 
     # No of nickles
-    int nickles = no_of_coins(change, 5)
-
+    nickles = no_of_coins(change, 5)
     change %= 5
 
     # No of pennies
-    int pennies = no_of_coins(change, 1)
+    pennies = no_of_coins(change, 1)
 
-    int total_coins = (quarters + dimes + nickles + pennies)
+    total_coins = (quarters + dimes + nickles + pennies)
 
-    printf("%i\n", total_coins)
+    print(total_coins)
 
 
-// Function to calculate no of coins of given value
-int no_of_coins(int amount, int coin_value)
-{
-    int coins = (amount / coin_value);
-    return coins;
-}
+# Function to calculate no of coins of given value
+def no_of_coins(amount, coin_value)
+
+    coins = (amount / coin_value)
+    return coins
