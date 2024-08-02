5bed462aee595d4be1fb8ca0c4f6bc85e33cc27d
anam-aj <anamaj.786@gmail.com>
2024-08-02 10:23:48 +0000

    Fri, Aug 2, 2024, 3:53 PM +05:30

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -25,12 +25,12 @@ def main():
             print("AMEX")
 
         # Checks criteria for MasterCard
-        elif (d_count == 16) and (d2 > 50 and d2 < 56):
+        elif (d_num == 16) and (d2 > 50 and d2 < 56):
 
             print("MASTERCARD")
 
         # Checks criteria for Visa
-        elif (d_count == 13 or d_count == 16) and (d1 == 4):
+        elif (d_num == 13 or d_num == 16) and (d1 == 4):
 
             print("VISA")
 
