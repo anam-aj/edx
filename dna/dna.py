a376777dacd52fcdb628b684fb116a2bd942bf67
anam-aj <anamaj.786@gmail.com>
2024-08-03 12:59:46 +0000

    Sat, Aug 3, 2024, 6:29 PM +05:30

--- dna/dna.py
+++ dna/dna.py
@@ -26,8 +26,8 @@ def main():
     match_dict = {}
     for STR in STR_list:
         match_dict[STR] = longest_match(dna_sequence, STR)
-        
 
+    print(list_persons)
     # TODO: Check database for matching profiles
     for i in range(len(list_persons)):
         count = 0
