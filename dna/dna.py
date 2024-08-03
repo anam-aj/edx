f8311589f7f3a954e622a9ea8963a8ce5690607e
anam-aj <anamaj.786@gmail.com>
2024-08-03 12:35:57 +0000

    Sat, Aug 3, 2024, 6:05 PM +05:30

--- dna/dna.py
+++ dna/dna.py
@@ -13,33 +13,12 @@ def main():
     with open(sys.argv[1]) as file:
         dict_file = csv.DictReader(file)
         list_persons = list(dict_file)
+        print((list_persons[0]))
 
     # TODO: Read DNA sequence file into a variable
     with open(sys.argv[2]) as file:
         dna_sequence = file.read()
 
-    # TODO: Find longest match of each STR in DNA sequence
-    key_list = list(list_persons[0].keys())
-    cpy_list = key_list
-    STR_list = cpy_list.pop(0)
-
-    match_dict = {}
-    for STR in STR_list:
-        match_dict[STR] = longest_match(dna_sequence, STR)
-
-    # TODO: Check database for matching profiles
-    for i in range(len(list_persons)):
-        count = 0
-        for j in range(len(STR_list)):
-            count += 1
-            if list_persons[i][STR_list[j]] != match_dict[STR_list[j]]:
-                break
-        if count == len(STR_list):
-            print(list_persons[i][key_list[0]])
-            return
-
-    print("No match")
-    return
 
 
 def longest_match(sequence, subsequence):
--- rough/rough.py
+++ rough/rough.py
@@ -1,3 +1,22 @@
-str1 = "hello how are u"
+    # TODO: Find longest match of each STR in DNA sequence
+    key_list = list(list_persons[0].keys())
+    cpy_list = key_list
+    STR_list = cpy_list.pop(0)
 
-n = str1.count()
+    match_dict = {}
+    for STR in STR_list:
+        match_dict[STR] = longest_match(dna_sequence, STR)
+
+    # TODO: Check database for matching profiles
+    for i in range(len(list_persons)):
+        count = 0
+        for j in range(len(STR_list)):
+            count += 1
+            if list_persons[i][STR_list[j]] != match_dict[STR_list[j]]:
+                break
+        if count == len(STR_list):
+            print(list_persons[i][key_list[0]])
+            return
+
+    print("No match")
+    return
