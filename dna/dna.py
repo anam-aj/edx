387ef56342e25fe0de4f61782bac305aed00bd3c
anam-aj <anamaj.786@gmail.com>
2024-08-03 12:01:33 +0000

    Sat, Aug 3, 2024, 5:31 PM +05:30

--- dna/dna.py
+++ dna/dna.py
@@ -28,7 +28,8 @@ def main():
 
     # TODO: Check database for matching profiles
     for i in range(len(list_persons)):
-        for j in range(len(key_list) - 1)
+        for j in range(len(key_list) - 1):
+            if list_persons[i]
 
     return
 
