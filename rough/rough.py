77f3723d8ff0de709da6cf15fe4983b11f561fe0
anam-aj <anamaj.786@gmail.com>
2024-08-04 07:24:50 +0000

    Sun, Aug 4, 2024, 12:54 PM +05:30

--- rough/rough.py
+++ rough/rough.py
@@ -1,22 +1,9 @@
-    # TODO: Find longest match of each STR in DNA sequence
-    key_list = list(list_persons[0].keys())
-    cpy_list = key_list
-    STR_list = cpy_list.pop(0)
+# Prints all favorites in CSV using csv.reader
 
-    match_dict = {}
-    for STR in STR_list:
-        match_dict[STR] = longest_match(dna_sequence, STR)
+import csv
 
-    # TODO: Check database for matching profiles
-    for i in range(len(list_persons)):
-        count = 0
-        for j in range(len(STR_list)):
-            count += 1
-            if list_persons[i][STR_list[j]] != match_dict[STR_list[j]]:
-                break
-        if count == len(STR_list):
-            print(list_persons[i][key_list[0]])
-            return
+# Open CSV file
+with open("favorites.csv", "r") as file:
 
-    print("No match")
-    return
+    # Create reader
+    reader = csv.reader(file)
