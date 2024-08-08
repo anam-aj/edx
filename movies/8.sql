66a716c7046a54e51a5dd35eb47b8813ec32f294
anam-aj <anamaj.786@gmail.com>
2024-08-08 14:58:54 +0000

    Thu, Aug 8, 2024, 8:28 PM +05:30

--- movies/8.sql
+++ movies/8.sql
@@ -1,4 +1,4 @@
-SELECT name
-FROM people
-WHERE people JOIN stars ON people.id = stars.person_id
-
+SELECT *
+FROM (people JOIN stars ON people.id = stars.person_id)
+JOIN (stars JOIN movies ON stars.movie_id = movies.id)
+limit 5;
