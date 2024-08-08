e4f11fdd4f571bbe8f19b5353e9280147989496f
anam-aj <anamaj.786@gmail.com>
2024-08-08 15:45:40 +0000

    Thu, Aug 8, 2024, 9:15 PM +05:30

--- movies/8.sql
+++ movies/8.sql
@@ -1,13 +1,16 @@
+-- Select names
 SELECT name
 FROM people
 WHERE id IN
     (
+    -- Select person IDs
     SELECT person_id
     FROM stars
     WHERE movie_id =
         (
+        -- Select movie ID
         SELECT id
         FROM movies
         WHERE title = 'Toy Story'
         )
-    )
+    );
