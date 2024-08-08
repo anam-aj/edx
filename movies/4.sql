99eb1eef62dc3fbcb1655fbc23242a2bdd524140
anam-aj <anamaj.786@gmail.com>
2024-08-08 15:19:05 +0000

    Thu, Aug 8, 2024, 8:49 PM +05:30

--- movies/4.sql
+++ movies/4.sql
@@ -1,4 +1,13 @@
+--SELECT count(title)
+--FROM movies JOIN ratings
+--ON movies.id = ratings.movie_id
+--WHERE rating = 10.0;
+
 SELECT count(title)
-FROM movies JOIN ratings
-ON movies.id = ratings.movie_id
-WHERE rating = 10.0;
+FROM movies
+WHERE rating = (
+    select rating
+    from ratings
+    where rating = 10
+);
+
