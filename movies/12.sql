c88d6d4d947ea9ca8192c7fc782b8166eaf83d47
anam-aj <anamaj.786@gmail.com>
2024-08-09 15:06:13 +0000

    Fri, Aug 9, 2024, 8:36 PM +05:30

--- movies/12.sql
+++ movies/12.sql
@@ -6,12 +6,18 @@ WHERE id IN
     -- Select movie IDs
     SELECT movie_id
     FROM stars
-    
-    WHERE person_id =
-        (
+    GROUP by movie_id
+    HAVING COUNT(
         -- Select person ID
         SELECT id
         FROM people
         WHERE name = 'Bradley Cooper'
-        )
+        ) = 1
+        AND
+        COUNT(
+        -- Select person ID
+        SELECT id
+        FROM people
+        WHERE name = 'Jennifer Lawrence'
+        ) = 1
     )
