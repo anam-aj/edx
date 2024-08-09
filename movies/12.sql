f0c18f66bdc0373e56b7fd0972b78ad63de4de03
anam-aj <anamaj.786@gmail.com>
2024-08-09 14:07:12 +0000

    Fri, Aug 9, 2024, 7:37 PM +05:30

---
+++ movies/12.sql
@@ -0,0 +1,16 @@
+-- Select title
+SELECT title
+FROM movies
+WHERE id IN
+    (
+    -- Select movie IDs
+    SELECT movie_id
+    FROM stars
+    WHERE person_id =
+        (
+        -- Select person ID
+        SELECT id
+        FROM people
+        WHERE name = 'Bradley Cooper'
+        )
+    )
