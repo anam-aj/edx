5d7f326a2b5e766b2bdd02aa648a35bb04978a0b
anam-aj <anamaj.786@gmail.com>
2024-08-09 19:21:29 +0000

    Sat, Aug 10, 2024, 12:51 AM +05:30

--- movies/13.sql
+++ movies/13.sql
@@ -1,18 +1,18 @@
 -- Select name
-SELECT name
+SELECT count(name)
 FROM people
 WHERE id IN(
     -- Select person IDs
     SELECT person_id
     FROM stars
-    WHERE movie_id
-    HAVING SUM(
-        person_id = (
+    WHERE movie_id IN(
+        SELECT movie_id
+        FROM stars
+        WHERE person_id = (
             -- Select person ID
             SELECT id
             FROM people
-            WHERE name = 'Bradley Cooper'
+            WHERE name = 'Kevin Bacon'
         )
     )
-)
-
+);
