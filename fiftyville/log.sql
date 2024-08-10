a83561ce78fd8aa9f70ee630d52341bbe7443fce
anam-aj <anamaj.786@gmail.com>
2024-08-10 06:18:20 +0000

    Sat, Aug 10, 2024, 11:48 AM +05:30

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
+
+--  To get the contents of database
 .schema
 
+-- Get the structure of table crime_scene_report
 select *  from crime_scene_reports limit 5;
+
