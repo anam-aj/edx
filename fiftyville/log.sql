3d4934a1492817e09ff0229498b09527d2221fc3
anam-aj <anamaj.786@gmail.com>
2024-08-10 16:58:34 +0000

    Sat, Aug 10, 2024, 10:28 PM +05:30

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -151,37 +151,6 @@ id IN (
 
 
 
-
-
-SELECT phone_number
-FROM (
-    SELECT *
-FROM people
-WHERE license_plate IN (
-    SELECT license_plate
-    FROM bakery_security_logs
-    WHERE (
-        year = 2023
-        AND month = 7
-        AND day = 28
-        AND hour = 10
-        AND minute <=30
-        AND activity = 'exit'
-    )
-)
-AND
-phone_number IN (
-    SELECT caller
-    FROM phone_calls
-    WHERE (
-        year = 2023
-        AND month = 7
-        AND day = 28
-        AND duration < 60
-    )
-)
-AND
-passport_number IN (
     SELECT passport_number
     FROM passengers
     WHERE flight_id IN (
@@ -194,22 +163,3 @@ passport_number IN (
             AND origin_airport_id = 8
         )
     )
-)
-AND
-id IN (
-    SELECT person_id
-    FROM bank_accounts
-    WHERE account_number IN (
-        SELECT account_number
-        FROM atm_transactions
-        WHERE (
-            year = 2023
-            AND month = 7
-            AND day = 28
-            AND transaction_type = 'withdraw'
-            AND atm_location = 'Leggett Street'
-        )
-    )
-)
-
-);
