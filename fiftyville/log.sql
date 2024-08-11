e6aaa7bdffba2b5d7c57a0d95bd26ccb67149c77
anam-aj <anamaj.786@gmail.com>
2024-08-11 06:04:15 +0000

    Sun, Aug 11, 2024, 11:34 AM +05:30

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -346,40 +346,40 @@ WHERE id IN (
             AND atm_location = 'Leggett Street'
         )
     )
-)
-AND phone_number IN (
-    SELECT caller
-    FROM phone_calls
-    WHERE (
-        year = 2023
-        AND month = 7
-        AND day = 28
-        AND duration < 60
     )
-)
-AND license_plate IN (
-    SELECT license_plate
-    FROM bakery_security_logs
-    WHERE (
-        year = 2023
-        AND month = 7
-        AND day = 28
-        AND hour = 10
-        AND minute <=30
-        AND activity = 'exit'
-    )
-)
-AND passport_number IN (
-    SELECT passport_number
-    FROM passengers
-    WHERE flight_id IN (
-        SELECT id
-        FROM flights
+    AND phone_number IN (
+        SELECT caller
+        FROM phone_calls
         WHERE (
             year = 2023
             AND month = 7
-            AND day = 29
-            AND id = 36
+            AND day = 28
+            AND duration < 60
         )
     )
-)
+    AND license_plate IN (
+        SELECT license_plate
+        FROM bakery_security_logs
+        WHERE (
+            year = 2023
+            AND month = 7
+            AND day = 28
+            AND hour = 10
+            AND minute <=30
+            AND activity = 'exit'
+        )
+    )
+    AND passport_number IN (
+        SELECT passport_number
+        FROM passengers
+        WHERE flight_id IN (
+            SELECT id
+            FROM flights
+            WHERE (
+                year = 2023
+                AND month = 7
+                AND day = 29
+                AND id = 36
+            )
+        )
+    )
