a75f41c5b3c1f8e241273913431344cf32940a49
anam-aj <anamaj.786@gmail.com>
2024-08-11 04:41:58 +0000

    Sun, Aug 11, 2024, 10:11 AM +05:30

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -30,7 +30,7 @@ If you have security footage from the bakery parking lot, you might want to look
 | 162 | Eugene  | 2023 | 7     | 28  |
 I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery,
 I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.
-                                                                                                |
+
 | 163 | Raymond | 2023 | 7     | 28  |
 As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call,
 I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief
