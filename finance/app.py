8a98adf0b742cfd242842047982fd688c0372081
anam-aj <anamaj.786@gmail.com>
2024-08-26 19:48:36 +0000

    Tue, Aug 27, 2024, 1:18 AM +05:30

--- finance/app.py
+++ finance/app.py
@@ -35,7 +35,7 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    info = "hello bhadwe"
+    rows = db.execute("SELECT * FROM holdings WHERE")
     return render_template("index.html", data = info)
 
 
