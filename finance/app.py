762318f8d624823af7cacd3f5d77d38054aa679d
anam-aj <anamaj.786@gmail.com>
2024-08-27 06:20:02 +0000

    Tue, Aug 27, 2024, 11:50 AM +05:30

--- finance/app.py
+++ finance/app.py
@@ -42,7 +42,8 @@ def index():
     cash = user_dict[0]["cash"]
     # User's share holdings
     holdings_dict = db.execute("SELECT * FROM holdings WHERE id = ? AND shares > 0", user_id)
-    stock_
+    symbol = 
+    stock = lookup(symbol)
     share_price =
     return render_template("index.html", holdings_data = holdings_dict)
 
