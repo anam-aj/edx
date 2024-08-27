4b61fa2facda7acfeb8bd675182d40a527444f51
anam-aj <anamaj.786@gmail.com>
2024-08-27 06:30:39 +0000

    Tue, Aug 27, 2024, 12:00 PM +05:30

--- finance/app.py
+++ finance/app.py
@@ -48,7 +48,7 @@ def index():
         price = stock["price"]
         shares = row["shares"]
         total = price * shares
-        
+        row[]
 
     return render_template("index.html", holdings_data = holdings_dict)
 
