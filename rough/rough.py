1adeaab3b10547723a3f71d73ad80edd089812d0
anam-aj <anamaj.786@gmail.com>
2024-09-01 14:22:57 +0000

    Sun, Sep 1, 2024, 7:52 PM +05:30

--- rough/rough.py
+++ rough/rough.py
@@ -1,10 +1,2 @@
-# Prints all favorites in CSV using csv.reader
-
-import csv
-
-# Open CSV file
-with open("favorites.csv", "r") as file:
-
-    # Create reader
-    reader = csv.reader(file)
-    print((reader))
+with open("hello.txt", "r") as hello:
+    print(hello.read())
