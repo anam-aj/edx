b93a4b6e1448b1b7c0ea091384fdc27b85e4b015
anam-aj <anamaj.786@gmail.com>
2024-09-03 08:19:33 +0000

    Tue, Sep 3, 2024, 1:49 PM +05:30

---
+++ einstein/einstein.py
@@ -0,0 +1,16 @@
+# Program to convert mass into energy
+
+def main():
+
+    # Promts user for input
+    mass = int(input("Mass: "))
+
+    # Conver mass to energy
+    c = 3 * 10**8
+    energy = mass *
+
+    # Prints to user
+    print(output)
+
+
+main()
