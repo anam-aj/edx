bf84659a9c7d298b909d890eaf74b718a9e45d79
anam-aj <anamaj.786@gmail.com>
2024-09-03 08:03:54 +0000

    Tue, Sep 3, 2024, 1:33 PM +05:30

---
+++ faces/faces.py
@@ -0,0 +1,15 @@
+# Program to replace emoticon with emoji
+
+def main():
+
+    # Promts user for input
+    user_input = input("Please type your sentence here: ")
+
+    # Replace emoticon with emoji
+    output = user_input.replace(" ", "")
+
+    # Prints to user
+    print(output)
+
+
+main()
