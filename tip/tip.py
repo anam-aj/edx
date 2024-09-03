43c0722d9d78b03be5a2fcbb97ead18ef75f883d
anam-aj <anamaj.786@gmail.com>
2024-09-03 08:44:46 +0000

    Tue, Sep 3, 2024, 2:14 PM +05:30

--- tip/tip.py
+++ tip/tip.py
@@ -12,13 +12,15 @@ def dollars_to_float(d):
 
     dollars = d.replace("$", "")
     dollars = float(dollars)
+
     return dollars
 
 
 def percent_to_float(p):
 
     percent = p.replace("%", "")
-    percent = float(percent)
+    percent = float(percent) / 100
+
     return percent
 
 
