36c13b3e6ec7bb38a1926744ae5c3a667141e696
anam-aj <anamaj.786@gmail.com>
2024-09-04 05:30:39 +0000

    Wed, Sep 4, 2024, 11:00 AM +05:30

--- deep/deep.py
+++ deep/deep.py
@@ -1,17 +1,19 @@
 # Program to answer the "Great question of life"
 
-# Promts user for answer
-answer = input("What is the answer to Great Question of Life? ")
 
-# Strip spaces and converts to lowercase
-answer = answer.lower().strip()
+def main():
 
-# Check answer
-if answer == "42":
-    print("Yes")
-elif answer == "forty two":
-    print("yes")
-elif answer == "forty-two":
-    print("Yes")
-else:
-    print("No")
+    # Promts user for answer
+    answer = input("What is the answer to Great Question of Life? ")
+
+    # Strip spaces and converts to lowercase
+    answer = answer.lower().strip()
+
+    # Check answer
+    if answer == "42" or answer == "forty two" or answer == "forty-two":
+        print("Yes")
+    else:
+        print("No")
+
+
+main()
