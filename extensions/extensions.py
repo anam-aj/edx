3e7ab43fe987aeca54fe435cb59c3a0dba231ba5
anam-aj <anamaj.786@gmail.com>
2024-09-04 06:40:21 +0000

    Wed, Sep 4, 2024, 12:10 PM +05:30

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -7,28 +7,35 @@ def main():
     file = input("File name: ")
 
     # Check extension and tell media type
-    media_type = check_type(file)
+    media_type = get_media_type(file)
 
     # Prints media type to user
     print(media_type)
 
 
-def check_type(file_name):
+def get_media_type(file_name):
 
     if file_name.endswith(".gif"):
         return ("image/gif")
+
     elif file_name.endswith(".jpg"):
         return ("image/jpeg")
+
     elif file_name.endswith(".jpeg"):
         return ("image/jpeg")
+
     elif file_name.endswith(".png"):
         return ("image/png")
+
     elif file_name.endswith(".pdf"):
         return ("application/pdf")
+
     elif file_name.endswith(".txt"):
         return ("text/plain")
+
     elif file_name.endswith(".zip"):
         return ("application/zip")
+
     else:
         return ("application/octet-stream")
 
