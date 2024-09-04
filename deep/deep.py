49fa140238f3067bfb8ae1dc624f840328c35a64
anam-aj <anamaj.786@gmail.com>
2024-09-04 05:16:44 +0000

    Wed, Sep 4, 2024, 10:46 AM +05:30

--- deep/deep.py
+++ deep/deep.py
@@ -6,4 +6,7 @@ answer = input("What is the answer to Great Question of Life? ")
 # Check answer
 if int(answer) == 42:
     print("Yes")
-elif answer.lower().strip == "forty two"
+elif answer.lower().strip == "forty two" or answer.lower().strip == "forty-two":
+    print("Yes")
+else:
+    print("No")
