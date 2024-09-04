2da3eb9b07d0abebae635a96941b88c5b54da6cb
anam-aj <anamaj.786@gmail.com>
2024-09-04 13:54:08 +0000

    Wed, Sep 4, 2024, 7:24 PM +05:30

---
+++ meal/meal.py
@@ -0,0 +1,20 @@
+# Program to tell meal type
+
+
+def main():
+
+    # Promts user for time
+    time = input("Please Enter Time: ")
+
+    # Get meal type
+    meal = meal_type(time)
+
+    # Prints meal type to user
+    print(meal)
+
+def convert(time):
+    ...
+
+
+if __name__ == "__main__":
+    main()
