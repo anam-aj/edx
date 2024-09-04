4221a27423554aa3957bdcfe494ade2cd618153e
anam-aj <anamaj.786@gmail.com>
2024-09-04 06:25:46 +0000

    Wed, Sep 4, 2024, 11:55 AM +05:30

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,1 +1,15 @@
-# Program to 
+# Program to find media type of file
+
+def main():
+
+    # Promts user for filename
+    file = input("File name: ")
+
+    # Check extension and tell media type
+    media_type = check_type(file)
+
+    # Prints media type to user
+    print(media_type)
+
+def check_type(file_name):
+    
