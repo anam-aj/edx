a3e0d5e2d208343063b19562fb309a37c39d0df8
anam-aj <anamaj.786@gmail.com>
2024-09-04 07:03:52 +0000

    Wed, Sep 4, 2024, 12:33 PM +05:30

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -4,6 +4,39 @@
 def main():
 
     # Promts user for input
-    exp = input("Expression: ")
+    expression = input("Expression: ")
 
-    
+    # Obtain result
+    result = get_result(expression)
+
+    # Print result to user
+    print(result)
+
+
+def get_result(exp):
+
+    # Split input and assign values
+    n1, op, n2 = exp.split()
+
+    # Convert string to float
+    n1 = float(n1)
+    n2 = float(n2)
+
+    # Add
+    if op == "+":
+        return n1 + n2
+
+    # Subtract
+    if op == "-":
+        return n1 - n2
+
+    # Product
+    if op == "*":
+        return n1 * n2
+
+    # Quotient
+    if op == "/":
+        return n1 / n2
+
+
+main()
