f5f546e7344db359faa6ced015142c9afe984bdd
anam-aj <anamaj.786@gmail.com>
2024-09-05 05:33:29 +0000

    Thu, Sep 5, 2024, 11:03 AM +05:30

--- coke/coke.py
+++ coke/coke.py
@@ -7,15 +7,20 @@ def main():
     cost = 50
     amount_due = cost
 
-    # Promts user for remaining amount
+    # While there is still amount due
     while amount_due > 0:
 
+        # Shows due amount and ask for coin
         print(f"Amount Due: {amount_due}")
         coin_value = int(input("Insert Coin: "))
+
+        # Ensure coin is of valid denomination
         if coin_value in [5, 10, 25]:
+
+            # Update due amount
             amount_due -= coin_value
 
-    # Chnage owed
+    # Shows change
     change = -1 * amount_due
     print(f"Change Owed: {change}")
 
