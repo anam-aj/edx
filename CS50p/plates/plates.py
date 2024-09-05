75d152f0183cc26bba40c018f6cc97a9ac2941ed
anam-aj <anamaj.786@gmail.com>
2024-09-05 11:27:25 +0000

    Thu, Sep 5, 2024, 4:57 PM +05:30

--- CS50p/plates/plates.py
+++ CS50p/plates/plates.py
@@ -46,8 +46,8 @@ def is_valid(s):
             # Update that first number is encountered(so further number can be "0")
             number_found = True
 
-        # Ensure that all character following first number are numeric
-        # (when character is not numeric)
+        # when character is not numeric
+        # (Ensure that all character following first number are numeric)
         elif number_found:
             return False
 
