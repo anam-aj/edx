705927959253e79fb1d86da0d4a3142825c0f9e4
anam-aj <anamaj.786@gmail.com>
2024-09-05 05:20:28 +0000

    Thu, Sep 5, 2024, 10:50 AM +05:30

--- coke/coke.py
+++ coke/coke.py
@@ -1,1 +1,15 @@
-# Program to 
+# Program to simulate coke vending machine
+
+
+def main():
+
+    # Set cost of coke
+    cost = 50
+    amount_due = cost
+
+    # Promts user for remaining amount
+    while cost > 0:
+
+        print(f"Amount Due: {amount_due}")
+        coin_value = int(input("Insert Coin: "))
+
