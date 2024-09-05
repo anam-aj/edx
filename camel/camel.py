d0dd54286771df0697e3cdf000adc366b7af3f86
anam-aj <anamaj.786@gmail.com>
2024-09-05 04:48:57 +0000

    Thu, Sep 5, 2024, 10:18 AM +05:30

---
+++ camel/camel.py
@@ -0,0 +1,19 @@
+# Program to convert camelCase to snake_case
+
+
+def main():
+
+    # Promts user for input
+    camel_case = input("camelCase: ")
+
+    # Coverts into snake_case
+    snake_case = make_snake_case(camel_case)
+
+    # Print snake_case to user
+    print(snake_case)
+
+
+def make_snake_case(string)
+
+
+main()
