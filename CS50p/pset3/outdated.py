59c6b59a16c269bc9c3e460fcc44b7312a367f0d
anam-aj <anamaj.786@gmail.com>
2024-09-06 06:53:44 +0000

    Fri, Sep 6, 2024, 12:23 PM +05:30

--- CS50p/pset3/outdated.py
+++ CS50p/pset3/outdated.py
@@ -1,1 +1,23 @@
-# Program to covert date to standard forma
+# Program to covert date to standard format
+
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+
+def main():
+
+    date = input("Date")
+
+main()
