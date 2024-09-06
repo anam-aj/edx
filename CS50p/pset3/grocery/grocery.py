3977857b063922462d0e6528007defa82b2966fe
anam-aj <anamaj.786@gmail.com>
2024-09-06 06:22:15 +0000

    Fri, Sep 6, 2024, 11:52 AM +05:30

--- CS50p/pset3/grocery/grocery.py
+++ CS50p/pset3/grocery/grocery.py
@@ -6,6 +6,7 @@ def main():
     # Dictionary to store items
     items = {}
 
+    # Promts user for iteam and add them to dictionary
     while True:
         try:
             item = input().upper()
@@ -17,7 +18,9 @@ def main():
             print()
             break
 
+    # Print items (sorted alphabetically)
     for key in sorted(items):
-            print(f"{items[key]} {key}")
+        print(f"{items[key]} {key}")
+
 
 main()
