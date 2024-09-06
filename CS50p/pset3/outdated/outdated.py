63933a58add04ed0ae07f9355ee1abe32f6bf938
anam-aj <anamaj.786@gmail.com>
2024-09-06 19:00:47 +0000

    Sat, Sep 7, 2024, 12:30 AM +05:30

--- CS50p/pset3/outdated/outdated.py
+++ CS50p/pset3/outdated/outdated.py
@@ -20,42 +20,8 @@ def main():
     while True:
         date = input("Date: ")
 
-        split_on_slash = False
-        split_on_space = False
 
-        try:
-            month, day, year = date.split("/")
-        except:
-            pass
-        else:
-            try:
-                month, day, year = int(month), int(day), int(year)
-                if month < 1 or month > 12 or day < 1 or day > 31:
-                    raise ValueError
-            except:
-                pass
-            else:
-                split_on_slash = True
-                break
 
-        try:
-            month, day, year = date.split(" ")
-        except:
-            pass
-        else:
-            try:
-                if month not in months:
-                    raise KeyError
-                if "," not in day:
-                    raise ValueError
-                day, year = int(day.replace(",", "")), int(year)
-                if day < 1 or day > 31:
-                    raise ValueError
-            except:
-                pass
-            else:
-                split_on_space = True
-                break
 
 def split_on_slash(date):
 
@@ -76,11 +42,26 @@ def split_on_slash(date):
    formatted_date = f"{year:04}-{month:02}-{day:02}"
 
 
+def split_on_space(date):
 
+    try:
+        month, day, year = date.split(" ")
+    except:
+        pass
+    else:
+        try:
+            if month not in months:
+                raise KeyError
+            if "," not in day:
+                raise ValueError
+            day, year = int(day.replace(",", "")), int(year)
+            if day < 1 or day > 31:
+                raise ValueError
+        except:
+            pass
+        else:
+            break
 
-
-
-    elif split_on_space:
         month_count = 0
         for m in months:
             month_count += 1
