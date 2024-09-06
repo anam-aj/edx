7bd374ce28eca32e85ce0e0b12ce34392c50fd96
anam-aj <anamaj.786@gmail.com>
2024-09-06 05:32:31 +0000

    Fri, Sep 6, 2024, 11:02 AM +05:30

--- CS50p/taqueria/taqueria.py
+++ CS50p/taqueria/taqueria.py
@@ -1,15 +1,15 @@
 # Program to place an order
 
 menu = {
-        "Baja Taco": 4.25,
-        "Burrito": 7.50,
-        "Bowl": 8.50,
-        "Nachos": 11.00,
-        "Quesadilla": 8.50,
-        "Super Burrito": 8.50,
-        "Super Quesadilla": 9.50,
-        "Taco": 3.00,
-        "Tortilla Salad": 8.00
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
 }
 
 
@@ -30,29 +30,5 @@ def main():
         else:
             print(f"${bill:.2f}")
 
-main()
-
-
-"""
-main()
-
-
-def main():
-
-    bill = 0
-
-    try:
-        while True:
-            item = input("Item: ").lower()
-            for key in menu:
-                if item == key.lower():
-                    bill += menu[key]
-                    print(f"${round(bill, 2)}")
-
-    except EOFError:
-        print()
-        return
-
 
 main()
-"""
