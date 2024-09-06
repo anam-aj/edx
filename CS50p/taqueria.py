5322caa657b177f70b898c1f98883b5f1d5ce1aa
anam-aj <anamaj.786@gmail.com>
2024-09-06 04:34:35 +0000

    Fri, Sep 6, 2024, 10:04 AM +05:30

--- CS50p/taqueria.py
+++ CS50p/taqueria.py
@@ -15,19 +15,9 @@ def main():
         "Tortilla Salad": 8.00
     }
 
-    
-# Convert camelCase to snake_case
-def make_snake_case(string):
-
-    snake_case = ""
-
-    for chr in string:
-        if chr.isupper():
-            snake_case += "_" + chr.lower()
-        else:
-            snake_case += chr
-
-    return snake_case
+    while True:
+        item = input("Item: ")
+        
 
 
 main()
