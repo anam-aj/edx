27460c3455a0bda4b20247560ac325574a206245
anam-aj <anamaj.786@gmail.com>
2024-09-06 03:32:07 +0000

    Fri, Sep 6, 2024, 9:02 AM +05:30

--- CS50p/pset3/fuel/fuel.py
+++ CS50p/pset3/fuel/fuel.py
@@ -6,7 +6,7 @@ def main():
     # Ask user to enter fraction
     fraction = get_fraction("Fraction: ")
 
-    
+
 
 
 
@@ -16,9 +16,9 @@ def get_fraction(string):
     while True:
 
         fraction = input(string)
-        numer, denom = fraction.split("/")
 
         try:
+            numer, denom = fraction.split("/")
             numer, denom = float(numer), float(denom)
             return (numer/denom)
         except ValueError:
