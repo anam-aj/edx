d20c43719ef3850f43908fc1aa2685011ead053b
anam-aj <anamaj.786@gmail.com>
2024-09-11 17:47:35 +0000

    Wed, Sep 11, 2024, 11:17 PM +05:30

--- CS50p/pset4/game/game.py
+++ CS50p/pset4/game/game.py
@@ -4,7 +4,39 @@ import random
 
 def main():
 
-    
+    # Promts user for level
+    while True:
+        level = input("Level: ")
+
+        try:
+            level = int(level)
+        except:
+            pass
+        else:
+            if level < 1:
+                print("Level has to be a positive integer")
+            else:
+                break
+
+    correct_value = random.randint(1, level)
+
+    # Ask user for guess
+    while True:
+        guess = input("Guess: ")
+
+        try:
+            guess = int(guess)
+        except:
+            pass
+        else:
+            # Check if user's guess is correct
+            if guess < correct_value:
+                print("Too small!")
+            elif guess > correct_value:
+                print("Too large!")
+            else:
+                print("Just right!")
+                break
 
 
 main()
