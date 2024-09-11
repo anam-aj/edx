50b5e70d314bb19157e0caca5988a6c766a1665c
anam-aj <anamaj.786@gmail.com>
2024-09-11 03:23:27 +0000

    Wed, Sep 11, 2024, 8:53 AM +05:30

--- CS50p/pset4/figlet/figlet.py
+++ CS50p/pset4/figlet/figlet.py
@@ -3,3 +3,15 @@
 import sys
 from pyfiglet import Figlet
 
+
+def main():
+
+    # Promts user to enter text
+    text = input("Input:  ")
+
+    figlet = Figlet(text)
+
+    print(figlet)
+
+
+main()
