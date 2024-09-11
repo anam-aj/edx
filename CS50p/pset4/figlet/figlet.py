fa63e9c620e4c4466120874f55d1961f692bfc08
anam-aj <anamaj.786@gmail.com>
2024-09-11 14:00:25 +0000

    Wed, Sep 11, 2024, 7:30 PM +05:30

--- CS50p/pset4/figlet/figlet.py
+++ CS50p/pset4/figlet/figlet.py
@@ -7,36 +7,35 @@ from pyfiglet import Figlet
 
 def main():
 
-    # Ensure validity iof command line arguments
-    if len(sys.argv) != 1 or len(sys.argv) == 3:
-        
+    # Check no of command line arguments
+    if len(sys.argv) == 1 or len(sys.argv) == 3:
 
-    # Promts user to enter text
-    text = input("Input:  ")
+        # Promts user to enter text
+        text = input("Input:  ")
 
-    figlet = Figlet()
-    # Get available fonts
-    fonts = figlet.getFonts()
+        figlet = Figlet()
+        # Get available fonts
+        fonts = figlet.getFonts()
 
-    # Command line argument not given
-    if len(sys.argv) == 1:
-        # Get random font
-        rand_font = random.choice(fonts)
-        figlet.setFont(font=rand_font)
-        # Prints text in random font
-        print(figlet.renderText(text))
-
-    # Command line arguments given
-    elif len(sys.argv) == 3:
-        # Check Command line arguments
-        if sys.argv[1] not in ["-f", "--font"]:
-            sys.exit("Invalid arguments")
-        elif sys.argv[2] not in fonts:
-            sys.exit("Enter valid font")
-        else:
-            # Prints text in user given font
-            figlet.setFont(font=sys.argv[2])
+        # Command line argument not given
+        if len(sys.argv) == 1:
+            # Get random font
+            rand_font = random.choice(fonts)
+            figlet.setFont(font=rand_font)
+            # Prints text in random font
             print(figlet.renderText(text))
+
+        # Command line arguments given
+        elif len(sys.argv) == 3:
+            # Check Command line arguments
+            if sys.argv[1] not in ["-f", "--font"]:
+                sys.exit("Invalid arguments")
+            elif sys.argv[2] not in fonts:
+                sys.exit("Enter valid font")
+            else:
+                # Prints text in user given font
+                figlet.setFont(font=sys.argv[2])
+                print(figlet.renderText(text))
 
     else:
         sys.exit("Please give valid Command Line Argument(s) i.e. '0' or '2'")
