7f46b2dac443c990dc0e08d220769939d78590f1
anam-aj <anamaj.786@gmail.com>
2024-09-12 10:20:43 +0000

    Thu, Sep 12, 2024, 3:50 PM +05:30

--- CS50p/pset4/bitcoin/bitcoin.py
+++ CS50p/pset4/bitcoin/bitcoin.py
@@ -6,8 +6,19 @@ import sys
 
 def main():
 
-    if is_valid()
-    number =  sys.argv[1]
+    if argv_is_valid():
+        number =  sys.argv[1]
+
+def argv_is_valid():
+
+    if len(sys.argv) != 2:
+        sys.exit("Please give number of bitcoin")
+
+    num_of_bitcoin = sys.argv[1]
+    try:
+        num_of_bitcoin = float(num_of_bitcoin)
+    except:
+        sys.exit("Enter a valid number")
 
 
 main()
