5bf4ca5714fc99bed04bd6e8878070b2f893bc89
anam-aj <anamaj.786@gmail.com>
2024-09-12 09:54:49 +0000

    Thu, Sep 12, 2024, 3:24 PM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -1,17 +1,32 @@
-# Program to convert text to figlet
-
-import random
-import sys
-from pyfiglet import Figlet
-
-
-def main():
-
-
-    figlet = Figlet()
-    # Get available fonts
-    fonts = figlet.getFonts()
-    print(fonts)
-
-
-main()
+{
+    "time": {
+        "updated": "Sep 12, 2024 09:52:44 UTC",
+        "updatedISO": "2024-09-12T09:52:44+00:00",
+        "updateduk": "Sep 12, 2024 at 10:52 BST",
+    },
+    "disclaimer": "This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org",
+    "chartName": "Bitcoin",
+    "bpi": {
+        "USD": {
+            "code": "USD",
+            "symbol": "&#36;",
+            "rate": "58,088.161",
+            "description": "United States Dollar",
+            "rate_float": 58088.1607,
+        },
+        "GBP": {
+            "code": "GBP",
+            "symbol": "&pound;",
+            "rate": "44,522.019",
+            "description": "British Pound Sterling",
+            "rate_float": 44522.0193,
+        },
+        "EUR": {
+            "code": "EUR",
+            "symbol": "&euro;",
+            "rate": "52,732.955",
+            "description": "Euro",
+            "rate_float": 52732.9551,
+        },
+    },
+}
