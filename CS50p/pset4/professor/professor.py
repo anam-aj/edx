efdc4a0be67d96c35ae4c5ab623a192c3377801b
anam-aj <anamaj.786@gmail.com>
2024-09-12 07:21:41 +0000

    Thu, Sep 12, 2024, 12:51 PM +05:30

--- CS50p/pset4/professor/professor.py
+++ CS50p/pset4/professor/professor.py
@@ -61,11 +61,11 @@ def get_level():
 def generate_integer(level):
 
     if level == 1:
-        return random.randint(1, 9)
+        return random.randint(0, 9)
     elif level == 2:
-        return random.randint(1, 99)
+        return random.randint(10, 99)
     elif level == 3:
-        return random.randint(1, 999)
+        return random.randint(100, 999)
     else:
         raise ValueError
 
