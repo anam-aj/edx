ca255534ac889a46092a217ff4e1e2f28e9c1b8c
anam-aj <anamaj.786@gmail.com>
2024-09-12 05:50:57 +0000

    Thu, Sep 12, 2024, 11:20 AM +05:30

--- CS50p/pset4/professor.py
+++ CS50p/pset4/professor.py
@@ -3,13 +3,21 @@
 import random
 
 
+
+import random
+
+
 def main():
+
+
+
+def get_level(string):
 
     # Promts user for level
     while True:
-        level = input("Level: ")
+        level = input(string)
 
-        # Ensure level is a positive integer
+        # Ensure level is valid
         try:
             level = int(level)
             if level not in [1,2,3]:
@@ -17,7 +25,31 @@ def main():
         except:
             pass
         else:
-            break
+            return level
+
+
+def generate_integer(level):
+    ...
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+
+
 
     # Set correct value
     correct_value = random.randint(1, level)
