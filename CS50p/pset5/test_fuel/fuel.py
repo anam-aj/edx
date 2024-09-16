748530e8d8b1f13be5bcc45a28cd0ce143016307
anam-aj <anamaj.786@gmail.com>
2024-09-16 16:58:44 +0000

    Mon, Sep 16, 2024, 10:28 PM +05:30

---
+++ CS50p/pset5/test_fuel/fuel.py
@@ -0,0 +1,60 @@
+# Program to convert fraction to percentage
+
+
+def main():
+
+    fraction = input(string)
+
+
+def convert(fraction):
+
+    try:
+        numer, denom = fraction.split("/")
+        # Ensure both numerator and denominator are integers
+        if not numer.isdigit() or not denom.isdigit():
+            raise ValueError
+        # Ensure fraction is not greater than 1
+        if numer > denom:
+            raise ValueError
+        # Generate and return the fraction
+        numer, denom = int(numer), int(denom)
+        return (numer/denom)
+
+    except ValueError:
+        print("Input format 'X/Y' and X,Y are integers")
+    except ZeroDivisionError:
+        print("Division by zero not allowed")
+
+
+def gauge(percentage):
+    ...
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+def main():
+
+    # Ask user to enter fraction
+    while True:
+        fraction = get_fraction("Fraction: ")
+        if 0 <= fraction <= 1:
+            break
+
+    # Convert to percent
+    percent = fraction * 100
+
+    # Display result
+    if percent <= 1:
+        print("E")
+    elif percent >= 99:
+        print("F")
+    else:
+        print(f"{round(percent)}%")
+
+
+
+
+main()
