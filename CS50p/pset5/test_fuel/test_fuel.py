5c0282595ff40b7fb18fbdc1ac3b022bb340f9a7
anam-aj <anamaj.786@gmail.com>
2024-09-17 05:44:51 +0000

    Tue, Sep 17, 2024, 11:14 AM +05:30

--- CS50p/pset5/test_fuel/test_fuel.py
+++ CS50p/pset5/test_fuel/test_fuel.py
@@ -33,6 +33,8 @@ def test_convert_zero_division():
 def test_gauge():
 
     assert gauge(100) == "F"
+    assert gauge(99) == "F"
     assert gauge(1) == "E"
+    assert gauge(0) == "E"
     assert gauge(50) == "50%"
     assert gauge(10) == "10%"
