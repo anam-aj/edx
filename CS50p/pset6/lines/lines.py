ce7f33f5e67a81cc4d8a456b03de0620fcae006a
anam-aj <anamaj.786@gmail.com>
2024-09-21 06:55:08 +0000

    Sat, Sep 21, 2024, 12:25 PM +05:30

--- CS50p/pset6/lines/lines.py
+++ CS50p/pset6/lines/lines.py
@@ -6,10 +6,13 @@ def main():
 
     # Ensure correct number of comand line argument
     if len(sys.argv) != 2:
-        
+        sys.exit("Please give one file name only")
 
-    # Ensure name ends with py
+    # Ensure name ends with .py
+    if not (sys.argv[1]).endswith(".py"):
+        sys.exit("Please enter valid python file")
 
+    
     # Ensure file exists
 
 
