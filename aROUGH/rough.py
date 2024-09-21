6e307b14dbbb672e1ea93116df8d930f49965b3e
anam-aj <anamaj.786@gmail.com>
2024-09-21 06:15:59 +0000

    Sat, Sep 21, 2024, 11:45 AM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -1,32 +1,5 @@
-{
-    "time": {
-        "updated": "Sep 12, 2024 09:52:44 UTC",
-        "updatedISO": "2024-09-12T09:52:44+00:00",
-        "updateduk": "Sep 12, 2024 at 10:52 BST",
-    },
-    "disclaimer": "This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org",
-    "chartName": "Bitcoin",
-    "bpi": {
-        "USD": {
-            "code": "USD",
-            "symbol": "&#36;",
-            "rate": "58,088.161",
-            "description": "United States Dollar",
-            "rate_float": 58088.1607,
-        },
-        "GBP": {
-            "code": "GBP",
-            "symbol": "&pound;",
-            "rate": "44,522.019",
-            "description": "British Pound Sterling",
-            "rate_float": 44522.0193,
-        },
-        "EUR": {
-            "code": "EUR",
-            "symbol": "&euro;",
-            "rate": "52,732.955",
-            "description": "Euro",
-            "rate_float": 52732.9551,
-        },
-    },
-}
+a = """ abc
+    xyz
+pqr   """
+
+print(a)
