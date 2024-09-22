6374a1a95b6e1edb45d17ea64e1911b6c2b485cc
anam-aj <anamaj.786@gmail.com>
2024-09-22 06:28:22 +0000

    Sun, Sep 22, 2024, 11:58 AM +05:30

--- CS50p/pset6/shirt/shirt.py
+++ CS50p/pset6/shirt/shirt.py
@@ -33,7 +33,7 @@ def validate_arguments():
     if not read_file or not write_file:
         sys.exit("Please enter image file")
 
-
+    # Ensure both input and outpun have same extension
 # Ensure file exist
 def open_file(file_name):
 
