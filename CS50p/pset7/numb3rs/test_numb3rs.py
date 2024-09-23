4a436b99546a5985cb5e48e0299872f3e3a098a7
anam-aj <anamaj.786@gmail.com>
2024-09-23 18:02:50 +0000

    Mon, Sep 23, 2024, 11:32 PM +05:30

--- CS50p/pset7/numb3rs/test_numb3rs.py
+++ CS50p/pset7/numb3rs/test_numb3rs.py
@@ -9,6 +9,8 @@ def test_correct_input():
 
 def test_out_of_range():
     assert validate("512.512.512.512") == False
+    assert validate("256.256.256.256") == False
+    assert validate("300.300.300.300") == False
     assert validate("1.1.1.1000") == False
 
 def test_word():
