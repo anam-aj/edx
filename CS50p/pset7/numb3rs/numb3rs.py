bde80f0161b2388ae91e964d88a53b2d06bfe4ae
anam-aj <anamaj.786@gmail.com>
2024-09-23 16:45:35 +0000

    Mon, Sep 23, 2024, 10:15 PM +05:30

---
+++ CS50p/pset7/numb3rs/numb3rs.py
@@ -0,0 +1,19 @@
+# Program to check validity of IP adresses
+
+import re
+import sys
+
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip):
+    ...
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
