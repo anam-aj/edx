80ab4c1d85f6094fd675473dd3173d3932e5aff6
anam-aj <anamaj.786@gmail.com>
2024-09-23 17:34:57 +0000

    Mon, Sep 23, 2024, 11:04 PM +05:30

---
+++ CS50p/pset7/numb3rs/test_numb3rs.py
@@ -0,0 +1,15 @@
+from numb3rs import validate
+
+
+def test_correct_input():
+    assert validate("127.0.0.1") == True
+    assert validate("255.255.255.255") == True
+
+
+def test_out_of_range():
+    assert validate("512.512.512.512") == False
+    assert validate("1.1.1.1000") == False
+
+def test_word():
+    assert validate("cat") == False
+    assert validate("cat.1.1.1") == False
