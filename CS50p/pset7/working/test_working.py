82b35e1bc7260af48818218706d0953026b6cdd9
anam-aj <anamaj.786@gmail.com>
2024-09-24 17:29:31 +0000

    Tue, Sep 24, 2024, 10:59 PM +05:30

--- CS50p/pset7/working/test_working.py
+++ CS50p/pset7/working/test_working.py
@@ -7,5 +7,10 @@ def test_valid_time():
 
 
 def test_invalid_time():
-    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
-    assert convert("09:00 AM to 05:00 PM") == "09:00 to 17:00"
+    assert convert("19 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("09:00 AM to 50:00 PM") == "09:00 to 17:00"
+
+
+def test_invalid_format():
+    assert convert("19 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("09:00 AM to 50:00 PM") == "09:00 to 17:00"
