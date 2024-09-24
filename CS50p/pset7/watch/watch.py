2dd361358e811fbc1af626ad36833c6ae99c5290
anam-aj <anamaj.786@gmail.com>
2024-09-24 14:21:48 +0000

    Tue, Sep 24, 2024, 7:51 PM +05:30

--- CS50p/pset7/watch/watch.py
+++ CS50p/pset7/watch/watch.py
@@ -10,8 +10,10 @@ def parse(s):
     pattern = r'iframe src="https?://(?:www.)?youtube.com/embed/(.+)"></iframe>'
 
     if match := re.search(pattern, s):
-        match.group(1)
-
+        video_id = match.group(1)
+        return f"https://youtu.be/{video_id}"
+    else:
+        return None
 
 if __name__ == "__main__":
     main()
