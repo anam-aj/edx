714a2087a0a36a662734da2c27ad374c5dcf7689
anam-aj <anamaj.786@gmail.com>
2024-09-24 15:26:57 +0000

    Tue, Sep 24, 2024, 8:56 PM +05:30

--- CS50p/pset7/working/working.py
+++ CS50p/pset7/working/working.py
@@ -15,12 +15,12 @@ def convert(s):
     # Pattern for time
     pattern1 = r"^(?P<start_hour>0?[1-9]|10|11|12):(?P<start_min>0?\d|[1-5]\d) AM to (?P<end_hour>0?[1-9]|10|11|12):(?P<end_min>0?\d|[1-5]\d) PM$"
 
-    # Find macthing pattern
+    # Find macthing pattern(AM to PM)
     if match := re.search(pattern1, s.strip()):
-        start_hr = match.group("start_hour")
-        start_mn = match.group("start_min")
-        end_hr = match.group("end_hour")
-        end_mn = match.group("end_min")
+        start_hr = int(match.group("start_hour"))
+        start_mn = int(match.group("start_min"))
+        end_hr = int(match.group("end_hour"))
+        end_mn = int(match.group("end_min"))
 
     # Return time in 24-hour format
 
