67e5eb89c6b3e99d8a978e3580acef139266949e
anam-aj <anamaj.786@gmail.com>
2024-09-24 14:52:41 +0000

    Tue, Sep 24, 2024, 8:22 PM +05:30

--- CS50p/pset7/working/working.py
+++ CS50p/pset7/working/working.py
@@ -1,14 +1,23 @@
+# Program to convert time from 12-hour format to 24-hour format
+
 import re
 import sys
 
 
 def main():
+
+    # Print time in 24-hour format to user
     print(convert(input("Hours: ")))
 
 
 def convert(s):
 
-    # 
+    # Pattern for time
+    pattern = "^ AM to PM$"
+
+    # Find macthing pattern
+
+    # Return time in 24-hour format
 
 
 if __name__ == "__main__":
