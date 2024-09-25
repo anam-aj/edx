9a6ffb1f5fac2c0708486ff7a4d7bba5551c9f22
anam-aj <anamaj.786@gmail.com>
2024-09-25 15:47:30 +0000

    Wed, Sep 25, 2024, 9:17 PM +05:30

--- CS50p/pset7/um/um.py
+++ CS50p/pset7/um/um.py
@@ -10,7 +10,7 @@ def main():
 
 def count(s):
 
-    pattern = r"(?:^(um)[^a-zA-Z])+|[^a-zA-Z](um)[^a-zA-Z]|(?:[^a-zA-Z]*(um)$)|(?:^(um)$)"
+    pattern = r"(?:^(um)[^a-zA-Z])|[^a-zA-Z](um)[^a-zA-Z]|(?:[^a-zA-Z]*(um)$)|(?:^(um)$)"
     if match := re.search(pattern, s, re.IGNORECASE):
         count = 0
         for m in match.groups():
