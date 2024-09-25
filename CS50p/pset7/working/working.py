2132a400a4f2f0d06df6aed2501822449b9689e4
anam-aj <anamaj.786@gmail.com>
2024-09-25 13:01:28 +0000

    Wed, Sep 25, 2024, 6:31 PM +05:30

--- CS50p/pset7/working/working.py
+++ CS50p/pset7/working/working.py
@@ -16,24 +16,28 @@ def convert(s):
     pattern1 = r"^(?P<start_hour>0?[1-9]|10|11|12)(:(?P<start_min>0?\d|[1-5]\d))? AM to (?P<end_hour>0?[1-9]|10|11|12)(:(?P<end_min>0?\d|[1-5]\d))? PM$"
     pattern2 = r"^(?P<start_hour>0?[1-9]|10|11|12)(:(?P<start_min>0?\d|[1-5]\d))? PM to (?P<end_hour>0?[1-9]|10|11|12)(:(?P<end_min>0?\d|[1-5]\d))? AM$"
 
-    # Found macthing pattern(AM to PM)
+    # Macthing pattern found(AM to PM)
     if match := re.search(pattern1, s.strip()):
-        # S
+        # Convert starting hour
         if int(start_hr) < 12:
             start_hr = match.group("start_hour")
         else:
             start_hr = "00"
+        # Convert starting minute
         if match.group("start_min"):
             start_mn = match.group("start_min")
         else:
             start_mn = "00"
+        # Convert ending hour
         end_hr = match.group("end_hour")
+        end_hr = str(int(end_hr) + 12)
+        # Convert ending minute
         if match.group("end_min"):
             end_mn = match.group("end_min")
         else:
             end_mn = "00"
         # Convert time to 24-hour format and return it
-        end_hr = str(int(end_hr) + 12)
+
         time = f"{start_hr.zfill(2)}:{start_mn} to {end_hr.zfill(2)}:{end_mn}"
         return time
     # Found macthing pattern(PM to AM)
