a114c8fa3203f8cc003d36cd36505a67b3941797
anam-aj <anamaj.786@gmail.com>
2024-09-25 14:46:44 +0000

    Wed, Sep 25, 2024, 8:16 PM +05:30

--- CS50p/pset7/um/um.py
+++ CS50p/pset7/um/um.py
@@ -3,11 +3,15 @@ import sys
 
 
 def main():
+
+    # Prints number of repetetion to user
     print(count(input("Text: ")))
 
 
 def count(s):
+
     pattern = r"[^a-zA-Z]+um[^a-zA-Z]+"
+    match = re.search(pattern, s, re.IGNORECASE)
 
     if match := matches.
 
