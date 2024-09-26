a6a0965ff0600bc921c3df02d95f62cc492b1d16
anam-aj <anamaj.786@gmail.com>
2024-09-26 04:23:51 +0000

    Thu, Sep 26, 2024, 9:53 AM +05:30

--- CS50p/pset7/um/um.py
+++ CS50p/pset7/um/um.py
@@ -14,12 +14,12 @@ def count(s):
     if match := re.findall(pattern, s, re.IGNORECASE):
         count = 0
         print(match)
-        for m in match.groups():
-            if m:
-                count += 1
-        return count
-    else:
-        return 0
+        #for m in match.groups():
+            #if m:
+                #count += 1
+        #return count
+    #else:
+        #return 0
 
 
 if __name__ == "__main__":
