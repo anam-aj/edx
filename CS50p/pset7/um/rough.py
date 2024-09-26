8c48d2c97fb89cc2cd7cc5581df1fd78ea370706
anam-aj <anamaj.786@gmail.com>
2024-09-26 06:16:23 +0000

    Thu, Sep 26, 2024, 11:46 AM +05:30

--- CS50p/pset7/um/rough.py
+++ CS50p/pset7/um/rough.py
@@ -5,7 +5,7 @@ import sys
 def main():
 
     text = "abc cd ab ef ef gh gh"
-    pattern = r"(abc )|( cb)|(ef)|(gh)"
+    pattern = r"(abc )|(^cd)|(ef)|(gh)"
 
     if match := re.findall(pattern, text, re.IGNORECASE):
         print(match)
