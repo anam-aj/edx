d56056934282aa374801e0a368bbf202c6c9a987
anam-aj <anamaj.786@gmail.com>
2024-09-26 05:26:31 +0000

    Thu, Sep 26, 2024, 10:56 AM +05:30

--- CS50p/pset7/um/rough.py
+++ CS50p/pset7/um/rough.py
@@ -6,15 +6,9 @@ def main():
 
     text = "ab ab cd cd ef ef gh gh"
     pattern = r"(ab)|(cd)|(ef)|(gh)"
-    if match := re.findall(pattern, s, re.IGNORECASE):
-        count = 0
+
+    if match := re.findall(pattern, text, re.IGNORECASE):
         print(match)
-        #for m in match.groups():
-            #if m:
-                #count += 1
-        #return count
-    #else:
-        #return 0
 
 
 if __name__ == "__main__":
