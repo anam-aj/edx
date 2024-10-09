ce0e2c708ff4c83ce5adf48fb225ab1e5fb3785a
anam-aj <anamaj.786@gmail.com>
2024-10-09 15:58:41 +0000

    Wed, Oct 9, 2024, 9:28 PM +05:30

--- CS50p/pset8/seasons/seasons.py
+++ CS50p/pset8/seasons/seasons.py
@@ -1,15 +1,19 @@
 # Program to tell age in minutes
 
+import re
+
 from datetime import date
+
 
 
 def main():
 
     # Promt user for birthdate
     birthdate = input("Enter birthdate: ")
-    
 
-...
+    # Ensure birthdate is in YYYY-MM-DD format
+    pattern = r"^\d{4}-\d{2}-\d{2}$"
+
 
 
 if __name__ == "__main__":
