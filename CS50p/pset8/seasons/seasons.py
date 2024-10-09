c086e4fbaa1e773ab541931fb2496cc488c6da05
anam-aj <anamaj.786@gmail.com>
2024-10-09 16:16:52 +0000

    Wed, Oct 9, 2024, 9:46 PM +05:30

--- CS50p/pset8/seasons/seasons.py
+++ CS50p/pset8/seasons/seasons.py
@@ -16,12 +16,13 @@ def main():
     pattern = r"^\d{4}-\d{2}-\d{2}$"
     if not re.search(pattern, birthdate):
         sys.exit("birthdate format: YYYY-MM-DD")
+
     year, month, day = birthdate.split("-")
     birth_date = date(int(year), int(month), int(day))
     current_date = date.today()
 
-    age = __sub__(birth_date)
-
+    age = __sub__(birth_date, current_date)
+    total_minutes = (age.days * 24 * 60) + (age.seconds // 60)
 
 
 if __name__ == "__main__":
