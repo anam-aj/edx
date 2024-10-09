ad6e922bea28354d2da3e3486a027caa681de58e
anam-aj <anamaj.786@gmail.com>
2024-10-09 16:52:38 +0000

    Wed, Oct 9, 2024, 10:22 PM +05:30

--- CS50p/pset8/seasons/seasons.py
+++ CS50p/pset8/seasons/seasons.py
@@ -10,20 +10,7 @@ from datetime import date
 
 def main():
 
-    # Promt user for birthdate
-    birthdate = input("Enter birthdate: ")
 
-    # Ensure birthdate is in YYYY-MM-DD format
-    pattern = r"^\d{4}-\d{2}-\d{2}$"
-    if not re.search(pattern, birthdate):
-        sys.exit("Invalid date, correct format: YYYY-MM-DD")
-
-    # Ensure birth date is valid and gets date object of bith date
-    year, month, day = birthdate.split("-")
-    try:
-        birth_date = date(int(year), int(month), int(day))
-    except ValueError:
-        sys.exit("Invalid date")
 
     # Fetch current date
     current_date = date.today()
@@ -35,6 +22,25 @@ def main():
     age = p.number_to_words(total_minutes)
 
     print(f"{age.capitalize()} minutes")
+
+
+# Functions to get valid date object
+def get_date(text):
+
+    # Promt user for date
+    date = input(text)
+
+    # Ensure birthdate is in YYYY-MM-DD format
+    pattern = r"^\d{4}-\d{2}-\d{2}$"
+    if not re.search(pattern, date):
+        sys.exit("Invalid date, correct format: YYYY-MM-DD")
+
+    # Ensure birth date is valid and gets date object of bith date
+    year, month, day = date.split("-")
+    try:
+        birth_date = date(int(year), int(month), int(day))
+    except ValueError:
+        sys.exit("Invalid date")
 
 
 if __name__ == "__main__":
