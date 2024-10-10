e203393a712b8ec090842877aa9b8a68e4f60665
anam-aj <anamaj.786@gmail.com>
2024-10-10 14:42:23 +0000

    Thu, Oct 10, 2024, 8:12 PM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -1,5 +1,13 @@
-a = "   "
-print(a.rstrip())
-a = a.rstrip()
-print(len(a))
-print(bool(a))
+import random
+
+
+class Hat:
+    def __init__(self):
+        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+    def sort(self, name):
+        print(name, "is in", random.choice(self.houses))
+
+
+hat = Hat()
+hat.sort("Harry")
