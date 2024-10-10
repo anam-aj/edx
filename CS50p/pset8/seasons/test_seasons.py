6527e79f51cffd20b19498de5d80a3a2b19a8e1d
anam-aj <anamaj.786@gmail.com>
2024-10-10 05:41:36 +0000

    Thu, Oct 10, 2024, 11:11 AM +05:30

--- CS50p/pset8/seasons/test_seasons.py
+++ CS50p/pset8/seasons/test_seasons.py
@@ -9,9 +9,11 @@ def test_correct_date_format():
 def test_incorrect_date_format():
     assert check_date_format("2010-1-01") == False
     assert check_date_format("2010-01-1") == False
-    assert check_date_format("2010-01-0") == False
+    assert check_date_format("2010-1-01") == False
     assert check_date_format("1 January, 2010") == False
 
 
 def test_date_object():
-    
+    assert date_object("1 January, 2010") == False
+    assert date_object("2010-13-01") == False
+    assert date_object("2010-01-40") == False
