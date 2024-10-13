67a899a9ef31f3c15d2016758449d9965d1d1a27
anam-aj <anamaj.786@gmail.com>
2024-10-13 06:46:08 +0000

    Sun, Oct 13, 2024, 12:16 PM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -1,14 +1,6 @@
-import random
+def main():
 
+    print(cookie())
 
-class Hat:
-    def __init__(self):
-        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
-
-    def sort(self, name):
-        print(name, "is in", random.choice(self.houses))
-        return "hello"
-
-hat = Hat()
-word = hat.sort("Harry")
-print(word)
+def cookie():
+    return ("🍪" * 5)
