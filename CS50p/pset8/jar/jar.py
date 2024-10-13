2e3f9a83dcc1f12bc4fd68d80660431f46b5ddaf
anam-aj <anamaj.786@gmail.com>
2024-10-13 06:14:31 +0000

    Sun, Oct 13, 2024, 11:44 AM +05:30

--- CS50p/pset8/jar/jar.py
+++ CS50p/pset8/jar/jar.py
@@ -1,6 +1,6 @@
 class Jar:
     def __init__(self, capacity=12):
-        if not (capacity > 0) or not (capacity % 1 == 0):
+        if not (int(capacity) > 0) or not (int(capacity) % 1 == 0):
             raise ValueError
         self.capacity = capacity
 
