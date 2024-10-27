4ddf2388c48d02cd017322367afbc9fc761202ce
anam-aj <anamaj.786@gmail.com>
2024-10-27 08:40:10 +0000

    Sun, Oct 27, 2024, 2:10 PM +05:30

--- CS50p/pset8/jar/jar.py
+++ CS50p/pset8/jar/jar.py
@@ -41,10 +41,10 @@ def main():
 
     my_jar = Jar(15)
     print(my_jar)
-    my_jar.deposit(3)
-    print(my_jar)
-    my_jar.withdraw(2)
-    print(my_jar)
+    #my_jar.deposit(3)
+    #print(my_jar)
+    #my_jar.withdraw(2)
+    #print(my_jar)
     #my_jar.deposit(5)
     #print(my_jar)
 
