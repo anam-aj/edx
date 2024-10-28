5eb426791ad9cf93c123896626316cdb9a17426b
anam-aj <anamaj.786@gmail.com>
2024-10-28 04:36:28 +0000

    Mon, Oct 28, 2024, 10:06 AM +05:30

--- CS50p/pset8/shirtificate.py
+++ CS50p/pset8/shirtificate.py
@@ -2,6 +2,20 @@
 
 from fpdf import FPDF
 
+
+class PDF(FPDF):
+    def header(self):
+        # Rendering logo:
+        self.image("../docs/fpdf2-logo.png", 10, 8, 33)
+        # Setting font: helvetica bold 15
+        self.set_font("helvetica", "B", 15)
+        # Moving cursor to the right:
+        self.cell(80)
+        # Printing title:
+        self.cell(30, 10, "Title", border=1, align="C")
+        # Performing a line break:
+        self.ln(20)
+
 def main():
     name  = input("Enter your name: ")
 
