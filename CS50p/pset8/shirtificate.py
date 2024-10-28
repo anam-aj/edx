d55f230f2549a912710958e90c7fdf5272abaa3f
anam-aj <anamaj.786@gmail.com>
2024-10-28 04:54:28 +0000

    Mon, Oct 28, 2024, 10:24 AM +05:30

--- CS50p/pset8/shirtificate.py
+++ CS50p/pset8/shirtificate.py
@@ -14,13 +14,12 @@ def create_shirtificate(user_text):
     # Create object
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_margin(0)
     # Rendering image:
     pdf.image("shirtificate.png")
     # Setting font: helvetica bold 15
     pdf.set_font("helvetica", "B", 15)
     # Add name and msg
-    pdf.cell(0, 10, f"{user_text} took CS50", align="C")
+    pdf.cell(0, 10, f"{user_text} took CS50", h=-10, align="C")
     # Generate output
     pdf.output("shirtificate.pdf")
 
