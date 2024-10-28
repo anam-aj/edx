5585b920583a4781e28ded469515a52ba9f5f32d
anam-aj <anamaj.786@gmail.com>
2024-10-28 04:20:24 +0000

    Mon, Oct 28, 2024, 9:50 AM +05:30

--- CS50p/pset8/shirtificate.py
+++ CS50p/pset8/shirtificate.py
@@ -12,7 +12,7 @@ def create_shirtificate(name):
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("helvetica", "B", 16)
-    pdf.cell(40, 10, name)
+    pdf.cell(0, 10, name, 0, 1, 'C')
     pdf.output("shirtificate.pdf")
 
 
