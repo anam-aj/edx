cb816fcfb51d2ffff69db5091b5e6b1b9da16b07
anam-aj <anamaj.786@gmail.com>
2024-10-28 07:20:00 +0000

    Mon, Oct 28, 2024, 12:50 PM +05:30

--- CS50p/pset8/shirtificate.py
+++ CS50p/pset8/shirtificate.py
@@ -4,12 +4,12 @@ from fpdf import FPDF
 
 
 def main():
-    #name  = input("Enter your name: ")
+    name  = input("Enter your name: ")
 
-    create_shirtificate()
+    create_shirtificate(name)
 
 
-def create_shirtificate():
+def create_shirtificate(user_text):
 
     # Create object
     pdf = FPDF()
@@ -17,12 +17,12 @@ def create_shirtificate():
     # Render image:
     pdf.image("shirtificate.png", x=10, y=60, w=190)
     # Header text
-    pdf.set_font("Courier", "B", 40)
+    pdf.set_font("Arial", "B", 50)
     pdf.cell(0, 30, "CS50 Shirtificate",0 , 1, "C")
     # Shirt text
     pdf.set_text_color(255, 255, 255)
-    pdf.set_font("Courier", "B", 30)
-    pdf.cell(0, 140, "anam took CS50",0 , 1, "C")
+    pdf.set_font("Arial", "B", 30)
+    pdf.cell(0, 150, f"{user_text} took CS50",0 , 1, "C")
     # Output file
     pdf.output("shirtificate.pdf")
 
