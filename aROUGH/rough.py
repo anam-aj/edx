da5bbf8502a2ac38f69378379c180de88a24a6dd
anam-aj <anamaj.786@gmail.com>
2024-11-01 17:07:18 +0000

    Fri, Nov 1, 2024, 10:37 PM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -1,10 +1,11 @@
-from termcolor import colored
 import textwrap
-
-# Colorize your text
-colored_text = colored("Happy Halloween!", "red")
+from termcolor import colored
 
 # Wrap your text
-wrapped_text = textwrap.fill(colored_text, width=20)
-
+wrapped_text = textwrap.fill("Happy Halloweenasdasdasasdadasdasdadaddadadadadadad!", width=20)
 print(wrapped_text)
+
+# Colorize your text
+colored_text = colored(wrapped_text, "red")
+
+print(colored_text)
