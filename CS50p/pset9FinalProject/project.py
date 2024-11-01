dee4965f2f1820f152ee50ce6c7c1890bad44d29
anam-aj <anamaj.786@gmail.com>
2024-11-01 17:18:28 +0000

    Fri, Nov 1, 2024, 10:48 PM +05:30

--- CS50p/pset9FinalProject/project.py
+++ CS50p/pset9FinalProject/project.py
@@ -1,5 +1,10 @@
 # Program to implement To-Do List
 
+
+import textwrap
+
+
+from termcolor import colored
 from tabulate import tabulate
 
 
@@ -110,5 +115,34 @@ def fetch_list(list_object):
 
     return tasks
 
+
+def wrap_text(text):
+
+
+# Wrap your text
+wrapped_text = textwrap.fill("Happy Halloween!", width=20)
+
+# Split the wrapped text into lines
+lines = wrapped_text.split('\n')
+
+# Create the top border
+top_border = "+" + "-" * 22 + "+"
+
+# Create the bottom border
+bottom_border = top_border
+
+# Add side borders to each line
+boxed_text = [top_border]
+for line in lines:
+    boxed_text.append("| " + line.ljust(20) + " |")
+boxed_text.append(bottom_border)
+
+# Join the boxed text into a single string
+boxed_text = "\n".join(boxed_text)
+
+# Colorize your text
+colored_text = colored(boxed_text, "red")
+
+print(colored_text)
 
 main()
