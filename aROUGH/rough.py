cddf2c49b5d41c5e012bc6f29e9f573216cc75fc
anam-aj <anamaj.786@gmail.com>
2024-11-01 17:30:57 +0000

    Fri, Nov 1, 2024, 11:00 PM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -1,29 +1,9 @@
-import textwrap
-from termcolor import colored
+from tabulate import tabulate
 
-# Wrap your text
-wrapped_text = textwrap.fill("Haaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa !", width=20)
-print(wrapped_text)
+data = [
+    ["\033[91mRed\033[0m", "Apple"],
+    ["\033[92mGreen\033[0m", "Lime"],
+    ["\033[94mBlue\033[0m", "Berry"]
+]
 
-# Split the wrapped text into lines
-lines = wrapped_text.split('\n')
-
-# Create the top border
-top_border = "+" + "-" * 22 + "+"
-
-# Create the bottom border
-bottom_border = top_border
-
-# Add side borders to each line
-boxed_text = [top_border]
-for line in lines:
-    boxed_text.append("| " + line.ljust(20) + " |")
-boxed_text.append(bottom_border)
-
-# Join the boxed text into a single string
-boxed_text = "\n".join(boxed_text)
-
-# Colorize your text
-colored_text = colored(boxed_text, "red")
-
-return colored_text)
+print(tabulate(data, headers=["Color", "Fruit"]))
