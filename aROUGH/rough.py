b79c25bad6f85ecf846c08190cf1d04cc4ab55c5
anam-aj <anamaj.786@gmail.com>
2024-11-01 17:09:16 +0000

    Fri, Nov 1, 2024, 10:39 PM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -2,10 +2,27 @@ import textwrap
 from termcolor import colored
 
 # Wrap your text
-wrapped_text = textwrap.fill("Happy Halloweenasdasdasasdadasdasdadaddadadadadadad!", width=20)
-print(wrapped_text)
+wrapped_text = textwrap.fill("Happy Halloween!", width=20)
+
+# Split the wrapped text into lines
+lines = wrapped_text.split('\n')
+
+# Create the top border
+top_border = "+" + "-" * 22 + "+"
+
+# Create the bottom border
+bottom_border = top_border
+
+# Add side borders to each line
+boxed_text = [top_border]
+for line in lines:
+    boxed_text.append("| " + line.ljust(20) + " |")
+boxed_text.append(bottom_border)
+
+# Join the boxed text into a single string
+boxed_text = "\n".join(boxed_text)
 
 # Colorize your text
-colored_text = colored(wrapped_text, "red")
+colored_text = colored(boxed_text, "red")
 
 print(colored_text)
