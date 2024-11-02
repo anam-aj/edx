039670471d80e22dc30d89d238455c833e994f3b
anam-aj <anamaj.786@gmail.com>
2024-11-02 17:07:28 +0000

    Sat, Nov 2, 2024, 10:37 PM +05:30

--- CS50p/pset9FinalProject/test_project.py
+++ CS50p/pset9FinalProject/test_project.py
@@ -1,1 +1,12 @@
-from project import Task, ToDoList, 
+import pytest
+
+
+from project import Task, ToDoList,menu
+
+
+def test_menu(capsys):
+    menu()
+    captured = capsys.readouterr()
+    assert "Select the option number from below" in captured.out
+    assert "1: View Tasks" in captured.out
+    assert "5: Save list and Exit program" in captured.out
