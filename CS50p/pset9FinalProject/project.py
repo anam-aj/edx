fa9b927a1522a15bc060a4385b0af6a83ac02bcd
anam-aj <anamaj.786@gmail.com>
2024-11-03 05:10:40 +0000

    Sun, Nov 3, 2024, 10:40 AM +05:30

--- CS50p/pset9FinalProject/project.py
+++ CS50p/pset9FinalProject/project.py
@@ -49,7 +49,7 @@ def main():
                     tasks_list, headers=["No.", "Task", "Status"]
                 )
                 colored_list = colored(tabulated_task_list, "cyan")
-                print(f"----TO DO LIST----\n{colored_list}\n")
+                print(f"\n{'-' * 8}TO DO LIST{'-' * 8}\n{colored_list}\n")
             else:
                 print(wrap_text("To-Do list is Empty\n", "red"))
 
