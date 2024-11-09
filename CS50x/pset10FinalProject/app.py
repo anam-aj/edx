29b99ae66d0fd0b18f5c3ca2b07ba73be3f207c2
anam-aj <anamaj.786@gmail.com>
2024-11-09 06:57:53 +0000

    Sat, Nov 9, 2024, 12:27 PM +05:30

--- CS50x/pset10FinalProject/app.py
+++ CS50x/pset10FinalProject/app.py
@@ -44,8 +44,7 @@ def login():
         # Ensure username was submitted
         if not request.form.get("username"):
             flash("Username required")
-            return redirect("/login")
-            #return render_template("login.html")
+            return render_template("login.html")
         # Ensure password was submitted
         elif not request.form.get("password"):
             flash("password required")
