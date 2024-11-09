68ccec50b970984990a91cce8aac96d6a34dd3ec
anam-aj <anamaj.786@gmail.com>
2024-11-09 16:39:09 +0000

    Sat, Nov 9, 2024, 10:09 PM +05:30

--- CS50x/pset10FinalProject/app.py
+++ CS50x/pset10FinalProject/app.py
@@ -5,7 +5,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import lookup, apology, login_required, open_user_notes, open_user_notes, save_notes
+from helpers import login_required, open_user_notes, open_user_notes, save_notes
 
 # Configure application
 app = Flask(__name__)
