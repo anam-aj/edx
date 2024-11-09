45ea767dfc9843ef8c89286dd6fc0c6ddfadad23
anam-aj <anamaj.786@gmail.com>
2024-11-09 11:55:51 +0000

    Sat, Nov 9, 2024, 5:25 PM +05:30

--- CS50x/pset10FinalProject/app.py
+++ CS50x/pset10FinalProject/app.py
@@ -5,13 +5,13 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, open_user_notes, open_user_notes, save_notes
+from helpers import lookup, apology, login_required, open_user_notes, open_user_notes, save_notes
 
 # Configure application
 app = Flask(__name__)
 
 # Custom filter
-app.jinja_env.filters["usd"] = usd
+#app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
