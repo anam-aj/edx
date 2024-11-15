bab2c7a2fc79776b44e800579b18d3e0309c088a
anam-aj <anamaj.786@gmail.com>
2024-11-15 14:25:37 +0000

    Fri, Nov 15, 2024, 7:55 PM +05:30

--- CS50sql/pset0/players/10.sql
+++ CS50sql/pset0/players/10.sql
@@ -1,8 +1,5 @@
-SELECT height
+SELECT height AS "Top 10 Tallest Players after 2000"
 FROM players
-
+WHERE (debut > 2000-01-01
 ORDER BY height
 LIMIT 10;
-WHERE debut > 2000-01-01
-
-AS "Top 10 Tallest Players after 2000"
