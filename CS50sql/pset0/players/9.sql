08fc27952a6eb62822c4225ba0ffcb3c8fe4659d
anam-aj <anamaj.786@gmail.com>
2024-11-15 14:11:57 +0000

    Fri, Nov 15, 2024, 7:41 PM +05:30

--- CS50sql/pset0/players/9.sql
+++ CS50sql/pset0/players/9.sql
@@ -1,5 +1,4 @@
 SELECT first_name, last_name
 FROM players
-WHERE last_game
-AND birth_state = 'PA'
-ORDER BY debut DESC, first_name, last_name;
+WHERE final_game = 
+ORDER BY first_name, last_name;
