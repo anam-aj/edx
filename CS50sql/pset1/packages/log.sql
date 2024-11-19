24f252738a4daad83616b08df833adebee1b876a
anam-aj <anamaj.786@gmail.com>
2024-11-19 12:11:29 +0000

    Tue, Nov 19, 2024, 5:41 PM +05:30

--- CS50sql/pset1/packages/log.sql
+++ CS50sql/pset1/packages/log.sql
@@ -1,6 +1,13 @@
 
 -- *** The Lost Letter ***
-SELECT id 
+SELECT id
+FROM packages
+WHERE from_address_id = (
+    SELECT id
+    FROM addresses
+    WHERE address = '900 Somerville Avenue'
+)
+
 
 -- *** The Devious Delivery ***
 
