b03a11bdd55ac83cc27d50a1907d37a0c503ea58
anam-aj <anamaj.786@gmail.com>
2024-11-19 16:07:52 +0000

    Tue, Nov 19, 2024, 9:37 PM +05:30

--- CS50sql/pset1/dese/4.sql
+++ CS50sql/pset1/dese/4.sql
@@ -1,4 +1,4 @@
-SELECT city, count("name") AS "Number of Schools"
+SELECT city, count(*) AS "Number of Schools"
 FROM schools
 GROUP BY city
 ORDER BY "Number of Schools" DESC, city
--- CS50sql/pset1/dese/5.sql
+++ CS50sql/pset1/dese/5.sql
@@ -1,6 +1,5 @@
-SELECT city, COUNT("name") AS "Number of Schools"
+SELECT city, COUNT(*) AS "Number of Schools"
 FROM schools
 GROUP BY city
 HAVING "Number of Schools" < 4
 ORDER BY "Number of Schools" DESC, city
-
