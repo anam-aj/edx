38248ff59eff4f9d5ff9f37273128fa844c72249
anam-aj <anamaj.786@gmail.com>
2024-11-19 17:38:50 +0000

    Tue, Nov 19, 2024, 11:08 PM +05:30

--- CS50sql/pset1/dese/9.sql
+++ CS50sql/pset1/dese/9.sql
@@ -1,9 +1,7 @@
 SELECT name
 FROM districts
 JOIN expenditures ON districts.id = expenditures.district_id
-WHERE pupils = MIN("pupils")
-
-/*IN (
+WHERE pupils IN (
     SELECT MIN("pupils")
     FROM expenditures
 )
