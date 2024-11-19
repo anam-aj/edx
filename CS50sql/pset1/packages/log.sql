d8f232e4cd08370c7c445f1e4de5eaf09e32f145
anam-aj <anamaj.786@gmail.com>
2024-11-19 12:13:09 +0000

    Tue, Nov 19, 2024, 5:43 PM +05:30

--- CS50sql/pset1/packages/log.sql
+++ CS50sql/pset1/packages/log.sql
@@ -3,9 +3,17 @@
 SELECT id
 FROM packages
 WHERE from_address_id = (
+    (
     SELECT id
     FROM addresses
     WHERE address = '900 Somerville Avenue'
+    )
+    AND
+    (
+    SELECT id
+    FROM addresses
+    WHERE address LIKE '900 Somerville Avenue'
+    )
 )
 
 
