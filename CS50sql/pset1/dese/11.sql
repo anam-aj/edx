1542ef1b4d3d9470bec5691796e4482717a5e5c3
anam-aj <anamaj.786@gmail.com>
2024-11-19 18:28:58 +0000

    Tue, Nov 19, 2024, 11:58 PM +05:30

--- CS50sql/pset1/dese/11.sql
+++ CS50sql/pset1/dese/11.sql
@@ -1,10 +1,4 @@
-SELECT name
+SELECT name, per_pupil_expenditure, graduated AS graduation_rate
 FROM schools
 JOIN graduation rates ON schools.id = graduation_rates.school_id
-JOIN
-
-SELECT name, per_pupil_expenditure
-FROM districts
-JOIN expenditures ON districts.id = expenditures.district_id
-ORDER BY per_pupil_expenditure DESC
-LIMIT 10
+JOIN ependitures ON schools.district_id = expenditures.district_id
