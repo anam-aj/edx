d27d3d7e5cb3abdc9cb9f9e4d9b68b6cf534a063
anam-aj <anamaj.786@gmail.com>
2024-11-20 16:58:25 +0000

    Wed, Nov 20, 2024, 10:28 PM +05:30

--- CS50sql/pset1/moneyball/2.sql
+++ CS50sql/pset1/moneyball/2.sql
@@ -1,8 +1,9 @@
 SELECT year, salary
 FROM salaries
 WHERE player_id = (
-    SELECT id, first_name, last_name
+    SELECT id
     FROM players
-    WHERE first_name LIKE 'Cal%'
-    AND last_name LIKE '%Ripken Jr.'
+    WHERE first_name = 'Cal'
+    AND last_name = 'Ripken'
 )
+ORDER BY year DESC
