ad4b09e7d14cede1d12857e5a9b21e6e0123b258
anam-aj <anamaj.786@gmail.com>
2024-11-20 15:40:50 +0000

    Wed, Nov 20, 2024, 9:10 PM +05:30

--- CS50sql/pset1/dese/12.sql
+++ CS50sql/pset1/dese/12.sql
@@ -3,3 +3,6 @@ FROM districts
 JOIN expenditures ON districts.id = expenditures.district_id
 JOIN staff_evaluations ON districts.id = staff_evaluations.district_id
 ORDER BY exemplary DESC, per_pupil_expenditure DESC
+
+SELECT AVG(per_pupil_expenditure)
+FROM expenditures
