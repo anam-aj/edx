df3be629636423488de23ba74485e180ec4a0594
anam-aj <anamaj.786@gmail.com>
2024-11-21 16:02:27 +0000

    Thu, Nov 21, 2024, 9:32 PM +05:30

--- CS50sql/pset1/moneyball/12.sql
+++ CS50sql/pset1/moneyball/12.sql
@@ -1,26 +1,21 @@
-
-    SELECT first_name, last_name
-    FROM players
-    JOIN salaries ON players.id = salaries.player_id
-    JOIN performances ON players.id = performances.player_id
-    WHERE salaries.year = performances.year
-    AND salaries.year = 2001
-    AND H > 0
-    AND salary >= 0
-    ORDER BY ("salary" / "H"), first_name, last_name
-    LIMIT 10
-    INTERSECT
-    SELECT first_name, last_name, ("salary" / "RBI") AS "RBI per hit"
-    FROM players
-    JOIN salaries ON players.id = salaries.player_id
-    JOIN performances ON players.id = performances.player_id
-    WHERE salaries.year = performances.year
-    AND salaries.year = 2001
-    AND H > 0
-    AND salary >= 0
-    ORDER BY "dollars per hit", first_name, last_name
-    LIMIT 10
-
-
-
-
+SELECT first_name, last_name
+FROM players
+JOIN salaries ON players.id = salaries.player_id
+JOIN performances ON players.id = performances.player_id
+WHERE salaries.year = performances.year
+AND salaries.year = 2001
+AND H > 0
+AND salary >= 0
+ORDER BY ("salary" / "H")
+LIMIT 10
+INTERSECT
+SELECT first_name, last_name
+FROM players
+JOIN salaries ON players.id = salaries.player_id
+JOIN performances ON players.id = performances.player_id
+WHERE salaries.year = performances.year
+AND salaries.year = 2001
+AND H > 0
+AND salary >= 0
+ORDER BY ("salary" / "RBI")
+LIMIT 10
