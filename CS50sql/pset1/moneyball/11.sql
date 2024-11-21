bcde3daa0c175d622ffade9a07d0dc757f2e1d15
anam-aj <anamaj.786@gmail.com>
2024-11-21 15:40:44 +0000

    Thu, Nov 21, 2024, 9:10 PM +05:30

--- CS50sql/pset1/moneyball/11.sql
+++ CS50sql/pset1/moneyball/11.sql
@@ -1,4 +1,4 @@
-SELECT first_name, last_name, (salary / H) AS "dollars per hit"
+SELECT first_name, last_name, ("salary" / "H") AS "dollars per hit"
 FROM players
 JOIN salaries ON players.id = salaries.player_id
 JOIN performances ON players.id = performances.player_id
