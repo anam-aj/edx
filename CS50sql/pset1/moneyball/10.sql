43f9dcb7492ec51df50726731a2935f91f76a63e
anam-aj <anamaj.786@gmail.com>
2024-11-21 13:48:42 +0000

    Thu, Nov 21, 2024, 7:18 PM +05:30

--- CS50sql/pset1/moneyball/10.sql
+++ CS50sql/pset1/moneyball/10.sql
@@ -1,1 +1,4 @@
-SELECT first_name, last_name, salary, HR, 
+SELECT first_name, last_name, salary, HR, salaries.year
+FROM players
+JOIN salaries ON players.id = salaries.player_id
+JOIN performances ON players.id = performances.player_id
