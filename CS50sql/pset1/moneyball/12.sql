7615cebc9ec2fea443c373dcfb5d4dc92647995e
anam-aj <anamaj.786@gmail.com>
2024-11-21 16:15:23 +0000

    Thu, Nov 21, 2024, 9:45 PM +05:30

--- CS50sql/pset1/moneyball/12.sql
+++ CS50sql/pset1/moneyball/12.sql
@@ -1,23 +1,26 @@
 SELECT first_name, last_name
-FROM players
-JOIN salaries ON players.id = salaries.player_id
-JOIN performances ON players.id = performances.player_id
-WHERE salaries.year = performances.year
-AND salaries.year = 2001
-AND H > 0
-AND salary >= 0
-ORDER BY ("salary" / "H")
-LIMIT 10
+FROM (
+    SELECT first_name, last_name
+    FROM players
+    JOIN salaries ON players.id = salaries.player_id
+    JOIN performances ON players.id = performances.player_id
+    WHERE salaries.year = performances.year
+    AND salaries.year = 2001
+    AND H > 0
+    AND salary >= 0
+    ORDER BY ("salary" / "H")
+    LIMIT 10
+) AS lowest_hit_price
 
 INTERSECT
 
-SELECT first_name, last_name
-FROM players
-JOIN salaries ON players.id = salaries.player_id
-JOIN performances ON players.id = performances.player_id
-WHERE salaries.year = performances.year
-AND salaries.year = 2001
-AND RBI > 0
-AND salary >= 0
-ORDER BY ("salary" / "RBI")
-LIMIT 10
+    SELECT first_name, last_name
+    FROM players
+    JOIN salaries ON players.id = salaries.player_id
+    JOIN performances ON players.id = performances.player_id
+    WHERE salaries.year = performances.year
+    AND salaries.year = 2001
+    AND RBI > 0
+    AND salary >= 0
+    ORDER BY ("salary" / "RBI")
+    LIMIT 10
