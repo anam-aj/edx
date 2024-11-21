8520e613a5023dd21f28e27b150b841e2e02ff19
anam-aj <anamaj.786@gmail.com>
2024-11-21 12:50:19 +0000

    Thu, Nov 21, 2024, 6:20 PM +05:30

--- CS50sql/pset1/moneyball/6.sql
+++ CS50sql/pset1/moneyball/6.sql
@@ -2,4 +2,6 @@ SELECT name, SUM(H) AS "total hits"
 FROM teams
 JOIN performances on teams.id = performances.team_id
 GROUP BY name
-WHERE year = 2001   
+WHERE year = 2001
+ORDER BY "total hits" DESC
+LIMIT 5
