be2a4dc57da9893e34c103ab200f898c35323ccb
anam-aj <anamaj.786@gmail.com>
2024-11-24 06:11:18 +0000

    Sun, Nov 24, 2024, 11:41 AM +05:30

--- CS50sql/pset2/atl/schema.sql
+++ CS50sql/pset2/atl/schema.sql
@@ -1,16 +1,9 @@
-/*CREATE TABLE passengers (
+-- Table for storing passengers data
+CREATE TABLE passengers (
     first_name,
     last_name,
     age
-);*/
-
-
-INSERT INTO passengers (
-    first_name,
-    last_name,
-    age)
-VALUES ('abc', 'xyz', 12);
-
+);
 
 
 
