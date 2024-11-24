42abd1fef7ef7c297cc0f310bd803984533d7d16
anam-aj <anamaj.786@gmail.com>
2024-11-24 07:01:50 +0000

    Sun, Nov 24, 2024, 12:31 PM +05:30

--- CS50sql/pset2/atl/schema.sql
+++ CS50sql/pset2/atl/schema.sql
@@ -11,6 +11,7 @@ CREATE TABLE check_in (
     id INTEGER
     passenger_id INTEGER,
     flight_id INTEGER,
-    age INTEGER NOT NULL,
     PRIMARY KEY(id)
+    FOREIGN KEY(passenger_id) REFERENCES passengers(id)
+    FOREIGN KEY(flight_id) REFERENCES flights(id)
 );
