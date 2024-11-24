641292237d0e8a2e75f501426cf8ee515f2497b0
anam-aj <anamaj.786@gmail.com>
2024-11-24 06:50:42 +0000

    Sun, Nov 24, 2024, 12:20 PM +05:30

--- CS50sql/pset2/atl/schema.sql
+++ CS50sql/pset2/atl/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE passengers (
     id INTEGER
-    first_name TEXT,
-    last_name TEXT,
-    age NUMERIC
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    age NUMERIC NOT NULL
     PRIMARY KEY (id)
 )
