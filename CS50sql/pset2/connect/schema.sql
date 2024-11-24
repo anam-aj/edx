036f52ecdb6bdcdbad58dafe13cb60dc5cce9640
anam-aj <anamaj.786@gmail.com>
2024-11-24 12:17:02 +0000

    Sun, Nov 24, 2024, 5:47 PM +05:30

--- CS50sql/pset2/connect/schema.sql
+++ CS50sql/pset2/connect/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id INTEGER,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    username TEXT NOT NULL,
+    username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
     PRIMARY KEY(id)
 );
