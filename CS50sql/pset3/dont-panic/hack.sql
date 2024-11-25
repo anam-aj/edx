6e47a22e915634a570c542a956c6fc5ced1bb269
anam-aj <anamaj.786@gmail.com>
2024-11-25 19:08:04 +0000

    Tue, Nov 26, 2024, 12:38 AM +05:30

--- CS50sql/pset3/dont-panic/hack.sql
+++ CS50sql/pset3/dont-panic/hack.sql
@@ -1,3 +1,4 @@
+-- Frame 'emily33'
 UPDATE users
 SET password = (
     SELECT password
@@ -6,9 +7,11 @@ SET password = (
 )
 WHERE username = 'admin';
 
+-- Change admin's password
 UPDATE users
 SET password = '982c0381c279d139fd221fce974916e7'
 WHERE username = 'admin';
 
+-- Deletes trace
 DELETE FROM user_logs
 WHERE new_password = '982c0381c279d139fd221fce974916e7';
