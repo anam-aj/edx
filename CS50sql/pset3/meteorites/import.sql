9c5f8632ff4375dfab61016843ea0eb35fe98ebb
anam-aj <anamaj.786@gmail.com>
2024-11-26 10:47:27 +0000

    Tue, Nov 26, 2024, 4:17 PM +05:30

--- CS50sql/pset3/meteorites/import.sql
+++ CS50sql/pset3/meteorites/import.sql
@@ -4,7 +4,11 @@
 -- Create new temporary table with proper id column
 CREATE TABLE temp2 (
     id INTEGER,
-    name TEXT ,
-
-
+    name TEXT DEFAULT NULL,
+    class TEXT DEFAULT NULL,
+    mass NUMERIC DEFAULT NULL,
+    discovery TEXT DEFAULT NULL,
+    year NUMERIC DEFAULT NULL,
+    lat NUMERIC DEFAULT NULL,
+    long NUMERIC DEFAULT NULL,
 )
