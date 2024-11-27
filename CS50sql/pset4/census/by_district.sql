6851fb785615c81f85aaffef4b1d683db1d58edb
anam-aj <anamaj.786@gmail.com>
2024-11-27 20:06:27 +0000

    Thu, Nov 28, 2024, 1:36 AM +05:30

--- CS50sql/pset4/census/by_district.sql
+++ CS50sql/pset4/census/by_district.sql
@@ -1,9 +1,10 @@
 CREATE VIEW "by_district" AS
 SELECT
-    "district"
+    "district",
     SUM("families") AS "families",
     SUM("households") AS "households",
     SUM("population") AS "population",
     SUM("male") AS "male",
     SUM("female") AS "female"
-FROM census;
+FROM census
+GROUP BY "district";
