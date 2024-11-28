802053d30f9d9fc912aaa308f73f4482c6fd2db3
anam-aj <anamaj.786@gmail.com>
2024-11-28 17:57:36 +0000

    Thu, Nov 28, 2024, 11:27 PM +05:30

--- CS50sql/pset4/bnb/frequently_reviewed.sql
+++ CS50sql/pset4/bnb/frequently_reviewed.sql
@@ -1,4 +1,4 @@
-CREATE VIEW available AS
+CREATE VIEW frequently_reviewed AS
 SELECT id, property_type, host_name, accomodates, date
 FROM listings
 JOIN availabilities ON listings.id = avialabilities.listing_id
