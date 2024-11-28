2a945c76759a217df5f8236fc4f330554806addd
anam-aj <anamaj.786@gmail.com>
2024-11-28 17:52:50 +0000

    Thu, Nov 28, 2024, 11:22 PM +05:30

--- CS50sql/pset4/bnb/available.sql
+++ CS50sql/pset4/bnb/available.sql
@@ -2,4 +2,4 @@ CREATE VIEW available AS
 SELECT id, property_type, host_name, accomodates, date
 FROM listings
 JOIN availabilities ON listings.id = avialabilities.listing_id
-WHERE available = ';
+WHERE available = 1;
