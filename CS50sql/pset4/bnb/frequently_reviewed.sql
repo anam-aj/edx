1ce7b0a3d5f1c6e72de29596f4d248d65a6404e6
anam-aj <anamaj.786@gmail.com>
2024-11-28 18:27:37 +0000

    Thu, Nov 28, 2024, 11:57 PM +05:30

--- CS50sql/pset4/bnb/frequently_reviewed.sql
+++ CS50sql/pset4/bnb/frequently_reviewed.sql
@@ -2,4 +2,4 @@ CREATE VIEW frequently_reviewed AS
 SELECT listings.id AS id, property_type, host_name, COUNT(*) AS review
 FROM listings
 JOIN reviews ON listings.id = reviews.listing_id
-WHERE available = 1;
+GROUP BY listin;
