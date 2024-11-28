39639dad5051e4950ab57930fd18397be3ae70c2
anam-aj <anamaj.786@gmail.com>
2024-11-28 12:08:20 +0000

    Thu, Nov 28, 2024, 5:38 PM +05:30

--- CS50sql/pset4/private/private.sql
+++ CS50sql/pset4/private/private.sql
@@ -1,69 +1,35 @@
-
-/*SELECT TRIM(substr("sentence", 98, 4)) AS "phrase"
+CREATE VIEW "temp message" AS
+SELECT "id", TRIM(substr("sentence", 98, 4)) AS "phrase"
 FROM "sentences"
 WHERE id = 14
 UNION
-SELECT TRIM(substr("sentence", 3, 5))
+SELECT "id", TRIM(substr("sentence", 3, 5))
 FROM "sentences"
 WHERE id = 114
 UNION
-SELECT TRIM(substr("sentence", 72, 9))
+SELECT "id", TRIM(substr("sentence", 72, 9))
 FROM "sentences"
 WHERE id = 618
 UNION
-SELECT TRIM(substr("sentence", 7, 3))
+SELECT "id", TRIM(substr("sentence", 7, 3))
 FROM "sentences"
 WHERE id = 630
 UNION
-SELECT TRIM(substr("sentence", 12, 5))
+SELECT "id", TRIM(substr("sentence", 12, 5))
 FROM "sentences"
 WHERE id = 932
 UNION
-SELECT TRIM(substr("sentence", 50, 7))
+SELECT "id", TRIM(substr("sentence", 50, 7))
 FROM "sentences"
 WHERE id = 2230
 UNION
-SELECT TRIM(substr("sentence", 44, 10))
+SELECT "id", TRIM(substr("sentence", 44, 10))
 FROM "sentences"
 WHERE id = 2346
 UNION
-SELECT TRIM(substr("sentence", 14, 5))
+SELECT "id", TRIM(substr("sentence", 14, 5))
 FROM "sentences"
-WHERE id = 3041
-ORDER BY "id";*/
-
-CREATE VIEW "message" AS
-    SELECT "id", TRIM(substr("sentence", 98, 4)) AS "phrase"
-    FROM "sentences"
-    WHERE id = 14
-    UNION
-    SELECT "id", TRIM(substr("sentence", 3, 5))
-    FROM "sentences"
-    WHERE id = 114
-    UNION
-    SELECT "id", TRIM(substr("sentence", 72, 9))
-    FROM "sentences"
-    WHERE id = 618
-    UNION
-    SELECT "id", TRIM(substr("sentence", 7, 3))
-    FROM "sentences"
-    WHERE id = 630
-    UNION
-    SELECT "id", TRIM(substr("sentence", 12, 5))
-    FROM "sentences"
-    WHERE id = 932
-    UNION
-    SELECT "id", TRIM(substr("sentence", 50, 7))
-    FROM "sentences"
-    WHERE id = 2230
-    UNION
-    SELECT "id", TRIM(substr("sentence", 44, 10))
-    FROM "sentences"
-    WHERE id = 2346
-    UNION
-    SELECT "id", TRIM(substr("sentence", 14, 5))
-    FROM "sentences"
-    WHERE id = 3041
+WHERE id = 3041;
 
 
 CREATE VIEW "message" AS
