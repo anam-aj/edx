fc3ec3ef36537b9f16fabccdecab87a8f51f40d3
anam-aj <anamaj.786@gmail.com>
2024-11-28 11:49:02 +0000

    Thu, Nov 28, 2024, 5:19 PM +05:30

--- CS50sql/pset4/private/private.sql
+++ CS50sql/pset4/private/private.sql
@@ -1,5 +1,5 @@
 
-SELECT TRIM(substr("sentence", 98, 4)) AS "phrase"
+/*SELECT TRIM(substr("sentence", 98, 4)) AS "phrase"
 FROM "sentences"
 WHERE id = 14
 UNION
@@ -30,39 +30,41 @@ UNION
 SELECT TRIM(substr("sentence", 14, 5))
 FROM "sentences"
 WHERE id = 3041
-ORDER BY "id";
+ORDER BY "id";*/
 
 WITH "message table" AS
-SELECT "id", TRIM(substr("sentence", 98, 4)) AS "phrase"
-FROM "sentences"
-WHERE id = 14
-UNION
-SELECT "id", TRIM(substr("sentence", 3, 5))
-FROM "sentences"
-WHERE id = 114
-UNION
-SELECT "id", TRIM(substr("sentence", 72, 9))
-FROM "sentences"
-WHERE id = 618
-UNION
-SELECT "id", TRIM(substr("sentence", 7, 3))
-FROM "sentences"
-WHERE id = 630
-UNION
-SELECT "id", TRIM(substr("sentence", 12, 5))
-FROM "sentences"
-WHERE id = 932
-UNION
-SELECT "id", TRIM(substr("sentence", 50, 7))
-FROM "sentences"
-WHERE id = 2230
-UNION
-SELECT "id", TRIM(substr("sentence", 44, 10))
-FROM "sentences"
-WHERE id = 2346
-UNION
-SELECT "id", TRIM(substr("sentence", 14, 5))
-FROM "sentences"
-WHERE id = 3041;
+    SELECT "id", TRIM(substr("sentence", 98, 4)) AS "phrase"
+    FROM "sentences"
+    WHERE id = 14
+    UNION
+    SELECT "id", TRIM(substr("sentence", 3, 5))
+    FROM "sentences"
+    WHERE id = 114
+    UNION
+    SELECT "id", TRIM(substr("sentence", 72, 9))
+    FROM "sentences"
+    WHERE id = 618
+    UNION
+    SELECT "id", TRIM(substr("sentence", 7, 3))
+    FROM "sentences"
+    WHERE id = 630
+    UNION
+    SELECT "id", TRIM(substr("sentence", 12, 5))
+    FROM "sentences"
+    WHERE id = 932
+    UNION
+    SELECT "id", TRIM(substr("sentence", 50, 7))
+    FROM "sentences"
+    WHERE id = 2230
+    UNION
+    SELECT "id", TRIM(substr("sentence", 44, 10))
+    FROM "sentences"
+    WHERE id = 2346
+    UNION
+    SELECT "id", TRIM(substr("sentence", 14, 5))
+    FROM "sentences"
+    WHERE id = 3041
 CREATE VIEW "message" AS
-
+SELECT "phrase"
+FROM "message table"
+ORDER BY "id";
