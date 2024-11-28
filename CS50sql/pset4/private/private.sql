4c7ce8d7dff1a44c522164b79d2349c7c7c0d9a4
anam-aj <anamaj.786@gmail.com>
2024-11-28 08:20:35 +0000

    Thu, Nov 28, 2024, 1:50 PM +05:30

--- CS50sql/pset4/private/private.sql
+++ CS50sql/pset4/private/private.sql
@@ -1,4 +1,25 @@
 CREATE VIEW "message" AS
 SELECT substr("sentence", 98, 4)
 FROM "sentences"
-WHERE id = 14;
+WHERE id = 14
+UNION
+SELECT substr("sentence", 3, 5)
+FROM "sentences"
+WHERE id = 114
+UNION
+SELECT substr("sentence", 98, 4)
+FROM "sentences"
+WHERE id = 14
+UNION
+SELECT substr("sentence", 98, 4)
+FROM "sentences"
+WHERE id = 14
+UNION
+SELECT substr("sentence", 98, 4)
+FROM "sentences"
+WHERE id = 14
+UNION
+SELECT substr("sentence", 98, 4)
+FROM "sentences"
+WHERE id = 14
+UNION;
