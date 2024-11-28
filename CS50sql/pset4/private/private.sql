6c05188acb0cbb047dc801df7d7c469125d9037e
anam-aj <anamaj.786@gmail.com>
2024-11-28 08:26:39 +0000

    Thu, Nov 28, 2024, 1:56 PM +05:30

--- CS50sql/pset4/private/private.sql
+++ CS50sql/pset4/private/private.sql
@@ -7,19 +7,27 @@ SELECT substr("sentence", 3, 5)
 FROM "sentences"
 WHERE id = 114
 UNION
-SELECT substr("sentence", 98, 4)
+SELECT substr("sentence", 72, 9)
 FROM "sentences"
-WHERE id = 14
+WHERE id = 618
 UNION
-SELECT substr("sentence", 98, 4)
+SELECT substr("sentence", 7, 3)
 FROM "sentences"
-WHERE id = 14
+WHERE id = 630
 UNION
-SELECT substr("sentence", 98, 4)
+SELECT substr("sentence", 12, 5)
 FROM "sentences"
-WHERE id = 14
+WHERE id = 932
 UNION
-SELECT substr("sentence", 98, 4)
+SELECT substr("sentence", 50, 7)
 FROM "sentences"
-WHERE id = 14
+WHERE id = 2230
+UNION
+SELECT substr("sentence", 44, 10)
+FROM "sentences"
+WHERE id = 2346
+UNION
+SELECT substr("sentence", 14, 5 )
+FROM "sentences"
+WHERE id = 3041
 UNION;
