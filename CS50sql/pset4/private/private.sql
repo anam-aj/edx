ca88484625ef32125845c077f2d8936423352457
anam-aj <anamaj.786@gmail.com>
2024-11-28 09:12:01 +0000

    Thu, Nov 28, 2024, 2:42 PM +05:30

--- CS50sql/pset4/private/private.sql
+++ CS50sql/pset4/private/private.sql
@@ -1,32 +1,32 @@
 CREATE VIEW "message" AS
-SELECT substr("sentence", 98, 4) AS "phrase"
+SELECT TRIM(substr("sentence", 98, 4)) AS "phrase"
 FROM "sentences"
 WHERE id = 14
 UNION
-SELECT substr("sentence", 3, 5)
+SELECT TRIM(substr("sentence", 3, 5))
 FROM "sentences"
 WHERE id = 114
 UNION
-SELECT substr("sentence", 72, 9)
+SELECT TRIM(substr("sentence", 72, 9))
 FROM "sentences"
 WHERE id = 618
 UNION
-SELECT substr("sentence", 7, 3)
+SELECT TRIM(substr("sentence", 7, 3))
 FROM "sentences"
 WHERE id = 630
 UNION
-SELECT substr("sentence", 12, 5)
+SELECT TRIM(substr("sentence", 12, 5))
 FROM "sentences"
 WHERE id = 932
 UNION
-SELECT substr("sentence", 50, 7)
+SELECT TRIM(substr("sentence", 50, 7))
 FROM "sentences"
 WHERE id = 2230
 UNION
-SELECT substr("sentence", 44, 10)
+SELECT TRIM(substr("sentence", 44, 10))
 FROM "sentences"
 WHERE id = 2346
 UNION
-SELECT substr("sentence", 14, 5)
+SELECT TRIM(substr("sentence", 14, 5))
 FROM "sentences"
 WHERE id = 3041;
