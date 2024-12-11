ad55677c10e4e9b672810ce0eb41ede277971046
anam-aj <anamaj.786@gmail.com>
2024-12-11 14:26:54 +0000

    Wed, Dec 11, 2024, 7:56 PM +05:30

--- CS50sql/pset5/harvard/indexes.sql
+++ CS50sql/pset5/harvard/indexes.sql
@@ -4,4 +4,4 @@ CREATE INDEX index_course_id ON enrollments (course_id);
 
 CREATE INDEX index_department_number_semester_title ON courses (department, number, semester, title);
 
-
+CREATE INDEX index_course_id_requirement_id ON satisfies (course_id, requirement_id);
