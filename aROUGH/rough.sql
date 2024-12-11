d96e524412871260c48dd741d114b08faf5b5fbc
anam-aj <anamaj.786@gmail.com>
2024-12-11 05:10:02 +0000

    Wed, Dec 11, 2024, 10:40 AM +05:30

--- aROUGH/rough.sql
+++ aROUGH/rough.sql
@@ -1,9 +1,12 @@
 CREATE INDEX test_index
-ON users ()
+ON users (phone_number)
+WHERE joined_date < '2010-01-01';
 
 
+/*
 select *
 from users
 where joined_date < '2010-01-01'
 order by joined_date
-desc limit 10;
+desc limit 10;?
+*/
