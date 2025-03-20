db132f3645eef6601ed6c2e84ae62228f752e695
anam-aj <anamaj.786@gmail.com>
2025-03-20 03:38:52 +0000

    Thu, Mar 20, 2025, 9:08 AM +05:30

---
+++ CS50r/pset3/happy/happy.R
@@ -0,0 +1,18 @@
+# Ask user the name of country
+country <- readline("Country: ")
+
+# Read CSVs
+year_2020 <- read.csv("2020.csv")
+year_2020$year <- 2020
+year_2021 <- read.csv("2021.csv")
+year_2022 <- read.csv("2022.csv")
+year_2023 <- read.csv("2023.csv")
+year_2024 <- read.csv("2024.csv")
+
+# Output the happiness score year wise
+years <- c(2020:2024)
+country_list <-
+
+if (country %in% )
+
+# Data absent
