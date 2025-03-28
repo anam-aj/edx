3752cb5936be6c21e8b777804935a18829cd4dab
anam-aj <anamaj.786@gmail.com>
2025-03-28 05:54:22 +0000

    Fri, Mar 28, 2025, 11:24 AM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -48,7 +48,7 @@ dfUT1 = df[df.UT == 1]
 
 dfUT1.reset_index(inplace=True)
 
-dfUT1.drop(columns=[‘index’],inplace=True)
+dfUT1.drop(columns=['index'], inplace=True)
 
 dfUT1.set_index('Name', inplace=True)
 
