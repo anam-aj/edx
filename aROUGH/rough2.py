5b18e05a2144058e97b4b250d9722dd3e0d10335
anam-aj <anamaj.786@gmail.com>
2025-04-15 13:40:04 +0000

    Tue, Apr 15, 2025, 7:10 PM +05:30

--- aROUGH/rough2.py
+++ aROUGH/rough2.py
@@ -2,7 +2,7 @@ def insertion_Sort(list3):
     n= len(list3)
     for i in range(n): # Traverse through all elements
         temp = list3[i]
-        j = i-1
+        j = i - 1
         while j >=0 and temp< list3[j] :
             list3[j+1] = list3[j]
             j = j-1
