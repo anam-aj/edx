360b4d42f473caf75dd92409b8413dfaea750b46
anam-aj <anamaj.786@gmail.com>
2025-06-30 17:39:31 +0000

    Mon, Jun 30, 2025, 11:09 PM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -1,4 +1,17 @@
-my_dict = {"name": "Alice", "age": 25, "city": "New York"}
-for key, value in my_dict.items():
-    print(5)
-    print(key, value)
+    # Empty dictionary to hold each list as a key-value pair
+    # key = columnname, value = list of correspondig values from all rows
+    data = {}
+
+    # Open CSV file
+    with open (filename) as file:
+        rows = csv.DictReader(file)
+
+        # Add column names to 'data' as keys
+        columns = rows.keys()
+        for name in columns:
+            data[name] = []
+
+        # Populate the columns(keys) in 'data' with values
+        for row in rows:
+            for col, val in row.items:
+                data[col].append(val)
