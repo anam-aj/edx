6a32ce100887d0c445556a3d249592c9706219fc
anam-aj <anamaj.786@gmail.com>
2025-06-30 17:53:33 +0000

    Mon, Jun 30, 2025, 11:23 PM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -3,13 +3,14 @@ import csv
 # Empty dictionary to hold each list as a key-value pair
 # key = columnname, value = list of correspondig values from all rows
 data = {}
+rows = {}
 
 # Open CSV file
 with open ("shopping.csv") as file:
-    rows = csv.DictReader(file)
+    rows = list(csv.DictReader(file))
 
 # Add column names to 'data' as keys
-columns = rows.fieldnames
+columns = rows[0].keys()
 for name in columns:
     data[name] = []
 
