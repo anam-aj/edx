989e74739725df03302d63ca4b9b97b93d2dcf67
anam-aj <anamaj.786@gmail.com>
2025-06-30 17:41:00 +0000

    Mon, Jun 30, 2025, 11:11 PM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -1,17 +1,19 @@
-    # Empty dictionary to hold each list as a key-value pair
-    # key = columnname, value = list of correspondig values from all rows
-    data = {}
+import csv
 
-    # Open CSV file
-    with open (filename) as file:
-        rows = csv.DictReader(file)
+# Empty dictionary to hold each list as a key-value pair
+# key = columnname, value = list of correspondig values from all rows
+data = {}
 
-        # Add column names to 'data' as keys
-        columns = rows.keys()
-        for name in columns:
-            data[name] = []
+# Open CSV file
+with open ("shopping.csv") as file:
+    rows = csv.DictReader(file)
 
-        # Populate the columns(keys) in 'data' with values
-        for row in rows:
-            for col, val in row.items:
-                data[col].append(val)
+    # Add column names to 'data' as keys
+    columns = rows.keys()
+    for name in columns:
+        data[name] = []
+
+    # Populate the columns(keys) in 'data' with values
+    for row in rows:
+        for col, val in row.items:
+            data[col].append(val)
