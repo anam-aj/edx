4c2e2f017f02f90018d0e1822978af00a80b0624
anam-aj <anamaj.786@gmail.com>
2025-06-30 17:54:56 +0000

    Mon, Jun 30, 2025, 11:24 PM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -3,25 +3,24 @@ import csv
 # Empty dictionary to hold each list as a key-value pair
 # key = columnname, value = list of correspondig values from all rows
 data = {}
-rows = {}
 
 # Open CSV file
 with open ("shopping.csv") as file:
-    rows = list(csv.DictReader(file))
+    rows = csv.DictReader(file)
 
-# Add column names to 'data' as keys
-columns = rows[0].keys()
-for name in columns:
-    data[name] = []
+    # Add column names to 'data' as keys
+    columns = rows.fieldnames
+    for name in columns:
+        data[name] = []
 
-# Populate the columns(keys) in 'data' with values
-for row in rows:
-    for col, val in row.items():
-        data[col].append(val)
+    # Populate the columns(keys) in 'data' with values
+    for row in rows:
+        for col, val in row.items():
+            data[col].append(val)
 
-count = 0
-for key, value in data.items():
-    print(key)
-    print(data[key][0:4])
+    count = 0
+    for key, value in data.items():
+        print(key)
+        print(data[key][0:4])
 
 
--- aROUGH/rough2.py
+++ aROUGH/rough2.py
@@ -1,4 +1,25 @@
-if True:
-    a = 1
+import csv
 
-print(a)
+# Empty dictionary to hold each list as a key-value pair
+# key = columnname, value = list of correspondig values from all rows
+data = {}
+rows = {}
+
+# Open CSV file
+with open ("shopping.csv") as file:
+    rows = list(csv.DictReader(file))
+
+# Add column names to 'data' as keys
+columns = rows[0].keys()
+for name in columns:
+    data[name] = []
+
+# Populate the columns(keys) in 'data' with values
+for row in rows:
+    for col, val in row.items():
+        data[col].append(val)
+
+count = 0
+for key, value in data.items():
+    print(key)
+    print(data[key][0:4])
