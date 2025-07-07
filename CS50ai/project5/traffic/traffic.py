dc5b7aeea5e093378dad185784bf9f68c83a9aac
anam-aj <anamaj.786@gmail.com>
2025-07-07 14:17:52 +0000

    Mon, Jul 7, 2025, 7:47 PM +05:30

--- CS50ai/project5/traffic/traffic.py
+++ CS50ai/project5/traffic/traffic.py
@@ -93,6 +93,14 @@ def get_model():
 
     # Convolutional layer. Learn 32 filters using a 3x3 kernel
     tf.keras.layers.Conv2D(
+        64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+    ),
+
+    # Max-pooling layer, using 2x2 pool size
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+    # Convolutional layer. Learn 32 filters using a 3x3 kernel
+    tf.keras.layers.Conv2D(
         128, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
     ),
 
@@ -103,7 +111,7 @@ def get_model():
     tf.keras.layers.Flatten(),
 
     # Add a hidden layer with dropout
-    tf.keras.layers.Dense(256, activation="relu"),
+    tf.keras.layers.Dense(128, activation="relu"),
     tf.keras.layers.Dropout(0.5),
 
     # Add an output layer with output units for 43 catogories
