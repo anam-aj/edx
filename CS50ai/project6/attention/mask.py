20f2593f39403ff74ce0d90f41fc9b7d68a756c5
anam-aj <anamaj.786@gmail.com>
2025-07-15 18:23:35 +0000

    Tue, Jul 15, 2025, 11:53 PM +05:30

--- CS50ai/project6/attention/mask.py
+++ CS50ai/project6/attention/mask.py
@@ -66,6 +66,7 @@ def get_color_for_attention_score(attention_score):
     Return a tuple of three integers representing a shade of gray for the
     given `attention_score`. Each value should be in the range [0, 255].
     """
+
     # 
 
 
