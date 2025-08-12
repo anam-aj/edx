49a033274ca153288201f059a3c93604411b323d
anam-aj <anamaj.786@gmail.com>
2025-08-12 12:11:25 +0000

    Tue, Aug 12, 2025, 5:41 PM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -2,3 +2,26 @@ a = [1, 2, 3]
 a, b, c = a
 
 print(a, b, c)
+def max_pairwise_product(numbers):
+    n = len(numbers)
+
+    if numbers[0] > numbers[1]:
+        highest = numbers[0]
+        second_highest = numbers[1]
+    else:
+        highest = numbers[1]
+        second_highest = numbers[0]
+
+    for index in range(2, n):
+        if numbers[index] > highest:
+            second_highest = highest
+            highest = numbers[index]
+        elif numbers[index] > second_highest:
+            second_highest = numbers[index]
+
+    return (highest * second_highest)
+
+if __name__ == '__main__':
+    _ = int(input())
+    input_numbers = list(map(int, input().split()))
+    print(max_pairwise_product(input_numbers))
