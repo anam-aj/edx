dce45085c45098e04ca3d3088b5dd0616f1a2bb4
anam-aj <anamaj.786@gmail.com>
2025-08-12 12:58:08 +0000

    Tue, Aug 12, 2025, 6:28 PM +05:30

--- DSA Coursera/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ DSA Coursera/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -2,12 +2,9 @@ def fibonacci_number(n):
     first_num = 0
     second_num = 1
 
-    if n == 1:
-        return 0
-    elif n == 2:
-        return 1
-    else:
+    for number in range(n):
         
+
 
 
 
