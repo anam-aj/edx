4da5da05a70058644709f3aaef06af4af5c8ca5d
anam-aj <anamaj.786@gmail.com>
2025-08-13 09:06:17 +0000

    Wed, Aug 13, 2025, 2:36 PM +05:30

--- DSA_Coursera/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
+++ DSA_Coursera/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
@@ -1,5 +1,5 @@
 def fibonacci_sum_squares(n):
-    if n <= 1:
+    '''if n <= 1:
         return n
 
     previous, current, sum = 0, 1, 1
@@ -8,7 +8,35 @@ def fibonacci_sum_squares(n):
         previous, current = current, previous + current
         sum += current * current
 
-    return sum % 10
+    return sum % 10'''
+
+    if n < 1:
+        return 0
+
+    previous, current, _sum = 0, 1, 1
+    modulo_of_pre_cur_sum = [(0, 1, 1)]
+    repeat_after = 1
+
+    if n < (10 ** 3):
+        small = n
+    else:
+        small = 10 ** 3
+
+    for _ in range(1, small):
+        previous, current = current, previous + current
+        _sum = _sum + current
+        previous %= 10
+        current %= 10
+        _sum %= 10
+        current_modulo_of_pre_cur_sum = (previous, current, _sum)
+        if current_modulo_of_pre_cur_sum == (0, 1, 1):
+            break
+        else:
+            modulo_of_pre_cur_sum.append(current_modulo_of_pre_cur_sum)
+            repeat_after += 1
+
+    index = n % repeat_after
+    return modulo_of_pre_cur_sum[index - 1][2]
 
 
 if __name__ == '__main__':
