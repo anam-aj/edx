fa1637c16ad85d8bf98f69ce6f4be2352f0c0202
anam-aj <anamaj.786@gmail.com>
2025-08-29 17:42:56 +0000

    Fri, Aug 29, 2025, 11:12 PM +05:30

--- DSA_Coursera/week4_divide_and_conquer/3_majority_element/majority_element.py
+++ DSA_Coursera/week4_divide_and_conquer/3_majority_element/majority_element.py
@@ -21,31 +21,41 @@ def quick_sort(elements, l, r):
     if l >= r:
         return
 
-    index = partition(elements, l, r)
+    index1, index2 = partition(elements, l, r)
 
-    quick_sort(elements, l, index - 1)
-    quick_sort(elements, index + 1 , r)
+    quick_sort(elements, l, index1 - 1)
+    quick_sort(elements, index2 + 1 , r)
 
 
 def partition(elements, l, r):
 
     pivot = elements[l]
-    j = l
+    index1 = l
+    index2 = l
 
     for i in range(l + 1, r + 1):
         if elements[i] > pivot:
             pass
-        elif elements[i] <= pivot:
-            tmp = elements[j + 1]
-            elements[j + 1] = elements[i]
+        elif elements[i] == pivot:
+            tmp = elements[index2 + 1]
+            elements[index2 + 1] = elements[i]
             elements[i] = tmp
-            j += 1
+            index2 += 1
+        elif elements[i] < pivot:
+            tmp_id1 = elements[index1]
+            tmp = elements[index2 + 1]
+            elements[index1] = elements[i]
+            elements[index2 + 1] = tmp_id1
+            elements[i] = tmp
+            index1 += 1
+            index2 += 1
 
-    tmp = elements[l]
-    elements[l] = elements[j]
-    elements[j] = tmp
+    '''tmp = elements[l]
+    elements[l] = elements[index1 - 1]
+    elements[index1 - 1] = tmp
+    index1 -= 1'''
 
-    return j
+    return (index1, index2)
 
 
 if __name__ == '__main__':
