e2979bc3da957dcf4b81a04bf8a2c54c355fad63
anam-aj <anamaj.786@gmail.com>
2025-09-09 10:19:57 +0000

    Tue, Sep 9, 2025, 3:49 PM +05:30

--- DSA_Coursera/week4_divide_and_conquer/5_number_of_inversions/inversions.py
+++ DSA_Coursera/week4_divide_and_conquer/5_number_of_inversions/inversions.py
@@ -3,10 +3,55 @@ from itertools import combinations
 
 def inversions_naive(a):
     number_of_inversions = 0
-    for i, j in combinations(range(len(a)), 2):
-        if a[i] > a[j]:
-            number_of_inversions += 1
+
+    sorted_array, number_of_inversions = mergesort(a, number_of_inversions)
+
     return number_of_inversions
+
+
+def mergesort(array, inversion):
+
+    length = len(array)
+
+    if length == 1:
+        return array, inversion
+
+    mid = length // 2
+
+    left, inv_l = mergesort(array[:mid], inversion)
+    right, inv_r = mergesort(array[mid:length], inv_l)
+
+    sorted_array, inv_s = merge(left, right, inv_r)
+
+    return sorted_array, inv_s
+
+
+def merge(arr1, arr2, inversion):
+
+    merged_arr = []
+
+    i, j = 0, 0
+
+    while i < len(arr1) and j < len(arr2):
+
+        element_1, element_2 = arr1[i], arr2[j]
+
+        if element_1 <= element_2:
+            merged_arr.append(element_1)
+            i += 1
+        else:
+            merged_arr.append(element_2)
+            j += 1
+            inversion += len(arr1) - i
+
+    while i < len(arr1):
+        merged_arr.append(arr1[i])
+        i += 1
+    while j < len(arr2):
+        merged_arr.append(arr2[j])
+        j += 1
+
+    return merged_arr, inversion
 
 
 if __name__ == '__main__':
--- DSA_Coursera/week4_divide_and_conquer/6_organizing_a_lottery/points_and_segments.py
+++ DSA_Coursera/week4_divide_and_conquer/6_organizing_a_lottery/points_and_segments.py
@@ -5,12 +5,82 @@ def points_cover_naive(starts, ends, points):
     assert len(starts) == len(ends)
     count = [0] * len(points)
 
-    for index, point in enumerate(points):
+    '''for index, point in enumerate(points):
         for start, end in zip(starts, ends):
             if start <= point <= end:
-                count[index] += 1
+                count[index] += 1'''
+
+    sorted_starts = mergesort(starts)
+    sorted_ends = mergesort(ends)
+
+    for index, point in enumerate(points):
+        ...
+
 
     return count
+
+
+def binary_search(keys, query):
+    # write your code here
+
+    start = 0
+    end = len(keys) - 1
+
+    while end >= start:
+        mid = (end + start) // 2
+
+        if query == keys[mid]:
+            return mid
+        elif query < keys[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return -1
+
+
+def mergesort(array):
+
+    length = len(array)
+
+    if length == 1:
+        return array
+
+    mid = length // 2
+
+    left = mergesort(array[:mid])
+    right = mergesort(array[mid:length])
+
+    sorted_array = merge(left, right)
+
+    return sorted_array
+
+
+def merge(arr1, arr2):
+
+    merged_arr = []
+
+    i, j = 0, 0
+
+    while i < len(arr1) and j < len(arr2):
+
+        element_1, element_2 = arr1[i], arr2[j]
+
+        if element_1 <= element_2:
+            merged_arr.append(element_1)
+            i += 1
+        else:
+            merged_arr.append(element_2)
+            j += 1
+
+    while i < len(arr1):
+        merged_arr.append(arr1[i])
+        i += 1
+    while j < len(arr2):
+        merged_arr.append(arr2[j])
+        j += 1
+
+    return merged_arr
 
 
 if __name__ == '__main__':
