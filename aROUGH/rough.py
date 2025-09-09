7b3acf180d2a4bb0e3507035a9dd9c541d387263
anam-aj <anamaj.786@gmail.com>
2025-09-09 15:28:14 +0000

    Tue, Sep 9, 2025, 8:58 PM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -1,3 +1,23 @@
-l = ['a', 2, 'c']
+def binary_search_duplicate(keys, query):
+    # write your code here
 
-print(enumerate(l))
+    start = 0
+    end = len(keys) - 1
+
+    index = -1
+
+    while end >= start:
+        mid = (end + start) // 2
+
+        if keys[mid] <= query:
+            index = mid
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return index
+
+l = [-1,1,1,1,2,2,3,3,3,3,3,4,4,4,5,5,6,6]
+k = [ 0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7]
+
+print(binary_search_duplicate(l, 6))
