30741ef076fd786ea75a7b89c4233d2ebc628f5c
anam-aj <anamaj.786@gmail.com>
2025-09-09 16:47:56 +0000

    Tue, Sep 9, 2025, 10:17 PM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -37,7 +37,9 @@ def search_index_ends(keys, query):
 
     return index
 
+
 l = [ 1.1,1.4,1.5,1.6,2,2,3,3,3,3,3,4,4,4,5,5,6,6]
 k = [ 0  ,1  ,2  ,3  ,4,5,6,7,8,9,0,1,2,3,4,5,6,7]
+
 
 print(binary_search_duplicate(l,2))
