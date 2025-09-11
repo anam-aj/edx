d680e859bb45f225f3769c00b8e2db66612fc2b4
anam-aj <anamaj.786@gmail.com>
2025-09-11 09:46:27 +0000

    Thu, Sep 11, 2025, 3:16 PM +05:30

--- DSA_Coursera/week4_divide_and_conquer/7_closest_points/closest.py
+++ DSA_Coursera/week4_divide_and_conquer/7_closest_points/closest.py
@@ -11,13 +11,71 @@ def distance_squared(first_point, second_point):
 
 
 def minimum_distance_squared_naive(points):
-    min_distance_squared = float("inf")
 
-    for p, q in combinations(points, 2):
+    point_sorted_by_x = mergesort(points)
+    min_distance_squared = float("inf")
+    d = shortest_distance(points_sorted_by_x)
+
+    '''for p, q in combinations(points, 2):
         min_distance_squared = min(min_distance_squared,
-                                   distance_squared(p, q))
+                                   distance_squared(p, q))'''
+
+    for p in points:
+        ...
 
     return min_distance_squared
+
+def shortest_distance(sorted_points):
+
+    start = 0
+    end = len(sorted_points) - 1
+
+    index -1 
+
+
+
+def mergesort(array):
+
+    length = len(array)
+
+    if length == 1:
+        return array
+
+    mid = length // 2
+
+    left = mergesort(array[:mid])
+    right = mergesort(array[mid:length])
+
+    sorted_array = merge(left, right)
+
+    return sorted_array
+
+
+def merge(arr1, arr2):
+
+    merged_arr = []
+
+    i, j = 0, 0
+
+    while i < len(arr1) and j < len(arr2):
+
+        element_1, element_2 = arr1[i], arr2[j]
+
+        if element_1.x <= element_2.x:
+            merged_arr.append(element_1)
+            i += 1
+        else:
+            merged_arr.append(element_2)
+            j += 1
+
+    while i < len(arr1):
+        merged_arr.append(arr1[i])
+        i += 1
+    while j < len(arr2):
+        merged_arr.append(arr2[j])
+        j += 1
+
+    return merged_arr
 
 
 if __name__ == '__main__':
