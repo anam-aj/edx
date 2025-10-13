1f988ab5f26bfcd6467be03037e142b6ccd7e83c
anam-aj <anamaj.786@gmail.com>
2025-10-13 17:37:00 +0000

    Mon, Oct 13, 2025, 11:07 PM +05:30

--- DSA_Coursera/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ DSA_Coursera/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -1,19 +1,31 @@
 def compute_operations(n):
 
-    min_steps = [0]
-    steps = [[], [1], ]
+    steps_list = [[], [1], ]
 
     for num in range(2, n + 1):
-        steps.append([])
+
+        least_steps = []
+
         if num - 1 > 0:
-            ...
+            steps = steps_list[num - 1].copy()
+            steps.append(num)
+            least_steps[:] = steps
+
         if num % 2 == 0:
-            ...
+            new_steps = steps_list[num // 2].copy()
+            new_steps.append(num)
+            if len(new_steps) < len(least_steps):
+                least_steps[:] = new_steps
+
         if num % 3 == 0:
-            ...
+            new_steps = steps_list[num // 3].copy()
+            new_steps.append(num)
+            if len(new_steps) < len(least_steps):
+                least_steps[:] = new_steps
 
+        steps_list.append(least_steps)
 
-    return []
+    return steps_list[num]
 
 
 if __name__ == '__main__':
