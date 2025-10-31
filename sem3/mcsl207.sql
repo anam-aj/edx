1b26c87d714f51b4b5f288f5297b42396fce4c39
anam-aj <anamaj.786@gmail.com>
2025-10-31 09:43:01 +0000

    Fri, Oct 31, 2025, 3:13 PM +05:30

---
+++ sem3/mcsl207.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Member (
+    memberId CHAR(4) PRIMARY KEY,
+    memberName VARCHAR(50),
+    memberAddress VARCHAR(100),
+    memberPhone VARCHAR(15)
+);
+
+CREATE TABLE Book (
+    BookId CHAR(4) PRIMARY KEY,
+    BookTitle VARCHAR(100),
+    FirstAuthor VARCHAR(50)
+);
+
+CREATE TABLE BookIssued (
+    memberID CHAR(4),
+    BookID CHAR(4),
+    IssueDate DATE,
+    returnDate DATE,
+    PRIMARY KEY(memberID, BookID),
+    FOREIGN KEY(memberID) REFERENCES Member(memberId),
+    FOREIGN KEY(BookID) REFERENCES Book(BookId)
+);
