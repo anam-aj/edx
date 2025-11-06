985a554034609e582d5283b166630f17825c6515
anam-aj <anamaj.786@gmail.com>
2025-11-06 13:37:18 +0000

    Thu, Nov 6, 2025, 7:07 PM +05:30

--- DSA_Coursera/week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ DSA_Coursera/week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -1,7 +1,14 @@
 def edit_distance(first_string, second_string):
 
     m, n = len(first_string), len(second_string)
-    
+
+    dist_matrix = [[]]
+
+    for i in range(m + 1):
+        dist_matrix[i][0] = i
+    for i in range(n + 1):
+        dist_matrix[0][i] = i
+
 
     return 0
 
