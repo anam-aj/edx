6f1626452b8bee8919e2866633fdd5f576d182ef
anam-aj <anamaj.786@gmail.com>
2025-11-06 13:46:26 +0000

    Thu, Nov 6, 2025, 7:16 PM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -4,14 +4,16 @@ dist_matrix = []
 for i in range(m + 1):
     dist_matrix.append([])
     for j in range(n + 1):
-        
+        dist_matrix[i].append(0)
 
+print(dist_matrix)
 
+'''
 for i in range(m + 1):
     dist_matrix[i][0] = i
 for i in range(n + 1):
     dist_matrix[0][i] = i
 
 print(dist_matrix)
-
+'''
 
