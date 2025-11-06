dc9b4861fcc8c2fd7311ea86a7439f93e6e6a44e
anam-aj <anamaj.786@gmail.com>
2025-11-06 13:39:09 +0000

    Thu, Nov 6, 2025, 7:09 PM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -1,12 +1,9 @@
-{
-    "Date": "Sun, 02 Nov 2025 16:55:57 GMT",
-    "X-Clv-Request-Id": "880c7bb4-02e6-4b48-9921-0387ced6ee2b",
-    "Server": "Cleversafe",
-    "X-Clv-S3-Version": "2.5",
-    "Accept-Ranges": "bytes",
-    "x-amz-request-id": "880c7bb4-02e6-4b48-9921-0387ced6ee2b",
-    "ETag": '"8bb44578fff8fdcc3d2972be9ece0164"',
-    "Content-Type": "image/png",
-    "Last-Modified": "Wed, 16 Nov 2022 03:32:41 GMT",
-    "Content-Length": "78776",
-}
+m, n = 3, 4
+dist_matrix = [[]]
+
+for i in range(m + 1):
+    dist_matrix[i][0] = i
+for i in range(n + 1):
+    dist_matrix[0][i] = i
+
+print(dist_matrix)
