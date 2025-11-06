835c7a5bd789b54fb97834fd4ca052987996e546
anam-aj <anamaj.786@gmail.com>
2025-11-06 14:03:32 +0000

    Thu, Nov 6, 2025, 7:33 PM +05:30

--- DSA_Coursera/week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ DSA_Coursera/week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -2,21 +2,24 @@ def edit_distance(first_string, second_string):
 
     m, n = len(first_string), len(second_string)
 
-    dist_matrix = []
+    edit_distances = []
 
     # Create empty matrix for edit distances
     for i in range(m + 1):
-        dist_matrix.append([])
+        edit_distances.append([])
         for _ in range(n + 1):
-            dist_matrix[i].append(0)
+            edit_distances[i].append(0)
 
     # Fill in edit distance for first row and first column
     for i in range(m + 1):
-        dist_matrix[i][0] = i
+        edit_distances[i][0] = i
     for i in range(n + 1):
-        dist_matrix[0][i] = i
+        edit_distances[0][i] = i
 
-    for i in range(1, )
+    # Compute edit distances for all i and j positions
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            insertion = edit_distances[]
 
 
     return 0
