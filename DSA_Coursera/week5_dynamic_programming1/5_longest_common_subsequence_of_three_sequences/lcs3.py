87f587b86ee4daab2338c9c83020b39c0362e042
anam-aj <anamaj.786@gmail.com>
2025-11-07 07:40:55 +0000

    Fri, Nov 7, 2025, 1:10 PM +05:30

--- DSA_Coursera/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
+++ DSA_Coursera/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
@@ -12,11 +12,16 @@ def lcs3(first_sequence, second_sequence, third_sequence):
 
     for i in range(1, m + 1):
         for j in range(1, n + 1):
-            if a[i - 1] == b[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
-            else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+            for k in range(1, r + 1):
+
+                if (first_sequence[i - 1] == second_sequence[j - 1] and
+                    first_sequence[i - 1] == second_sequence[j - 1]):
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
     return dp[m][n]
+
 
 if __name__ == '__main__':
     n = int(input())
