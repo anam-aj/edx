56e81f514f02d173398d42b0639e72263a8c6380
anam-aj <anamaj.786@gmail.com>
2025-11-07 06:46:41 +0000

    Fri, Nov 7, 2025, 12:16 PM +05:30

--- DSA_Coursera/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ DSA_Coursera/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -1,3 +1,4 @@
+'''
 def lcs2(first_sequence, second_sequence):
 
     m, n = len(first_sequence), len(second_sequence)
@@ -24,6 +25,18 @@ def lcs2(first_sequence, second_sequence):
                 lcs[i][j] = max(insertion, deletion, mis_match)
 
     return lcs[m][n]
+'''
+
+def lcs2(a, b):
+    m, n = len(a), len(b)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if a[i - 1] == b[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    return dp[m][n]
 
 
 if __name__ == '__main__':
