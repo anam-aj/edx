935d5d14df982fd1d98dadafecd3c8a59971f762
anam-aj <anamaj.786@gmail.com>
2025-11-19 07:08:39 +0000

    Wed, Nov 19, 2025, 12:38 PM +05:30

---
+++ sem3/c.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+//#include <string>
+
+using namespace std;
+
+int main()
+{
+    //string name;
+    cout << "enter name" << endl;
+    cin >> string name;
+    cout << "welcome " << name << endl;
+    return 0;
+}
