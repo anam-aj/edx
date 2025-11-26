9d7ea37e838d345b940b1807e502f61bb29fe588
anam-aj <anamaj.786@gmail.com>
2025-11-26 07:14:56 +0000

    Wed, Nov 26, 2025, 12:44 PM +05:30

--- aROUGH/rough1.c
+++ aROUGH/rough1.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-#define STR1         "A macro definition!\n"
-#define STR2         "must be all on one line!\n"
-#define EXPR1       1+2+3
-#define EXPR2       EXPR1+5
-#define ABS(x)      (((x) < 0) ? - (x):(x))
-#define MAX(p,q)    ((p < q) ? (q):(p))
-#define BIGGEST(p,q,r)   (MAX(p, q) < r)?(r):(MAX(p, q))
 
-int main(void)
+
+int main()
 {
-    printf(STR1);
-    printf(STR2);
-    printf("Largest number among %d, %d and %d is %d\n",EXPR1, EXPR2, ABS(-3), BIGGEST(1,2,3));
+    int a;
+    scanf("%d", &a);
+    printf("you entered %d\n", a);
 }
-
 
 
