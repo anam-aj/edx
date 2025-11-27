8b35e1a6d7b339f5b9a95f7b32bf14c6bc407a90
anam-aj <anamaj.786@gmail.com>
2025-11-27 05:16:51 +0000

    Thu, Nov 27, 2025, 10:46 AM +05:30

--- DSA_Coursera/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ DSA_Coursera/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -12,13 +12,10 @@ def maximum_gold(capacity, weights):
 
     for item in range(1, len(weights) + 1):
         weight_i = weights[item - 1]
-
         for current_capacity in range(1, capacity + 1):
             dp_value[item][current_capacity] = dp_value[item - 1][current_capacity]
-
             if weight_i <= current_capacity:
                 val = dp_value[item - 1][current_capacity - weight_i] + weight_i
-
                 if dp_value[item][current_capacity] < val:
                     dp_value[item][current_capacity] = val
 
