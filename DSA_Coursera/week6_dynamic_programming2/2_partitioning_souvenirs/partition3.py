4d8fa1fc481685505eb4c68da47d4ee78e5ce084
anam-aj <anamaj.786@gmail.com>
2025-11-27 13:56:06 +0000

    Thu, Nov 27, 2025, 7:26 PM +05:30

--- DSA_Coursera/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
+++ DSA_Coursera/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
@@ -4,7 +4,7 @@ from sys import stdin
 def partition3(values):
     if values.sum() % 3 != 0:
         return False
-    target = values.sum() / 3
+    target = values.sum() // 3
 
 if __name__ == '__main__':
     input_n, *input_values = list(map(int, stdin.read().split()))
