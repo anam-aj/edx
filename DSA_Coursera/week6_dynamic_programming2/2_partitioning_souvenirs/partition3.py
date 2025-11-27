3d247550d05417f3f2f154cd6da13f0d6bb554be
anam-aj <anamaj.786@gmail.com>
2025-11-27 14:00:12 +0000

    Thu, Nov 27, 2025, 7:30 PM +05:30

--- DSA_Coursera/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
+++ DSA_Coursera/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
@@ -10,7 +10,7 @@ def partition3(values):
         if value > target:
             return False
 
-    
+    dp_table = []
 
 if __name__ == '__main__':
     input_n, *input_values = list(map(int, stdin.read().split()))
