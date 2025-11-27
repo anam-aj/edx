cc21702f9979242cceba5d3cf51490f0c55d4f34
anam-aj <anamaj.786@gmail.com>
2025-11-27 13:59:16 +0000

    Thu, Nov 27, 2025, 7:29 PM +05:30

--- DSA_Coursera/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
+++ DSA_Coursera/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
@@ -6,6 +6,12 @@ def partition3(values):
         return False
     target = values.sum() // 3
 
+    for value in values:
+        if value > target:
+            return False
+
+    
+
 if __name__ == '__main__':
     input_n, *input_values = list(map(int, stdin.read().split()))
     assert input_n == len(input_values)
