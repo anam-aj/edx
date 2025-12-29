99d6ac68b70cbbd3d073c9eae410a5d55f51342d
anam-aj <anamaj.786@gmail.com>
2025-12-29 06:26:53 +0000

    Mon, Dec 29, 2026, 11:56 AM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -1,194 +1,83 @@
-import sqlite3
-import pandas as pd
+# Code for ETL operations on Country-GDP data
 
-# Connect to the SQLite3 service
-conn = sqlite3.connect('STAFF.db')
-
-# Define table parameters
-table_name = 'INSTRUCTOR'
-attribute_list = ['ID', 'FNAME', 'LNAME', 'CITY', 'CCODE']
-
-# Read the CSV data
-file_path = '/home/project/INSTRUCTOR.csv'
-df = pd.read_csv(file_path, names = attribute_list)
-
-# Load the CSV to the database
-df.to_sql(table_name, conn, if_exists = 'replace', index = False)
-print('Table is ready')
-
-# Query 1: Display all rows of the table
-query_statement = f"SELECT * FROM {table_name}"
-query_output = pd.read_sql(query_statement, conn)
-print(query_statement)
-print(query_output)
-
-# Query 2: Display only the FNAME column for the full table.
-query_statement = f"SELECT FNAME FROM {table_name}"
-query_output = pd.read_sql(query_statement, conn)
-print(query_statement)
-print(query_output)
-
-# Query 3: Display the count of the total number of rows.
-query_statement = f"SELECT COUNT(*) FROM {table_name}"
-query_output = pd.read_sql(query_statement, conn)
-print(query_statement)
-print(query_output)
-
-# Define data to be appended
-data_dict = {'ID' : [100],
-            'FNAME' : ['John'],
-            'LNAME' : ['Doe'],
-            'CITY' : ['Paris'],
-            'CCODE' : ['FR']}
-data_append = pd.DataFrame(data_dict)
-
-# Append data to the table
-data_append.to_sql(table_name, conn, if_exists = 'append', index = False)
-print('Data appended successfully')
-
-# Close the connection
-conn.close()
-
-
-
-
-
-
-import requests
-import sqlite3
-import pandas as pd
+# Importing the required libraries
 from bs4 import BeautifulSoup
-
-url = 'https://web.archive.org/web/20230902185655/https://en.everybodywiki.com/100_Most_Highly-Ranked_Films'
-db_name = 'Movies.db'
-table_name = 'Top_50'
-csv_path = 'top_50_films.csv'
-df = pd.DataFrame(columns=["Average Rank","Film","Year"])
-count = 0
-
-html_page = requests.get(url).text
-data = BeautifulSoup(html_page, 'html.parser')
-
-tables = data.find_all('tbody')
-rows = tables[0].find_all('tr')
-
-for row in rows:
-    if count < 50 :
-        col = row.find_all('td')
-        if len(col)!=0:
-            data_dict = {"Average Rank": int(col[0].contents[0]),
-                         "Film": str(col[1].contents[0]),
-                         "Year": int(col[2].contents[0])}
-            df1 = pd.DataFrame(data_dict, index=[0])
-            df = pd.concat([df,df1], ignore_index=True)
-            count+=1
-    else:
-        break
-
-print(df)
-
-df.to_csv(csv_path)
-
-conn = sqlite3.connect(db_name)
-df.to_sql(table_name, conn, if_exists='replace', index=False)
-conn.close()
-
-
-
-
-
-
-import glob
+import requests
 import pandas as pd
-import xml.etree.ElementTree as ET
+import numpy as np
+import sqlite3
 from datetime import datetime
+import lxml
 
-log_file = "log_file.txt"
-target_file = "transformed_data.csv"
-
-def extract_from_csv(file_to_process):
-    dataframe = pd.read_csv(file_to_process)
-    return dataframe
-
-def extract_from_json(file_to_process):
-    dataframe = pd.read_json(file_to_process, lines=True)
-    return dataframe
-
-def extract_from_xml(file_to_process):
-    dataframe = pd.DataFrame(columns=["name", "height", "weight"])
-    tree = ET.parse(file_to_process)
-    root = tree.getroot()
-    for person in root:
-        name = person.find("name").text
-        height = float(person.find("height").text)
-        weight = float(person.find("weight").text)
-        dataframe = pd.concat([dataframe, pd.DataFrame([{"name":name,"height":height, "weight":weight}])], ignore_index=True)
-    return dataframe
-
-def extract():
-    extracted_data = pd.DataFrame(columns=['name','height','weight'])  # create an empty data frame to hold extracted data
-
-    # process all csv files, except the target file
-    for csvfile in glob.glob("*.csv"):
-        if csvfile != target_file:  # check if the file is not the target file
-            extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_csv(csvfile))], ignore_index=True)
-
-    # process all json files
-    for jsonfile in glob.glob("*.json"):
-        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_json(jsonfile))], ignore_index=True)
-
-    # process all xml files
-    for xmlfile in glob.glob("*.xml"):
-        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_xml(xmlfile))], ignore_index=True)
-
-    return extracted_data
-
-def transform(data):
-    # Convert inches to meters and round off to two decimals
-    # 1 inch is 0.0254 meters
-    data['height'] = round(data.height * 0.0254,2)
-
-    # Convert pounds to kilograms and round off to two decimals
-    # 1 pound is 0.45359237 kilograms
-    data['weight'] = round(data.weight * 0.45359237,2)
-
-    return data
-
-def load_data(target_file, transformed_data):
-    transformed_data.to_csv(target_file)
 
 def log_progress(message):
+    ''' This function logs the mentioned message of a given stage of the
+    code execution to a log file. Function returns nothing'''
+
     timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second
     now = datetime.now() # get current timestamp
     timestamp = now.strftime(timestamp_format)
-    with open(log_file,"a") as f:
-        f.write(timestamp + ',' + message + '\n')
+    with open("./code_log.txt","a") as f:
+        f.write(timestamp + ' : ' + message + '\n')
 
-# Log the initialization of the ETL process
-log_progress("ETL Job Started")
 
-# Log the beginning of the Extraction process
-log_progress("Extract phase Started")
-extracted_data = extract()
+def extract(url, table_attribs):
+    ''' This function aims to extract the required
+    information from the website and save it to a data frame. The
+    function returns the data frame for further processing. '''
 
-# Log the completion of the Extraction process
-log_progress("Extract phase Ended")
+    return df
 
-# Log the beginning of the Transformation process
-log_progress("Transform phase Started")
-transformed_data = transform(extracted_data)
-print("Transformed Data")
-print(transformed_data)
+def transform(df, csv_path):
+    ''' This function accesses the CSV file for exchange rate
+    information, and adds three columns to the data frame, each
+    containing the transformed version of Market Cap column to
+    respective currencies'''
 
-# Log the completion of the Transformation process
-log_progress("Transform phase Ended")
+    return df
 
-# Log the beginning of the Loading process
-log_progress("Load phase Started")
-load_data(target_file,transformed_data)
+def load_to_csv(df, output_path):
+    ''' This function saves the final data frame as a CSV file in
+    the provided path. Function returns nothing.'''
 
-# Log the completion of the Loading process
-log_progress("Load phase Ended")
+def load_to_db(df, sql_connection, table_name):
+    ''' This function saves the final data frame to a database
+    table with the provided name. Function returns nothing.'''
 
-# Log the completion of the ETL process
-log_progress("ETL Job Ended")
+def run_query(query_statement, sql_connection):
+    ''' This function runs the query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
+
+''' Here, you define the required entities and call the relevant
+functions in the correct order to complete the project. Note that this
+portion is not inside any function.'''
+
+
+url = 'https://web.archive.org/web/20230908091635/https://en.wikipedia.org/wiki/List_of_largest_banks'
+table_attribs = ["Name", "MC_USD_Billion"]
+db_name = 'Banks.db'
+table_name = 'Largest_banks'
+csv_path = './Largest_banks_data.csv'
+log_progress('Preliminaries complete. Initiating ETL process')
+
+df = extract(url, table_attribs)
+log_progress('Data extraction complete. Initiating Transformation process')
+
+df = transform(df)
+log_progress('Data transformation complete. Initiating loading process')
+
+load_to_csv(df, csv_path)
+log_progress('Data saved to CSV file')
+
+sql_connection = sqlite3.connect('World_Economies.db')
+log_progress('SQL Connection initiated.')
+
+load_to_db(df, sql_connection, table_name)
+log_progress('Data loaded to Database as table. Running the query')
+
+query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+run_query(query_statement, sql_connection)
+log_progress('Process Complete.')
+
+sql_connection.close()
+log_progress('Server Connection closed')
