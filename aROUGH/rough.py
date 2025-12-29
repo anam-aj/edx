0935cf9a0c00b0b3b5fb81ecddcfa856455c0d14
anam-aj <anamaj.786@gmail.com>
2025-12-29 06:27:41 +0000

    Mon, Dec 29, 2026, 11:57 AM +05:30

--- aROUGH/rough.py
+++ aROUGH/rough.py
@@ -53,7 +53,7 @@ functions in the correct order to complete the project. Note that this
 portion is not inside any function.'''
 
 
-url = 'https://web.archive.org/web/20230908091635/https://en.wikipedia.org/wiki/List_of_largest_banks'
+url ='https://web.archive.org/web/20230908091635/https://en.wikipedia.org/wiki/List_of_largest_banks'
 table_attribs = ["Name", "MC_USD_Billion"]
 db_name = 'Banks.db'
 table_name = 'Largest_banks'
