86ce2da4f2803380ac76a121c3c4cfe08203c4b6
anam <anamaj.786@gmail.com>
2026-01-04 19:34:39 +0000

    Mon, Jan 5, 2026, 1:04 AM +05:30

--- DSA_Coursera/2_data_structure/week2_priority_queues_and_disjoint_sets/job_queue.py
+++ DSA_Coursera/2_data_structure/week2_priority_queues_and_disjoint_sets/job_queue.py
@@ -3,18 +3,42 @@
 from collections import namedtuple
 
 AssignedJob = namedtuple("AssignedJob", ["worker", "started_at"])
+Thread = namedtuple("Thread", ["available_at", "index"])
 
 
 def assign_jobs(n_workers, jobs):
     # TODO: replace this code with a faster algorithm.
     result = []
-    next_free_time = [0] * n_workers
+    threads = [Thread(0, i) for i in range(n_workers)]
+
     for job in jobs:
-        next_worker = min(range(n_workers), key=lambda w: next_free_time[w])
-        result.append(AssignedJob(next_worker, next_free_time[next_worker]))
-        next_free_time[next_worker] += job
+        next_worker = threads[0]
+        result.append(AssignedJob(next_worker.index, next_worker.available_at))
+        updated_thread = Thread((next_worker.available_at + job), next_worker.index)
+        threads[0] = updated_thread
+        shift_down(0, threads)
 
     return result
+
+
+def shift_down(index, array):
+    while True:
+        left_child = 2 * index + 1
+        right_child = 2 * index + 2
+        if left_child >= len(array):
+            break
+
+        min_index = index
+        if left_child < len(array) and array[left_child] < array[min_index]:
+            min_index = left_child
+        if right_child < len(array) and array[right_child] < array[min_index]:
+            min_index = right_child
+
+        if min_index == index:
+            break
+        else:
+            array[index], array[min_index] = array[min_index], array[index]
+            index = min_index
 
 
 def main():
