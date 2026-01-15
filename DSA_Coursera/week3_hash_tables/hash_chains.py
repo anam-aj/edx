bf49fa5a6fe32a85424e1ee02455126d7b907bc5
anam <anamaj.786@gmail.com>
2026-01-15 18:01:33 +0000

    Thu, Jan 15, 2026, 11:31 PM +05:30

--- DSA_Coursera/week3_hash_tables/hash_chains.py
+++ DSA_Coursera/week3_hash_tables/hash_chains.py
@@ -17,7 +17,7 @@ class QueryProcessor:
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
         # store all strings in one list
-        self.elems = []
+        self.elems = [[] for _ in range(bucket_count)]
 
     def _hash_func(self, s):
         ans = 0
@@ -35,28 +35,27 @@ class QueryProcessor:
         return Query(input().split())
 
     def process_query(self, query):
-        if query.type == "check":
-            # use reverse order, because we append strings to the end
-            self.write_chain(cur for cur in reversed(self.elems)
-                        if self._hash_func(cur) == query.ind)
+        if query.type == 'check':
+            self.write_chain(cur for cur in reversed(self.elems[query.ind]))
         else:
+            hash_value = self._hash_func(query.s)
             try:
-                ind = self.elems.index(query.s)
+                index = self.elems[hash_value].index(query.s)
+                query_found = True
             except ValueError:
-                ind = -1
-            if query.type == 'find':
-                self.write_search_result(ind != -1)
-            elif query.type == 'add':
-                if ind == -1:
-                    self.elems.append(query.s)
-            else:
-                if ind != -1:
-                    self.elems.pop(ind)
+                query_found = False
+            if query.type == 'add' and not query_found:
+                self.elems[hash_value].append(query.s)
+            elif query.type == 'del' and query_found:
+                self.elems[hash_value].pop(index)
+            elif query.type == 'find':
+                self.write_search_result(query_found)
 
     def process_queries(self):
         n = int(input())
         for i in range(n):
             self.process_query(self.read_query())
+
 
 if __name__ == '__main__':
     bucket_count = int(input())
