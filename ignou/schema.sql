cfdf52e849533aae93dda3113a5ff1daef363afe
anam <anamaj.786@gmail.com>
2026-04-22 09:09:22 +0000

    Wed, Apr 22, 2026, 2:39 PM +05:30

--- ignou/schema.sql
+++ ignou/schema.sql
@@ -1,1 +1,19 @@
+CREATE DATABASE StudentExamSystem;
+USE StudentExamSystem;
 
+-- Table 1: Student
+CREATE TABLE Student (
+    StudentID VARCHAR(20) PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    ProgrammeCode VARCHAR(10) NOT NULL,
+    DateOfEnrolment DATE NOT NULL
+);
+
+-- Table 2: FeePaid
+CREATE TABLE FeePaid (
+    StudentID VARCHAR(20),
+    Semester INT NOT NULL,
+    dateofPayment DATE NOT NULL,
+    AmountPaid DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (StudentID) REFERENCES Student(StudentID) ON DELETE CASCADE
+);
