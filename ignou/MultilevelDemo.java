f239acbf6e9690b873f4cf4f2ff0b524c46b06e4
anam <anamaj.786@gmail.com>
2026-04-22 20:05:10 +0000

    Thu, Apr 23, 2026, 1:35 AM +05:30

--- ignou/MultilevelDemo.java
+++ ignou/MultilevelDemo.java
@@ -1,18 +1,9 @@
-class Animal { // Grandparent
-    void eat() { System.out.println("Eating..."); }
-}
-class Dog extends Animal { // Parent
-    void bark() { System.out.println("Barking..."); }
-}
-class Puppy extends Dog { // Child
-    void weep() { System.out.println("Weeping..."); }
+interface Drawable {
+    void draw(); // implicitly public and abstract
 }
 
-public class MultilevelDemo {
-    public static void main(String[] args) {
-        Puppy p = new Puppy();
-        p.eat();  // Inherited from Animal
-        p.bark(); // Inherited from Dog
-        p.weep(); // Own method
+class Rectangle implements Drawable {
+    public void draw() {
+        System.out.println("Drawing a rectangle");
     }
 }
