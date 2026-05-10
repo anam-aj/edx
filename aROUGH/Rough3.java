c75c3a92542e4c3eacb642a14505061074c9562f
anam <anamaj.786@gmail.com>
2026-05-10 02:46:08 +0000

    Sun, May 10, 2026, 8:16 AM +05:30

---
+++ aROUGH/Rough3.java
@@ -0,0 +1,126 @@
+
+
+class Rough3 {
+
+    static int add(int a, int b) {
+
+        return a + b;
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println(add(10,20));
+
+    }
+
+}
+
+
+
+/*
+Question 2
+
+Write a Java
+program to find factorial using method.
+
+
+class MethodFactorial {
+
+    static int fact(int n) {
+
+        int f = 1;
+
+
+
+        for(int i=1;i<=n;i++)
+
+            f *= i;
+
+
+
+        return f;
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println(fact(5));
+
+    }
+
+}
+
+
+
+
+Question 3
+
+Write a Java
+program to find square using method.
+
+
+class SquareMethod {
+
+    static int square(int n) {
+
+        return n * n;
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println(square(6));
+
+    }
+
+}
+
+
+
+
+Question 4
+
+Write a Java
+program demonstrating method overloading.
+
+
+class Overloading {
+
+    void add(int a, int b) {
+
+        System.out.println(a +
+b);
+
+    }
+
+
+
+    void add(int a, int b, int c) {
+
+        System.out.println(a +
+b + c);
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Overloading
+obj = new Overloading();
+
+        obj.add(10,20);
+
+        obj.add(10,20,30);
+
+    }
+
+}
+*/
